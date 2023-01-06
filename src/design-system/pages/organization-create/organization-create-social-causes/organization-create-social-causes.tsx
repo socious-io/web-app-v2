58869d2b62bd835aae093a4fb82e7d35c6a0c403
Sajad Abbasi <s.abbasi@me.com>
2023-01-06 13:20:03 +0330

    fat: add search component

--- src/design-system/pages/organization-create/organization-create-social-causes/organization-create-social-causes.tsx
+++ src/design-system/pages/organization-create/organization-create-social-causes/organization-create-social-causes.tsx
@@ -1,10 +1,14 @@
+import { useState } from 'react';
 import { Button } from '../../../atoms/button/button';
 import { CategoriesClickable } from '../../../atoms/categories-clickable/categories-clickable';
+import { Search } from '../../../atoms/search/search';
 import { BottomStatic } from '../../../templates/bottom-static/bottom-static';
 import css from './organization-create-social-causes.module.scss';
 import { SOCIAL_CAUSES } from './organization-create-social-causes.services';
 
 export const OrganizationCreateSocialCauses = (): JSX.Element => {
+  const [searchValue, setSearchValue] = useState('');
+
   return (
     <div className={css.container}>
       <div className={css.container}>
@@ -14,6 +18,11 @@ export const OrganizationCreateSocialCauses = (): JSX.Element => {
               <div className={css.title}> What are your social causes?</div>
               <div className={css.limitStatement}>
                 Select up to 5 social causes.
+                <Search
+                  placeholder="Search"
+                  value={searchValue}
+                  onValueChange={setSearchValue}
+                />
               </div>
             </div>
             <div className={css.categoriesContainer}>
