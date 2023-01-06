55d794bb4e08e2d495e8db5a94d912e5b8c80115
Sajad Abbasi <s.abbasi@me.com>
2023-01-06 13:30:35 +0330

    feat: add organization-create-social-causes page (#835)

--- src/design-system/atoms/categories-clickable/categories-clickable.types.ts
+++ src/design-system/atoms/categories-clickable/categories-clickable.types.ts
@@ -1,7 +1,8 @@
 import { CSSProperties } from 'react';
 
 export interface CategoriesClickableProps extends CSSProperties {
-  list: {value: string; label: string}[];
-  selected?: string[]
+  list: { value: string; label: string }[];
+  selected?: string[];
   onChange?: (newList: string[]) => void;
+  clickable?: boolean;
 }
