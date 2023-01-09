07e52968fe3d61b41c05e5ff667aea23c73d4a05
Sajad Abbasi <s.abbasi@me.com>
2023-01-09 17:19:16 +0330

    feat: add desktop version of organization type page (#835)

--- src/design-system/pages/organization-create/organization-create-type/organization-create-type.tsx
+++ src/design-system/pages/organization-create/organization-create-type/organization-create-type.tsx
@@ -1,32 +1,29 @@
-import css from './organization-create-type.module.scss';
-import { BottomStatic } from '../../../templates/bottom-static/bottom-static';
-import { Button } from '../../../atoms/button/button';
-import { TypeSelector } from '../../../atoms/type-selector/type-selector';
-import { useNavigate } from '@tanstack/react-location';
-import { ORGANIZATION_TYPE } from './organization-create-type.services';
+import { isTouchDevice } from '../../../../core/device-type-detector';
+import { Desktop } from './desktop/desktop';
+import { Mobile } from './mobile/mobile';
 
 export const OrganizationCreateType = (): JSX.Element => {
-  const navigate = useNavigate();
+  return isTouchDevice() ? <Mobile /> : <Desktop />;
 
-  return (
-    <div className={css.container}>
-      <BottomStatic>
-        <div>
-          <div className={css.question}>What type of organization?</div>
-          <div>
-            <TypeSelector
-              padding="2rem 1rem"
-              onChange={console.log}
-              list={ORGANIZATION_TYPE}
-            />
-          </div>
-        </div>
-        <div className={css.bottom}>
-          <Button onClick={() => navigate({ to: '../social-causes' })}>
-            Continue
-          </Button>
-        </div>
-      </BottomStatic>
-    </div>
-  );
+  //   return (
+  //     <div className={css.container}>
+  //       <BottomStatic>
+  //         <div>
+  //           <div className={css.question}>What type of organization?</div>
+  //           <div>
+  //             <TypeSelector
+  //               padding="2rem 1rem"
+  //               onChange={console.log}
+  //               list={ORGANIZATION_TYPE}
+  //             />
+  //           </div>
+  //         </div>
+  //         <div className={css.bottom}>
+  //           <Button onClick={() => navigate({ to: '../social-causes' })}>
+  //             Continue
+  //           </Button>
+  //         </div>
+  //       </BottomStatic>
+  //     </div>
+  //   );
 };
--- src/design-system/pages/organization-create/profile/desktop/desktop.tsx
+++ src/design-system/pages/organization-create/profile/desktop/desktop.tsx
@@ -2,6 +2,8 @@ import css from './desktop.module.scss';
 import { Input } from '../../../../atoms/input/input';
 import { Textarea } from '../../../../atoms/textarea/textarea';
 import { Divider } from '../../../../templates/divider/divider';
+import { Card } from '../../../../atoms/card/card';
+import { Header } from '../../../../atoms/header/header';
 
 const sharedProps: Record<string, string> = {
   className: css.input,
@@ -11,17 +13,20 @@ const sharedProps: Record<string, string> = {
 export const Desktop = (): JSX.Element => {
   return (
     <div className={css.container}>
-      <Divider title="Basic info" divider="space">
-        <Input {...sharedProps} label="Organization name" placeholder="Organization name" />
-        <Textarea {...sharedProps} label="Bio" placeholder="Your organization's bio" />
-      </Divider>
-      <Divider title="Contact" divider="space">
-        <Input {...sharedProps} label="Organization email" placeholder="Organization email" />
-        <Input {...sharedProps} label="Country" placeholder="Country" />
-        <Input {...sharedProps} label="City" placeholder="City" />
-        <Input {...sharedProps} label="Address" placeholder="Address" />
-        <Input {...sharedProps} label="Website" placeholder="Website" />
-      </Divider>
+      <Card padding="0" className={css.card}>
+        <Header title={'xxx'} />
+        <Divider title="Basic info" divider="space">
+          <Input {...sharedProps} label="Organization name" placeholder="Organization name" />
+          <Textarea {...sharedProps} label="Bio" placeholder="Your organization's bio" />
+        </Divider>
+        <Divider title="Contact" divider="space">
+          <Input {...sharedProps} label="Organization email" placeholder="Organization email" />
+          <Input {...sharedProps} label="Country" placeholder="Country" />
+          <Input {...sharedProps} label="City" placeholder="City" />
+          <Input {...sharedProps} label="Address" placeholder="Address" />
+          <Input {...sharedProps} label="Website" placeholder="Website" />
+        </Divider>
+      </Card>
     </div>
   );
 };
