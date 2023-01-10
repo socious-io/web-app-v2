3c6c1527bb2d260ce5b71803437993d495fc3af2
Sajad Abbasi <s.abbasi@me.com>
2023-01-10 15:52:11 +0330

    fix: disabled steps in organization type page (#835)

--- src/design-system/atoms/steps/steps.types.ts
+++ src/design-system/atoms/steps/steps.types.ts
@@ -3,6 +3,7 @@ import { CSSProperties } from 'react';
 export interface StepsProps extends CSSProperties {
   length: number;
   current: number;
+  clickable?: boolean;
   autoPlay?: boolean;
   onStepClick?: (stepNumber: number) => void;
 }
