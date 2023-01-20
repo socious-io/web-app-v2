9607ed56b38e8dd1c5d924a2262129ee212c4fc7
Sajad Abbasi <s.abbasi@me.com>
2023-01-20 10:49:08 +0330

    chore: update gitignore

--- ios/DerivedData/3A957CBA-7995-4A5D-BCFA-4C339C80D826/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-App.build/Objects-normal/arm64/Pods-App-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/sajadabbasi/Projects/socious-web-app/ios/App/Pods/Target\ Support\ Files/Pods-App/Pods-App-dummy.m \
-  /Users/sajadabbasi/Projects/socious-web-app/ios/DerivedData/3A957CBA-7995-4A5D-BCFA-4C339C80D826/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-App.build/module.modulemap
--- ios/DerivedData/3A957CBA-7995-4A5D-BCFA-4C339C80D826/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-App.build/Objects-normal/arm64/Pods_App_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/sajadabbasi/Projects/socious-web-app/ios/DerivedData/3A957CBA-7995-4A5D-BCFA-4C339C80D826/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-App.build/DerivedSources/Pods_App_vers.c
