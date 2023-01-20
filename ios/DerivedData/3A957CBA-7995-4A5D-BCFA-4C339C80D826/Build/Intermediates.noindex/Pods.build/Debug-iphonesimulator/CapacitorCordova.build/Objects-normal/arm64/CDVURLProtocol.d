c72b30187cc9223c3877e893fdc3c3d1f9b5451e
Sajad Abbasi <s.abbasi@me.com>
2023-01-20 10:49:08 +0330

    chore: update gitignore

--- ios/DerivedData/3A957CBA-7995-4A5D-BCFA-4C339C80D826/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/CapacitorCordova.build/Objects-normal/arm64/CDVURLProtocol.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/sajadabbasi/Projects/socious-web-app/node_modules/@capacitor/ios/CapacitorCordova/CapacitorCordova/Classes/Public/CDVURLProtocol.m \
-  /Users/sajadabbasi/Projects/socious-web-app/ios/DerivedData/3A957CBA-7995-4A5D-BCFA-4C339C80D826/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/CapacitorCordova.build/module.modulemap \
-  /Users/sajadabbasi/Projects/socious-web-app/ios/App/Pods/Target\ Support\ Files/CapacitorCordova/CapacitorCordova-prefix.pch \
-  /Users/sajadabbasi/Projects/socious-web-app/node_modules/@capacitor/ios/CapacitorCordova/CapacitorCordova/Classes/Public/CDVURLProtocol.h \
-  /Users/sajadabbasi/Projects/socious-web-app/node_modules/@capacitor/ios/CapacitorCordova/CapacitorCordova/Classes/Public/CDVAvailability.h \
-  /Users/sajadabbasi/Projects/socious-web-app/node_modules/@capacitor/ios/CapacitorCordova/CapacitorCordova/Classes/Public/CDVViewController.h
