4f6447ee8011d448b8fa078ef853310d673e0951
Sajad Abbasi <s.abbasi@me.com>
2023-01-21 20:49:09 +0330

    feat: add contact-list page

--- src/design-system/organisms/contact-list/contact-list.tsx
+++ src/design-system/organisms/contact-list/contact-list.tsx
@@ -4,13 +4,22 @@ import css from './contact-list.module.scss';
 import { ContactListProps } from './contact-list.types';
 
 export const ContactList = (props: ContactListProps): JSX.Element => {
-  const { list, onSearch, ...rest } = props;
+  const { list, message, onSearch, ...rest } = props;
+
+  const messageJSX = (
+    <div className={css.message}>
+      <div className={css.title}>{message?.title}</div>
+      <div className={css.body}>{message?.body}</div>
+    </div>
+  );
+
   return (
     <div style={rest} className={css.container}>
       <div className={css.searchContainer}>
-        <Search flex="1" defaultValue="" placeholder="search name" onValueChange={onSearch} />
+        <Search flex="1" placeholder="search name" onValueChange={onSearch} />
       </div>
       <div>
+        {message && list.length === 0 && messageJSX}
         {list.map((contactData) => (
           <ContactItem key={contactData.id} {...contactData} />
         ))}
