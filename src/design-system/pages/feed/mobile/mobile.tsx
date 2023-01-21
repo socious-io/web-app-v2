99c24cf547f56c5d6312fd86fc77ae2d4e8fb1b1
Sara Kaveh <sara kaveh>
2023-01-21 20:35:22 +0330

    renamed from feed-card to feed-item

--- src/design-system/pages/feed/mobile/mobile.tsx
+++ src/design-system/pages/feed/mobile/mobile.tsx
@@ -1,10 +1,11 @@
-import { FeedCard } from '../../../molecules/feed-card/feed-card';
+
+import { FeedItem } from '../../../molecules/feed-item/feed-item';
 import css from './mobile.module.scss';
 
 export const Mobile = () => {
     return (
         <div className={css.container}>
-            <FeedCard />
+            <FeedItem />
         </div>
     )
 }
