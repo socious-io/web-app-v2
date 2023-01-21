3c5ce14d883fbc912e4d54ba8542eb85017aacb1
Sajad Abbasi <s.abbasi@me.com>
2023-01-21 18:29:55 +0330

    refactor: add right section to header

--- src/design-system/pages/chat/contact-list/mobile/mobile.tsx
+++ src/design-system/pages/chat/contact-list/mobile/mobile.tsx
@@ -1,10 +1,11 @@
+import { Avatar } from '../../../../atoms/avatar/avatar';
 import { Header } from '../../../../atoms/header/header';
 import { HeaderStaticMobile } from '../../../../templates/header-static-mobile/header-static-mobile';
 
 export const Mobile = (): JSX.Element => {
   return (
     <HeaderStaticMobile>
-      <Header height={0} title="title" />
+      <Header height="auto" title="title" right={<Avatar size="2rem" type="user" />} />
       <div>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati facere asperiores
         accusamus! Provident eos, placeat ipsum harum sint veniam esse suscipit architecto
