f5c2b47ca3e9e11eae3fbdc94f2e3ac447189856
Sajad Abbasi <s.abbasi@me.com>
2023-01-23 17:57:45 +0330

    feat: add chat-list organism

--- src/design-system/atoms/message/message.types.ts
+++ src/design-system/atoms/message/message.types.ts
@@ -1,7 +1,14 @@
 import { CSSProperties } from 'react';
 
-export interface MessageProps extends CSSProperties {
+export interface Message {
+  id: string;
   img: string;
-  type: 'sender' | 'receiver'
+  type: 'sender' | 'receiver';
+  text: string;
+}
+export interface MessageProps extends CSSProperties {
+  id: string;
+  img: string;
+  type: 'sender' | 'receiver';
   text: string;
 }
