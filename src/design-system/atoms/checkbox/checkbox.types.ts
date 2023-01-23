1676e132782c4093c8cd2355ba295f5bf8085f02
Sara Kaveh <sara kaveh>
2023-01-23 13:10:33 +0330

    completed checkbox component

---
+++ src/design-system/atoms/checkbox/checkbox.types.ts
@@ -0,0 +1,6 @@
+export type CheckboxProps = {
+    label: string;
+    id: string;
+    checked: boolean;
+    disabled?: boolean;
+}
--- src/design-system/pages/feed/desktop/desktop.tsx
+++ src/design-system/pages/feed/desktop/desktop.tsx
@@ -1,7 +1,10 @@
+import { Checkbox } from '../../../atoms/checkbox/checkbox';
 import css from './desktop.module.scss';
 
 export const Desktop = () => {
     return (
-        <div className={css.container}>desktop feed</div>
+        <div className={css.container}>
+            <Checkbox id='1' label='what is this?' checked={false} disabled={false} />
+        </div>
     )
 }
