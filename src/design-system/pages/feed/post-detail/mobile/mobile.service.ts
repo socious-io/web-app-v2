a8977338f1fbcc38b9317a0afa039bf88fd57385
Sara Kaveh <sara kaveh>
2023-01-29 23:27:39 +0330

    fixed send-box in post-detail

--- src/design-system/pages/feed/post-detail/mobile/mobile.service.ts
+++ src/design-system/pages/feed/post-detail/mobile/mobile.service.ts
@@ -1,6 +1,14 @@
-import { get } from "../../../../../core/http";
+import { get, post } from "../../../../../core/http";
+import { SearchReq } from "../../../../../core/types";
 import { Feed } from "../../../../organisms/feed-list/feed-list.types";
 
 export async function getPostDetail(id: string): Promise<Feed> {
-    return get(`posts/${id}`).then(({ data }) => data);
+  return get(`posts/${id}`).then(({ data }) => data);
+}
+
+export async function comments(content: string, id: string): Promise<SearchReq> {
+  const body = {
+    content
   }
+  return post(`posts/${id}/comments`, body).then(({ data }) => data);
+}
--- src/design-system/pages/feed/post-detail/mobile/mobile.tsx
+++ src/design-system/pages/feed/post-detail/mobile/mobile.tsx
@@ -1,14 +1,31 @@
+import { useState } from 'react';
 import { toCategoriesAdaptor } from '../../../../../core/adaptors';
 import { FeedItem } from '../../../../molecules/feed-item/feed-item';
+import { SendBox } from '../../../../molecules/send-box/send-box';
 import css from './mobile.module.scss';
+import { comments } from './mobile.service';
 import { MobileProps } from './mobile.types';
 
 export const Mobile = ({ data }: MobileProps) => {
+    const [message , setMessage] = useState('');
 
     const actionList = (likes: number, liked: boolean) => [
         { label: 'Like', iconName: 'heart-blue', like: likes, isLiked: liked },
         { label: 'Comment', iconName: 'comment-blue' },
     ];
+
+    const changeMessageHandler = (value: string) => {
+        console.log('value ==> ', value);
+        setMessage(value);
+
+
+    }
+
+    const sendMessage = () => {
+        comments(message, data.id).then(resp => {
+            console.log('resp', resp);
+        });
+    }
 
     return (
 
@@ -31,6 +48,12 @@ export const Mobile = ({ data }: MobileProps) => {
                 date={data.created_at}
                 categories={toCategoriesAdaptor(data.causes_tags)}
             />
+            <div className={css.sendBox}>
+                <SendBox onValueChange={changeMessageHandler} onSend={sendMessage} />
+            </div>
+            <div className={css.messages}>
+
+            </div>
         </div>
     )
 }
