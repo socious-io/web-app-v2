b03017c37c9bd0f40617517fba10fab557c627d6
Sara Kaveh <sara kaveh>
2023-01-30 01:02:48 +0330

    completed post-detail component

--- src/design-system/molecules/comment/comment.tsx
+++ src/design-system/molecules/comment/comment.tsx
@@ -1,28 +1,37 @@
 
+import { toRelativeTime } from '../../../core/relative-time';
 import { Avatar } from '../../atoms/avatar/avatar';
 import { ChatBox } from '../../atoms/chat-box/chat-box';
 import css from './comment.module.scss';
+import { CommentProps } from './comment.types';
 
-export const Comment = () => {
+export const Comment = (props: CommentProps) => {
+
     return (
-        <div className={css.container}>
-            <div className={css.info}>
-                <Avatar type='users' size='2rem' />
-                <span className={css.date}>2 min ago</span>
-            </div>
+        <>
+            {
+                props.list.map(item => <div key={item.id} className={css.container}>
+                    <div className={css.info}>
+                        <Avatar type='users' size='2rem' img={item.identity_meta.avatar} />
+                        <span>{item.identity_meta.name}</span>
+                        <span className={css.date}>{toRelativeTime(item.created_at)}</span>
+                    </div>
 
-            <div className={css.wrapper}>
-                <div className={css.messageBox}>
-                    <ChatBox type='sender'>
-                        ddd
-                    </ChatBox>
-                </div>
+                    <div className={css.wrapper}>
+                        <div className={css.messageBox}>
+                            <ChatBox type='sender'>
+                                {item.content}
+                            </ChatBox>
+                        </div>
 
-                <div className={css.like}>
-                    <img src="/icons/heart-blue.svg" />
-                    <span>0 likes</span>
-                </div>
-            </div>
-        </div>
+                        <div className={css.like}>
+                            <img src="/icons/heart-blue.svg" />
+                            <span>{item.likes} likes</span>
+                        </div>
+                    </div>
+                </div>)
+            }
+        </>
+
     )
 }
