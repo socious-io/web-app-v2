8b5b49c8428cd6aab3082f5228c34f0661eb9671
Sara Kaveh <sara kaveh>
2023-02-01 16:32:41 +0330

    fixed bug in search module

--- src/design-system/pages/search/search.tsx
+++ src/design-system/pages/search/search.tsx
@@ -3,43 +3,34 @@ import { Search as SearchAtom } from '../../atoms/search/search'
 import { search } from './search.services';
 import { useState } from 'react';
 import { PayloadModel } from './search.types';
-import { useMatch, useMatchRoute } from '@tanstack/react-location';
+import { useMatch, useNavigate } from '@tanstack/react-location';
 import { JobList } from '../../organisms/job-list/job-list';
-
-// const subMenuList = [{ label: 'Projects', value: 'projects' }, { label: 'Posts', value: 'posts' }];
-
-const menuList = [{ label: 'Projects', value: 'projects' }, { label: 'Posts', value: 'posts' }, { label: 'Socila Causes', value: 'social' }, { label: 'Skills ', value: 'skills' }]
-
-
 
 
 export const Search = () => {
     const data = useMatch().ownData;
     const [list, setList] = useState(data.items);
-    const [result, setResult] = useState(data.total_count);
+    const [result, setResult] = useState<number>(data.total_count);
+    const [page, setPage] = useState(1);
+    const navigate = useNavigate();
 
     const [state, setState] = useState<PayloadModel>({
         page: 1,
         filter: {},
         type: 'projects',
-        q: 'socious'
+        q: ''
     });
 
-    // const onChangeMenu = (value: string) => {
-    //     const payload = { ...state, type: value };
-    //     setState(payload);
-    //     getResponse(payload);
-    // }
-
-    const getResponse = (state: PayloadModel) => {
+    const getResponse = () => {
         search(state).then(resp => {
-            // setResult(resp.items.length);
+            setResult(resp.items.length);
             setList(resp.items);
         });
     };
 
     const onValueChange = (value: string) => {
-        console.log('value ==>', value);
+        setState({ ...state, q: value });
+        getResponse();
     }
 
     const onLike = () => {
@@ -50,33 +41,37 @@ export const Search = () => {
 
     }
 
-    const onMorePage = () => {
-
+    const onMorePageClick = () => {
+        search({ ...state, page: page + 1 }).then((resp) => {
+            setPage((v) => v + 1);
+            setList((list) => [...list, ...resp.items]);
+        });
     }
 
     return (
         <div className={css.container}>
             <div className={css.header}>
-                <img src='/icons/chevron-left.svg' />
+                <div onClick={() => { navigate({ to: '../jobs' }) }}>
+                    <img src='/icons/chevron-left.svg' />
+                </div>
                 <div className={css.search}>
                     <SearchAtom placeholder='Search' onValueChange={onValueChange} />
                 </div>
             </div>
-            {/* <div className={css.menu}>
-                <FilterMenu list={menuList} selectedValue='projects' onGetValue={onChangeMenu} />
-            </div> */}
-            <div className={css.result}>
-                <span>{result} Results</span>
-                <img src="/icons/filter-blue.svg" />
+            <div className={css.main}>
+                <div className={css.result}>
+                    <div className={css.info}>
+                        <span>{result}</span>
+                        <span>Results</span>
+                    </div>
+                    <img src="/icons/filter-blue.svg" />
+                </div>
+
+                <div className={css.mainList}>
+                    {/* <FeedList data={list} onLike={onLike} onRemoveLike={onRemoveLike} onMorePageClick={onMorePage} /> */}
+                    <JobList data={list} onMorePageClick={onMorePageClick} />
+                </div>
             </div>
-
-            <div className={css.mainList}>
-                {/* <FeedList data={list} onLike={onLike} onRemoveLike={onRemoveLike} onMorePageClick={onMorePage} /> */}
-                <JobList data={list} onMorePageClick={onMorePage} />
-
-
-            </div>
-
         </div>
     );
 };
