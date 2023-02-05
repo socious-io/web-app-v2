b0793b0524ce60948aa6f3acc13b06155b60cf99
Sara Kaveh <sara kaveh>
2023-02-05 12:56:10 +0330

    comleted style in password component

--- src/design-system/pages/forget-password/password/password.tsx
+++ src/design-system/pages/forget-password/password/password.tsx
@@ -1,5 +1,55 @@
+
+import { useNavigate } from '@tanstack/react-location';
+import { Button } from '../../../atoms/button/button';
+import { Input } from '../../../atoms/input/input';
+import css from './password.module.scss';
+import { PasswordQuality } from '../../../atoms/password-quality/password-quality';
+
+const validator = [
+    {
+        name: 'characters',
+        amount: 7
+    },
+    {
+        name: 'number',
+        amount: 1
+    },
+]
+
 export const Password = () => {
-    return(
-        <div>pass</div>
+    const navigate = useNavigate();
+
+    const changePassword = () => {
+        // navigate({ to: '../otp' });
+    }
+
+    const backToPerviousPage = () => {
+        navigate({ to: '../otp' });
+    }
+    //     name: 'characters' | 'number';
+    //     amount: number;
+    //   };
+
+    return (
+        <div className={css.container}>
+            <div className={css.header}>
+                <div onClick={backToPerviousPage}>
+                    <img src="/icons/chevron-left.svg" />
+                </div>
+            </div>
+            <div className={css.main}>
+                <span className={css.title}>Reset your password </span>
+                <div className={css.newPassword}>
+                    <Input variant='outline' placeholder='New password' label='New password' />
+                    <Input variant='outline' placeholder='Confirm new password' label='Confirm new password' />
+                    <PasswordQuality value='Saaraa@123' validators={validator} />
+                </div>
+            </div>
+            <div className={css.footer}>
+                <Button color='blue' onClick={changePassword}>
+                    Change your password
+                </Button>
+            </div>
+        </div>
     )
 }
