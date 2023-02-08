67e3203395eece5e769ea037f1d4beab52a5ccc8
Sajad Abbasi <s.abbasi@me.com>
2023-02-08 11:46:52 +0330

    fix: see more in post detail

--- src/design-system/molecules/feed-item/feed-item.tsx
+++ src/design-system/molecules/feed-item/feed-item.tsx
@@ -7,31 +7,33 @@ import { Typography } from '../../atoms/typography/typography';
 import css from './feed-item.module.scss';
 import { FeedItemProps } from './feed-item.types';
 
-
 export const FeedItem = (props: FeedItemProps): JSX.Element => {
-    return (
-        <Card>
-            <div className={css.header}>
-                <div className={css.info}>
-                    <Avatar type={props.type} size="2rem" img={props.imgAvatar} />
-                    <span>{props.name}</span>
-                    <span className={css.date}>{toRelativeTime(props.date)}</span>
-                </div>
+  const { type, actionList, date, lineLimit = 3, img, name, categories, imgAvatar } = props;
+  return (
+    <Card>
+      <div className={css.header}>
+        <div className={css.info}>
+          <Avatar type={type} size="2rem" img={imgAvatar} />
+          <span>{name}</span>
+          <span className={css.date}>{toRelativeTime(date)}</span>
+        </div>
 
-                <div className={css.icon}>
-                    <img src="/icons/three-dots-blue.svg" />
-                </div>
-            </div>
-            <div className={css.img}>
-                <img src={props.img} />
-            </div>
-            <CategoriesClickable list={props.categories} />
-            <div className={css.text}>
-                <Typography type='body' lineLimit={3} size='s2'>{props.text}</Typography>
-            </div>
-            <div>
-                <ActionList list={props.actionList} />
-            </div>
-        </Card>
-    );
+        <div className={css.icon}>
+          <img src="/icons/three-dots-blue.svg" />
+        </div>
+      </div>
+      <div className={css.img}>
+        <img src={img} />
+      </div>
+      <CategoriesClickable list={props.categories} />
+      <div className={css.text}>
+        <Typography type="body" lineLimit={lineLimit} size="s2">
+          {props.text}
+        </Typography>
+      </div>
+      <div>
+        <ActionList list={actionList} />
+      </div>
+    </Card>
+  );
 };
