05fc2f3f1e377f485c11f01ca21485665f6fbc38
Sara Kaveh <sara kaveh>
2023-02-08 13:26:21 +0330

    completed style in delete and password page

---
+++ src/design-system/pages/delete-profile/delete/delete.tsx
@@ -0,0 +1,45 @@
+import { useNavigate } from '@tanstack/react-location';
+import { Button } from '../../../atoms/button/button';
+import css from './delete.module.scss';
+
+export const Delete = () => {
+    const navigate = useNavigate();
+
+    const backToPerviousPage = () => {
+
+    }
+
+    const navigateToPassword = () => {
+        navigate({ to: '../password' });
+    }
+
+    const cancel = () => {
+        navigate({ to: '../../jobs' });
+    }
+
+    return (
+        <div className={css.container}>
+            <div className={css.header}>
+                <div onClick={backToPerviousPage}>
+                    <img src="/icons/chevron-left.svg" />
+                </div>
+            </div>
+            <div className={css.main}>
+                <div className={css.content}>
+                    <span className={css.title}>Are you sure? </span>
+                    <span className={css.text}>Deleteing your account will erase all your existing activity on Socious, including connections you've made, and jobs you've conterbuted to.</span>
+                    <br />
+                    <span>This action is irreversible.</span>
+                </div>
+            </div>
+            <div className={css.footer}>
+                <Button color='blue' onClick={navigateToPassword}>
+                    Continue
+                </Button>
+                <Button color='white' onClick={cancel}>
+                    Cancel
+                </Button>
+            </div>
+        </div>
+    )
+}
