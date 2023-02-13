30c2c61805956ce63b5bdd24358bd20d6bb09e99
Sara Kaveh <sara kaveh>
2023-02-13 13:23:53 +0330

    feature: add impact-points

--- src/design-system/atoms/impact-badge/impact-badge.tsx
+++ src/design-system/atoms/impact-badge/impact-badge.tsx
@@ -1,20 +1,10 @@
-import {LIST} from './impact-badge.constant';
 import css from './impact-badge.module.scss';
-import {ImpactBadgeProps} from './impact-badge.types';
+import { ImpactBadgeProps } from './impact-badge.types';
 
-export const ImpactBadge = ({name}: ImpactBadgeProps): JSX.Element => {
-  const color = LIST[name].color;
-
+export const ImpactBadge = (props: ImpactBadgeProps): JSX.Element => {
   return (
-    <div
-      role="button"
-      style={{backgroundColor: color}}
-      className={css.container}
-    >
-      {/* <img
-        alt="impact icon"
-        src={require(`../../../../asset/icons/impact-category-list/${name}.svg`)}
-      /> */}
+    <div role="button" style={{ backgroundColor: props.color }} className={css.container}>
+      <img src={props.iconUrl} />
     </div>
   );
 };
--- src/design-system/pages/achievements/achievements.service.ts
+++
@@ -1,6 +0,0 @@
-import { get } from '../../../core/http';
-
-export function getBadges() {
-  // return get('/user/badges');
-  return get('/user/impact-points');
-}
