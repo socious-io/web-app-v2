e821a0af8a9188a428e208dafe4bfeeaaba87000
Sara Kaveh <sara kaveh>
2023-02-13 17:58:51 +0330

    craeted menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Provider } from 'react-redux';
 import { Outlet, ReactLocation, Router } from '@tanstack/react-location';
 import { routes } from './core/routes';
 import store from './store/store';
+import { Menu } from './design-system/pages/menu/menu';
 
 const location = new ReactLocation();
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <Provider store={store}>
       <Router location={location} routes={routes}>
+        {/* <Menu /> */}
         <Outlet />
       </Router>
     </Provider>
---
+++ src/design-system/pages/menu/mobile/mobile.tsx
@@ -0,0 +1,7 @@
+import css from './mobile.module.scss';
+
+export const Mobile = () => {
+    return (
+        <div className={css.container}>menu</div>
+    )
+}
