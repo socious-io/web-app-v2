f655a6f5efc1c76e6710993bcac9938093b11c4a
Sara Kaveh <sara kaveh>
2023-02-13 13:00:01 +0330

    fixed bug releated to type for avatar

--- src/design-system/molecules/comment/comment.tsx
+++ src/design-system/molecules/comment/comment.tsx
@@ -1,6 +1,7 @@
 import { toRelativeTime } from '../../../core/relative-time';
 import { Avatar } from '../../atoms/avatar/avatar';
 import { ChatBox } from '../../atoms/chat-box/chat-box';
+import { Items } from '../../atoms/dropdown/items/items';
 import css from './comment.module.scss';
 import { CommentProps } from './comment.types';
 
@@ -29,7 +30,7 @@ export const Comment = (props: CommentProps) => {
       {props.list.map((item) => (
         <div key={item.id} className={css.container}>
           <div className={css.info}>
-            <Avatar type="users" size="2rem" img={item.identity_meta.avatar} />
+            <Avatar type={item.identity_type} size="2rem" img={item.identity_meta.avatar} />
             <span>{item.identity_meta.name}</span>
             <span className={css.date}>{toRelativeTime(item.created_at)}</span>
           </div>
