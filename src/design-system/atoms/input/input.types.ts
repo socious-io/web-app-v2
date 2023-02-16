8f42993ded8e89109b768836f77f2ea693bf1327
Sajad Abbasi <s.abbasi@me.com>
2023-02-16 09:34:28 +0330

    feat: add react-hook-form

--- src/design-system/atoms/input/input.types.ts
+++ src/design-system/atoms/input/input.types.ts
@@ -1,10 +1,12 @@
+import { FieldValue, FieldValues, Path, UseFormRegister } from 'react-hook-form';
 
 type ReactHTMLInputElement = React.DetailedHTMLProps<
   React.InputHTMLAttributes<HTMLInputElement>,
   HTMLInputElement
 >;
-export interface InputProps extends ReactHTMLInputElement {
-  label?: string;
+export interface InputProps<T = unknown> extends ReactHTMLInputElement {
+  label?: Path<T extends FieldValues>;
+  register?: UseFormRegister<T extends FieldValues>;
   variant?: 'outline';
   className?: string;
   errors?: string[];
