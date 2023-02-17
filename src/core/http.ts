5a9da79d71fdb1a7c4d35a5998f4ff03bbf677a1
Sajad Abbasi <s.abbasi@me.com>
2023-02-17 17:15:58 +0330

    refactor: http base url

--- src/core/http.ts
+++ src/core/http.ts
@@ -1,13 +1,13 @@
-import axios, { AxiosRequestConfig } from 'axios';
+import axios from 'axios';
 
 const http = axios.create({
-  baseURL: process.env.BASE_URL || 'https://dev.socious.io/api/v2',
+  baseURL: 'https://dev.socious.io/api/v2',
   withCredentials: true,
   timeout: 100000,
 });
 
-export async function post(uri: string, payload: unknown, config?: AxiosRequestConfig<unknown>) {
-  return http.post(uri, payload, config);
+export async function post(uri: string, payload: unknown) {
+  return http.post(uri, payload);
 }
 
 export async function get(uri: string) {
