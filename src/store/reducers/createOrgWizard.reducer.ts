dd05756f2064252f87ffd621731d663c548fb39d
Sajad Abbasi <s.abbasi@me.com>
2023-02-17 17:07:38 +0330

    fix: organization create profile validations

--- src/store/reducers/createOrgWizard.reducer.ts
+++ src/store/reducers/createOrgWizard.reducer.ts
@@ -2,7 +2,18 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export type CreateOrgWizard = {
   type: string;
-  SocialCauses: string[];
+  socialCauses: string[];
+  mission: string;
+  culture: string;
+  organizationName: string;
+  bio: string;
+  organizationEmail: string;
+  country: string;
+  city: number;
+  address: string;
+  countryCode: string;
+  phoneNumber: string;
+  website: string;
 };
 
 export const createOrgWizardSlice = createSlice({
@@ -12,6 +23,15 @@ export const createOrgWizardSlice = createSlice({
     socialCauses: [],
     mission: '',
     culture: '',
+    organizationName: '',
+    bio: '',
+    organizationEmail: '',
+    country: '',
+    city: 0,
+    address: '',
+    countryCode: '',
+    phoneNumber: '',
+    website: '',
   },
   reducers: {
     setOrgType: (state, action) => {
@@ -26,7 +46,48 @@ export const createOrgWizardSlice = createSlice({
     setCulture: (state, action) => {
       state.culture = action.payload;
     },
+    setOrganizationName: (state, action) => {
+      state.organizationName = action.payload;
+    },
+    setBio: (state, action) => {
+      state.bio = action.payload;
+    },
+    setOrganizationEmail: (state, action) => {
+      state.organizationEmail = action.payload;
+    },
+    setCountry: (state, action) => {
+      state.country = action.payload;
+    },
+    setCity: (state, action) => {
+      state.city = action.payload;
+    },
+    setAddress: (state, action) => {
+      state.address = action.payload;
+    },
+    setCountryCode: (state, action) => {
+      state.countryCode = action.payload;
+    },
+    setPhoneNumber: (state, action) => {
+      state.phoneNumber = action.payload;
+    },
+    setWebsite: (state, action) => {
+      state.website = action.payload;
+    },
   },
 });
 
-export const { setOrgType, setSocialCauses, setMission, setCulture } = createOrgWizardSlice.actions;
+export const {
+  setOrgType,
+  setSocialCauses,
+  setMission,
+  setCulture,
+  setOrganizationName,
+  setBio,
+  setOrganizationEmail,
+  setCountry,
+  setCity,
+  setAddress,
+  setCountryCode,
+  setPhoneNumber,
+  setWebsite,
+} = createOrgWizardSlice.actions;
