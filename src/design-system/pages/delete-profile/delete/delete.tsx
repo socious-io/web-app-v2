8c8ab27fc5f283fa9301f1ef91d9a53cd2bb8f5c
Sara Kaveh <sara kaveh>
2023-02-17 18:46:50 +0330

    added links and icons to menu

--- src/design-system/pages/delete-profile/delete/delete.tsx
+++ src/design-system/pages/delete-profile/delete/delete.tsx
@@ -6,7 +6,7 @@ export const Delete = () => {
     const navigate = useNavigate();
 
     const backToPerviousPage = () => {
-
+        navigate({ to: '../../jobs' });
     }
 
     const navigateToPassword = () => {
