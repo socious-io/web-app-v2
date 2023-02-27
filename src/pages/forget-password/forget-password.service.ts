f8f447ea94979b91c799c54e29c29b7f68024a9d
Sajad Abbasi <s.abbasi@me.com>
2023-02-27 18:27:36 +0330

    fix: forgot-password

--- src/pages/forget-password/forget-password.service.ts
+++ src/pages/forget-password/forget-password.service.ts
@@ -1,21 +1,21 @@
-import { get, post } from "../../core/http";
+import { get, post } from '../../core/http';
 
 export async function forgetPassword(email: string) {
-    const body = {
-        email
-    }
+  const body = {
+    email,
+  };
 
-    return post('/auth/forget-password', body).then(({ data }) => data);
+  return post('/auth/forget-password', body).then(({ data }) => data);
 }
 
 export async function confirm(email: string, code: string) {
-    return get(`/auth/otp/confirm/web?email=${email}&code=${code}`).then(({ data }) => data);
+  return get(`/auth/otp/confirm/web?email=${email}&code=${code}`).then(({ data }) => data);
 }
 
 export async function changePassword(password: string) {
-    const body = {
-        password
-    }
+  const body = {
+    password,
+  };
 
-    return post('/user/change-password-direct', body).then(({ data }) => data);
+  return post('/user/change-password-direct', body).then(({ data }) => data);
 }
