41332b54c043011940f923772ecd2538dc1b89f9
Sajad Abbasi <s.abbasi@me.com>
2023-02-27 21:35:07 +0330

    fix: delete-account

--- src/pages/delete-profile/delete-profile.service.ts
+++ src/pages/delete-profile/delete-profile.service.ts
@@ -1,19 +1,18 @@
-import { post } from "../../core/http";
-
+import { post } from '../../core/http';
 
 export async function login(email: string, password: string) {
-    const body = {
-        email,
-        password
-    }
+  const body = {
+    email,
+    password,
+  };
 
-    return post('/auth/web/login', body).then(({ data }) => data);
+  return post('/auth/web/login', body).then(({ data }) => data);
 }
 
-export async function deleteAccount(reason: string) {
-    const body = {
-        reason
-    }
+export async function deleteAccount(reason: string = 'unknown') {
+  const body = {
+    reason,
+  };
 
-    return post('/user/delete', body).then(({ data }) => data);
+  return post('/user/delete', body).then(({ data }) => data);
 }
