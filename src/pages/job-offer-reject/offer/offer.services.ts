14e1bdf09f56ead95c2a8b71ed5a331790ee17be
Sajad Abbasi <s.abbasi@me.com>
2023-03-05 19:38:15 +0330

    fix: offer page validation

--- src/pages/job-offer-reject/offer/offer.services.ts
+++ src/pages/job-offer-reject/offer/offer.services.ts
@@ -1,6 +1,9 @@
-export const formInitialState = {
-  paymentType: '',
-  paymentMode: '',
-  estimatedTotalHours: '',
-  message: '',
+import { required } from '../../../core/form';
+import { FormModel } from '../../../core/form/useForm/useForm.types';
+
+export const formModel: FormModel = {
+//   paymentType: { initialValue: '', validators: [required()] },
+//   paymentMode: { initialValue: '', validators: [required()] },
+  estimatedTotalHours: { initialValue: '', validators: [required()] },
+  message: { initialValue: '', validators: [required()] },
 };
