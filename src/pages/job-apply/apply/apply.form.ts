48c690fa063bf6734540f68da1bfd3a1e2e15731
Sajad Abbasi <s.abbasi@me.com>
2023-03-05 14:19:36 +0330

    fix: add country to job apply page

--- src/pages/job-apply/apply/apply.form.ts
+++ src/pages/job-apply/apply/apply.form.ts
@@ -3,6 +3,6 @@ import { FormModel } from '../../../core/form/useForm/useForm.types';
 
 export const formModel: FormModel = {
   cover_letter: { initialValue: '', validators: [required()] },
-  cv_link: { initialValue: '' },
+  cv_link: { initialValue: '', validators: [required()] },
   cv_name: { initialValue: '' },
 };
