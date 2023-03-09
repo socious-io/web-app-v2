ccde904d4d4ddd192421e00615a62eda6925c45c
Sajad Abbasi <s.abbasi@me.com>
2023-03-09 14:48:15 +0330

    feat: add validations to apply page

--- src/pages/job-apply/apply/apply.form.ts
+++ src/pages/job-apply/apply/apply.form.ts
@@ -1,8 +1,21 @@
 import { required } from '../../../core/form';
+import { website } from '../../../core/form/customValidators/customValidators';
 import { FormModel } from '../../../core/form/useForm/useForm.types';
 
 export const formModel: FormModel = {
-  cover_letter: { initialValue: '', validators: [required()] },
-  cv_link: { initialValue: '' },
+  cover_letter: {
+    initialValue: '',
+    validators: [
+      {
+        name: 'bla',
+        message: 'blaaaa',
+        validateWith: (value: string) => {
+            console.log(value.trim().length > 0)
+          return value.trim().length > 0;
+        },
+      },
+    ],
+  },
+  cv_link: { initialValue: '', validators: [website()] },
   cv_name: { initialValue: '' },
 };
