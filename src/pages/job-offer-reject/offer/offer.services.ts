430792845fc9cbc51c5c9468b69011c02156739c
Sajad Abbasi <s.abbasi@me.com>
2023-03-10 10:49:39 +0330

    fix: validation on offer page

--- src/pages/job-offer-reject/offer/offer.services.ts
+++ src/pages/job-offer-reject/offer/offer.services.ts
@@ -1,9 +1,8 @@
 import { required } from '../../../core/form';
+import { noEmptyString, number } from '../../../core/form/customValidators/customValidators';
 import { FormModel } from '../../../core/form/useForm/useForm.types';
 
 export const formModel: FormModel = {
-  //   paymentType: { initialValue: '', validators: [required()] },
-  //   paymentMode: { initialValue: '', validators: [required()] },
-  estimatedTotalHours: { initialValue: '', validators: [required()] },
-  message: { initialValue: '', validators: [required()] },
+  estimatedTotalHours: { initialValue: '', validators: [required(), number()] },
+  message: { initialValue: '', validators: [noEmptyString()] },
 };
