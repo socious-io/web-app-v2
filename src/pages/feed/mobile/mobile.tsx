1bdd0b2603370065cb6f08a5a4fe609d3f4c25b7
Sajad Abbasi <s.abbasi@me.com>
2023-03-14 10:40:39 +0330

    fix: action-sheet positioning

--- src/pages/feed/mobile/mobile.tsx
+++ src/pages/feed/mobile/mobile.tsx
@@ -8,12 +8,13 @@ import css from './mobile.module.scss';
 import { FeedsMobileProps } from './mobile.types';
 import { getFeedList, like, unlike } from './mobile.service';
 import { Search } from '../../../components/atoms/search/search';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { IdentityReq } from '../../../core/types';
 import { RootState } from '../../../store/store';
 import { ActionSheet, ActionSheetButtonStyle } from '@capacitor/action-sheet';
 import { endpoint } from '../../../core/endpoints';
 import { dialog } from '../../../core/dialog/dialog';
+import { visibility } from '../../../store/reducers/menu.reducer';
 
 const showActions = async (id: string) => {
   const result = await ActionSheet.showActions({
@@ -41,12 +42,17 @@ export const Mobile = ({ list }: FeedsMobileProps) => {
   const [feedList, setFeedList] = useState(list.items);
   const [page, setPage] = useState(1);
   const totalCount = list.total_count;
+  const dispatch = useDispatch();
 
   function onMorePage() {
     getFeedList({ page: page + 1 }).then((resp) => {
       setPage((v) => v + 1);
       setFeedList((list) => [...list, ...resp.items]);
     });
+  }
+
+  function openSidebar() {
+    dispatch(visibility(true));
   }
 
   const onShowSeeMore = (length: number): boolean => {
@@ -96,7 +102,7 @@ export const Mobile = ({ list }: FeedsMobileProps) => {
     <div className={css.container}>
       <div className={css.header}>
         <div className={css.menu}>
-          <Avatar size="2.25rem" type={identity?.type} img={avatarImg} />
+          <Avatar onClick={openSidebar} size="2.25rem" type={identity?.type} img={avatarImg} />
           <Search placeholder="Search" onValueChange={onChangeSearch} />
           <div onClick={navigateToChat}>
             <img className={css.logo} src="icons/chat-white.svg" />
