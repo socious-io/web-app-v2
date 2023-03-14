6618da523c09d077ee9e50748cbb5ca872894c22
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2023-03-14 21:25:54 +0330

    feat: add job description card according to payment section

---
+++ src/components/templates/job-description-card/index.tsx
@@ -0,0 +1,40 @@
+import { Card } from "src/components/atoms/card/card";
+import { Categories } from "src/components/atoms/categories/categories";
+import { ProfileView } from "src/components/molecules/profile-view/profile-view";
+import css from "./job-description-card.module.scss";
+import { JobDescriptionCardProps } from "./job-description-card.types";
+
+export const JobDescrioptionCard: React.FC<JobDescriptionCardProps> = ({
+  job_title,
+  start_date,
+  end_date,
+  img,
+  type,
+  name,
+  location,
+  info_list,
+  containerClassName = "",
+}) => {
+  const createList = info_list.map((info) => (
+    <div className={css["job__info"]} key={info.name}>
+      <img src={`/icons/${info.icon}.svg`} />
+      {info.name}
+    </div>
+  ));
+
+  return (
+    <Card className={`${css["job"]} ${containerClassName}`}>
+      <span className={css["job__title"]}>{job_title}</span>
+      <div className={css["job__profile"]}>
+        <ProfileView img={img} type={type} name={name} location={location} />
+      </div>
+      <div>
+        {start_date} - {end_date}
+      </div>
+      <div className={css["job__footer"]}>
+        <Categories list={createList} />
+      </div>
+    </Card>
+  );
+};
+
