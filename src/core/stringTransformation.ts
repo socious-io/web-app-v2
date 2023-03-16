f24ec306b08641578a5130578a4290b6fef5d396
Sajad Abbasi <s.abbasi@me.com>
2023-03-16 18:00:11 +0330

    fix: markdown convertions

--- src/core/stringTransformation.ts
+++ src/core/stringTransformation.ts
@@ -2,7 +2,7 @@ export function convertSnakeCaseToLowerCase(value: string): string {
   try {
     return value.toLowerCase()?.replaceAll('_', ' ');
   } catch {
-    console.error('value could not be transform to lowercase');
+    console.warn('value could not be transform to lowercase');
     return '';
   }
 }
