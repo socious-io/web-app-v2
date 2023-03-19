0896b1898041c537d624341226707b0348dd2fb2
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2023-03-19 16:07:12 +0330

    refactor: redesign connected wallet style

--- src/components/templates/payment-methods/payment-methods.types.ts
+++ src/components/templates/payment-methods/payment-methods.types.ts
@@ -1,6 +1,6 @@
 import { ButtonProps } from "src/components/atoms/button/button.types";
 
 export interface PaymentMethodsProps {
-  payement_methods: { button: ButtonProps; connected_address?: string }[];
+  payement_methods: { button: ButtonProps; connected_address?: string, wallet_icon?: string }[];
   containerClassName?: string;
 }
