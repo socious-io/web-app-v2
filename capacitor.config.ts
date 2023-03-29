2438dc57bb09e99fdcd568ab0bea173ddbde5d3d
Sajad Abbasi <s.abbasi@me.com>
2023-03-29 13:27:31 +0330

    fix: android cors issue

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,22 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
+// hostname: 'capacitor',
+// hostname: 'android',
+// androidScheme: 'native',
+
 const config: CapacitorConfig = {
   appId: 'jp.socious.network',
   appName: 'Socious',
   webDir: 'dist',
   bundledWebRuntime: false,
   server: {
-    hostname: 'socious.io',
+    hostname: 'capacitor.native',
     androidScheme: 'https',
   },
+  //   server: {
+  //     hostname: 'capacitor',
+  //     // androidScheme: 'native',
+  //   },
   plugins: {
     CapacitorCookies: {
       enabled: true,
@@ -17,3 +25,4 @@ const config: CapacitorConfig = {
 };
 
 export default config;
+// ALLOWED_ORIGINS=socious.io,capacitor://socious.io,https://capacitor,native://socious.io,capacitor://localhost,capacitor.native,webapp2.socious.io
