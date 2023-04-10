5113f97083e7921a312c5b78cd3d0beaab8b11ed
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2023-04-10 19:50:56 +0330

    refactor(general): refactor payment methods according to adding new cards feature

--- src/components/templates/payment-methods/payment-methods.types.ts
+++ src/components/templates/payment-methods/payment-methods.types.ts
@@ -1,7 +1,12 @@
-import { ButtonProps } from "src/components/atoms/button/button.types";
+import { CardItems } from 'src/core/types';
 
 export interface PaymentMethodsProps {
   crypto_method: React.ReactNode;
   fiat_method?: React.ReactNode;
+  added_cards?: CardItems[];
+  selectedCard?: string;
+  onSelectCard?: (value: string) => void;
+  onEditCard?: (value: string) => void;
+  onRemoveCard?: (value: string) => void;
   containerClassName?: string;
 }
