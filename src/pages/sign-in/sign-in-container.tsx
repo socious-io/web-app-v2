f86a6ddec3d70b9b5ed9e37886805d21b2407bfa
Sajad Abbasi <s.abbasi@me.com>
2023-04-19 11:34:25 +0330

    feat: add login desktop version

--- src/pages/sign-in/sign-in-container.tsx
+++ src/pages/sign-in/sign-in-container.tsx
@@ -2,6 +2,6 @@ import { isTouchDevice } from '../../core/device-type-detector';
 import { Desktop } from './desktop/desktop';
 import { Mobile } from './mobile/mobile';
 
-export const SignIn = (): JSX.Element => {
+export const SignInContainer = (): JSX.Element => {
   return isTouchDevice() ? <Mobile /> : <Desktop />;
 };
