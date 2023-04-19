c6e63bb94b26f7a8c6de32cc78de71eaddc9f7a4
Sajad Abbasi <s.abbasi@me.com>
2023-04-19 11:34:25 +0330

    feat: add login desktop version

--- src/pages/sign-in/desktop/desktop.tsx
+++ src/pages/sign-in/desktop/desktop.tsx
@@ -1,40 +1,13 @@
 import css from './desktop.module.scss';
-import { useNavigate } from '@tanstack/react-location';
 import { Button } from '../../../components/atoms/button/button';
 import { Input } from '../../../components/atoms/input/input';
 import { Link } from '../../../components/atoms/link/link';
 import { Typography } from '../../../components/atoms/typography/typography';
 import { BottomStatic } from '../../../components/templates/bottom-static/bottom-static';
-import { endpoint } from '../../../core/endpoints';
-import { formModel } from '../sign-in.form';
-import { useForm } from '../../../core/form';
-import { getFormValues } from '../../../core/form/customValidators/formValues';
-import { LoginPayload } from '../sign-in.types';
-import { handleError } from '../../../core/http';
-import { getIdentities } from '../../../core/api';
-import { setIdentityList } from '../../../store/reducers/identity.reducer';
-import store from '../../../store/store';
-import { LoginResp } from 'src/core/types';
-import { setAuthCookies } from '../sign-in.services';
+import { useSignInShared } from '../sign-in.shared';
 
 export const Desktop = (): JSX.Element => {
-  const navigate = useNavigate();
-  const form = useForm(formModel);
-
-  async function onLoginSucceed(loginResp: LoginResp) {
-    await setAuthCookies(loginResp);
-    const resp = await getIdentities();
-    store.dispatch(setIdentityList(resp));
-    navigate({ to: '/jobs', replace: true });
-  }
-
-  async function onLogin() {
-    const formValues = getFormValues(form) as LoginPayload;
-    endpoint.post.auth
-      .login(formValues)
-      .then(onLoginSucceed)
-      .catch(handleError({ title: 'Login Failed' }));
-  }
+  const { onLogin, form, navigate } = useSignInShared();
 
   return (
     <BottomStatic>
