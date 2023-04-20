1e4756ff40651e24fb6d5c3960f851fdaf55e1f5
Sajad Abbasi <s.abbasi@me.com>
2023-04-20 18:16:29 +0330

    feat(registration): add desktop version of signUpUserComplete component

---
+++ src/pages/sign-up/sign-up-user-complete/mobile/mobile.tsx
@@ -0,0 +1,75 @@
+import css from './mobile.module.scss';
+import { Button } from '../../../../components/atoms/button/button';
+import { Link } from '../../../../components/atoms/link/link';
+import { Typography } from '../../../../components/atoms/typography/typography';
+import { BottomStatic } from '../../../../components/templates/bottom-static/bottom-static';
+import { Input } from '../../../../components/atoms/input/input';
+import { useSignUpUserCompleteShared } from '../sign-up-user-complete.shared';
+
+export const Mobile = (): JSX.Element => {
+  const shared = useSignUpUserCompleteShared();
+
+  return (
+    <BottomStatic>
+      <div className={css.top}>
+        <div className={css.header}>
+          <Typography marginBottom=".5rem" type="heading" size="l">
+            Complete your profile
+          </Typography>
+          <Typography color="var(--color-gray-01)" type="body">
+            What should we call you?
+          </Typography>
+        </div>
+        <form className={css.formContainer}>
+          <Input
+            register={shared.form}
+            name="firstName"
+            autoComplete="firstName"
+            label="Your First Name"
+            placeholder="First name"
+          />
+          <Input
+            register={shared.form}
+            name="lastName"
+            autoComplete="lastName"
+            label="Your Last Name"
+            placeholder="Last name"
+          />
+          <Input
+            register={shared.form}
+            name="password"
+            type="password"
+            label="Choose a Password"
+            autoComplete="new-password"
+            placeholder="Password"
+          />
+        </form>
+        {/* <div className={css.passwordQuality}>
+          <PasswordQuality value={form.controls.password.value} validators={passwordQualityValidators} />
+        </div> */}
+
+        <div className={css.passwordQuality}>
+          <Typography textAlign="center" paddingBottom="1rem">
+            By signing up, you agree to Socious'{' '}
+            <Link onClick={shared.navigateToTermsConditions}>Terms of Service</Link> and{' '}
+            <Link onClick={shared.navigateToPrivacyPolicy}>Privacy Policy</Link>
+          </Typography>
+        </div>
+      </div>
+      <div>
+        <div className={css.bottom}>
+          {/* <Button disabled={!basicValidity} onClick={onSubmit(formState)}>
+            Join
+          </Button> */}
+          <Button onClick={shared.onSubmit} disabled={!shared.form.isValid}>
+            Join
+          </Button>
+          <Typography marginTop="1rem">
+            <span>Already a member? </span>
+            <Link onClick={shared.navigateToSignIn}>Sign in</Link>
+          </Typography>
+        </div>
+      </div>
+    </BottomStatic>
+  );
+};
---
+++ src/pages/sign-up/sign-up-user-complete/sign-up-user-complete.shared.tsx
@@ -0,0 +1,56 @@
+import { changePasswordDirect } from './sign-up-user-complete.services';
+import { useForm } from '../../../core/form';
+import { formModel } from './sign-up-user-complete.form';
+import { handleError } from '../../../core/http';
+import { getIdentities } from '../../../core/api';
+import { updateProfile } from './sign-up-user.complete.services';
+import { useDispatch } from 'react-redux';
+import { setIdentityList } from '../../../store/reducers/identity.reducer';
+import { useNavigate } from '@tanstack/react-location';
+
+export const useSignUpUserCompleteShared = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const form = useForm(formModel);
+
+  async function setProfileName() {
+    const identities = await getIdentities();
+    dispatch(setIdentityList(identities));
+    const username = identities.find((identity) => identity.current)?.meta.username;
+
+    const payload = {
+      username,
+      firstName: form.controls.firstName.value,
+      lastName: form.controls.lastName.value,
+    };
+    updateProfile(payload);
+  }
+
+  async function onSubmit() {
+    const password = form.controls.password.value as string;
+    changePasswordDirect(password)
+      .then(setProfileName)
+      .then(() => navigate({ to: '/jobs' }))
+      .catch(handleError());
+  }
+
+  function navigateToTermsConditions() {
+    navigate({ to: '/terms-conditions' });
+  }
+
+  function navigateToPrivacyPolicy() {
+    navigate({ to: '/privacy-policy' });
+  }
+
+  function navigateToSignIn() {
+    navigate({ to: '/sign-in' });
+  }
+
+  return {
+    form,
+    onSubmit,
+    navigateToTermsConditions,
+    navigateToPrivacyPolicy,
+    navigateToSignIn,
+  };
+};
