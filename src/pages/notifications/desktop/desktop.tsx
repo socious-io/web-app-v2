0fb8f320463151b810e7070c645b6a2120c52d70
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2023-04-24 22:05:17 +0330

    refactor(notifications): add settings icon for navigate to notifications settings page

--- src/pages/notifications/desktop/desktop.tsx
+++ src/pages/notifications/desktop/desktop.tsx
@@ -1,13 +1,20 @@
-import { Card } from "../../../components/atoms/card/card";
-import { TwoColumnCursor } from "../../../components/templates/two-column-cursor/two-column-cursor";
+import { useNavigate } from '@tanstack/react-location';
+import { Card } from '../../../components/atoms/card/card';
+import { TwoColumnCursor } from '../../../components/templates/two-column-cursor/two-column-cursor';
+import css from './desktop.module.scss';
 
 export const Desktop = (): JSX.Element => {
-    return (
-        <TwoColumnCursor>
-            <Card>Notification</Card>
-            <Card>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet possimus magnam libero porro, aliquid doloremque velit sapiente aperiam maiores autem maxime. Rem, corporis veritatis. Id nobis at qui provident veniam.
-            </Card>
-        </TwoColumnCursor>
-    );
+  const navigate = useNavigate();
+  return (
+    <TwoColumnCursor>
+      <Card className={css.settings}>
+        Notifications
+        <img src="/icons/settings-black.svg" onClick={() => navigate({ to: 'settings' })} />
+      </Card>
+      <Card>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet possimus magnam libero porro, aliquid doloremque
+        velit sapiente aperiam maiores autem maxime. Rem, corporis veritatis. Id nobis at qui provident veniam.
+      </Card>
+    </TwoColumnCursor>
+  );
 };
