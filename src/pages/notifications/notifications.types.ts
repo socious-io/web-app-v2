c81545b8ea2c166a24e68b082de07fc940b8a0e7
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2023-04-25 16:41:32 +0330

    feat(notifications): web desktop for notifications page

---
+++ src/pages/notifications/notifications.types.ts
@@ -0,0 +1,4 @@
+import { Notifications } from 'src/components/organisms/notification-list/Notification-list.types';
+import { Pagination } from 'src/core/types';
+
+export type Resolver = Pagination<Notifications[]>;
