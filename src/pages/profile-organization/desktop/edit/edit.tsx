483a8d0177f6fd9f7e72d3404f360f36b6821eb7
Sajad Abbasi <s.abbasi@me.com>
2023-05-11 13:03:32 +0330

    feat(profile): add profile edit (org) desktop version

--- src/pages/profile-organization/desktop/edit/edit.tsx
+++ src/pages/profile-organization/desktop/edit/edit.tsx
@@ -6,23 +6,97 @@ import { Dropdown } from 'src/components/atoms/dropdown-v2/dropdown';
 import { COUNTRIES } from 'src/constants/COUNTRIES';
 import { COUNTRY_CODES } from 'src/constants/COUNTRY_CODE';
 import { Category } from 'src/components/molecules/category/category';
-import { skillsToCategoryAdaptor, socialCausesToCategoryAdaptor } from 'src/core/adaptors';
-import { ProfileReq } from 'src/pages/profile-organization/profile-organization.types';
-import { useMatch } from '@tanstack/react-location';
-import { useMemo } from 'react';
-import { useForm } from 'src/core/form/useForm/useForm';
-import { useProfileUserEditShared } from 'src/pages/profile-user-edit/profile-user-edit.shared';
-import { generateFormModel } from 'src/pages/profile-user-edit/profile-user-edit.form';
+import { socialCausesToCategoryAdaptor } from 'src/core/adaptors';
+import { useEffect } from 'react';
 import { Input } from 'src/components/atoms/input/input';
 import { EditProps } from './edit.types';
+import { useProfileOrganizationEditShared } from 'src/pages/profile-organization-edit/profile-organization-edit.shared';
+import { DropdownItem } from 'src/components/atoms/dropdown-v2/dropdown.types';
 import { getFormValues } from 'src/core/form/customValidators/formValues';
 import { endpoint } from 'src/core/endpoints';
 
 export const EditOrganization = (props: EditProps): JSX.Element => {
+  const { onAvatarEdit, onCoverEdit, avatarImage, coverImage, updateCityList, form, cities, organization } =
+    useProfileOrganizationEditShared();
+
+  useEffect(() => {
+    updateCityList(organization.country);
+  }, []);
+
+  function onCountryUpdate(option: DropdownItem) {
+    updateCityList(option.value as string);
+  }
+
+  function onSave() {
+    const payload = getFormValues(form);
+    endpoint.post.organizations['orgs/update/{org_id}'](organization.id, payload).then(() => {
+      props.onClose();
+    });
+  }
+
   return (
     <Modal height={props.height} width={props.width} open={props.open} onClose={props.onClose}>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore quibusdam sequi sint perferendis error laboriosam
-      autem quasi odio. Repudiandae quia eos incidunt maiores sunt debitis possimus modi quo earum temporibus!
+      <>
+        <div className={css.mainHeader}>
+          <Header onBack={props.onClose} title="Edit" right={{ label: 'Save', onClick: onSave }} />
+        </div>
+        <div>
+          <div>
+            <div className={css.header}>
+              <div className={css.coverImage} style={{ backgroundImage: `url(${coverImage})` }} />
+              <div className={css.photoIcon} onClick={onCoverEdit}>
+                <img src="/icons/photos-white.svg" />
+              </div>
+              <div className={css.profileImgContainer}>
+                <div className={css.photoIcon} onClick={onAvatarEdit}>
+                  <img src="/icons/photos-white.svg" />
+                </div>
+                <div className={css.profileImage} style={{ backgroundImage: `url(${avatarImage})` }} />
+              </div>
+            </div>
+          </div>
+          <div className={css.formContainer}>
+            {/* <Dropdown label='Organization type' list={ORGANIZATION_TYPE} /> */}
+            <Input label="Name" register={form} name="name" placeholder="name" />
+            <Textarea label="bio" register={form} name="bio" placeholder="bio" />
+            <Category
+              register={form}
+              name="social_causes"
+              label="Social causes"
+              list={socialCausesToCategoryAdaptor()}
+              placeholder="Social causes"
+            />
+            <Input label="Email" register={form} name="email" placeholder="email" />
+            <Dropdown
+              register={form}
+              label="Country"
+              name="country"
+              list={COUNTRIES}
+              placeholder="country"
+              onValueChange={onCountryUpdate}
+            />
+            <Dropdown
+              register={form}
+              label="City"
+              name="city"
+              list={cities}
+              placeholder="city"
+              onValueChange={(option) => form.controls.geoname_id.setValue(option.id)}
+            />
+            <Input label="Address" register={form} name="address" placeholder="address" />
+            <div>
+              <div className={css.label}>Phone</div>
+              <div className={css.phoneContainer}>
+                <Dropdown register={form} name="mobile_country_code" placeholder="+1" list={COUNTRY_CODES} />
+                <Input register={form} name="phone" placeholder="phone" />
+              </div>
+            </div>
+            <Input label="Website" register={form} name="website" placeholder="http://website.com" />
+            <Textarea label="Mission" register={form} name="mission" placeholder="mission" />
+            <Textarea label="Culture" register={form} name="culture" placeholder="culture" />
+          </div>
+        </div>
+      </>
     </Modal>
   );
 };
