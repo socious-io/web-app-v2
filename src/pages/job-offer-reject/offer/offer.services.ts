1b7b129e90534a6c3e2533b29a42762096b26c24
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2023-05-16 15:22:55 +0330

    hotfix(offer): accept float number in amount and hour input field

--- src/pages/job-offer-reject/offer/offer.services.ts
+++ src/pages/job-offer-reject/offer/offer.services.ts
@@ -1,15 +1,16 @@
-import { min, required } from '../../../core/form';
+import { min, pattern, required } from '../../../core/form';
 import { noEmptyString, number } from '../../../core/form/customValidators/customValidators';
 import { FormModel } from '../../../core/form/useForm/useForm.types';
 
 export const formModel = (isPaid: boolean, isFiat: boolean): FormModel => {
+  const patternNumber = pattern('patternName', /^[+-]?\d+(\.\d+)?$/, 'value should be a number');
   const assignTotalPaid = isFiat
-    ? [required(), number(), min(22, 'Amount for fiat mode should not be less than 22$')]
-    : [required(), number()];
-  const assignTotalValidators = isPaid ? assignTotalPaid : [number()];
+    ? [required(), min(22, 'Amount for fiat mode should not be less than 22$'), patternNumber]
+    : [required(), patternNumber];
+  const assignTotalValidators = isPaid ? assignTotalPaid : [patternNumber];
   return {
     assignmentTotal: { initialValue: '', validators: assignTotalValidators },
-    estimatedTotalHours: { initialValue: '', validators: [required(), number()] },
+    estimatedTotalHours: { initialValue: '', validators: [required(), patternNumber] },
     message: { initialValue: '', validators: [noEmptyString()] },
   };
 };
