33b633f4faf58559718e7181d6cfcf670c6a27af
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2023-05-23 23:45:30 +0330

    feat(general): expanable component for long texts via see more button

---
+++ src/components/atoms/expandable-text/index.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+import { printWhen } from 'src/core/utils';
+import { TextClickableURLs } from '../text-clickable-urls';
+import { ExpandableTextProps } from './expandable-text.types';
+import css from './expandable-text.module.scss';
+
+export const ExpandableText: React.FC<ExpandableTextProps> = ({
+  text,
+  expectedLength = 200,
+  clickableUrls = true,
+  className,
+}) => {
+  const [maintext, setMainText] = useState(text);
+  const expect = text.slice(0, expectedLength);
+  const viewMoreCondition = expect.length < text.length;
+  const [shouldViewMore, setShouldViewMore] = useState(viewMoreCondition);
+
+  const toggleExpect = (): void => {
+    setMainText(text);
+    setShouldViewMore(!shouldViewMore);
+  };
+
+  useEffect(() => {
+    setShouldViewMore(viewMoreCondition);
+    setMainText(expect);
+  }, [text]);
+
+  return (
+    <div className={css['expect']}>
+      {clickableUrls ? <TextClickableURLs text={maintext} /> : maintext}
+      {printWhen(<>... </>, maintext.length < text.length)}
+      {printWhen(
+        <span className={css['expect__seeMore']} onClick={toggleExpect}>
+          See more
+        </span>,
+        shouldViewMore
+      )}
+    </div>
+  );
+};
