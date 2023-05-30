019c8dbca35054e922ad5a0d67501c5d29974f23
jeyem <me@e-mahmoudi.me>
2023-05-30 13:50:12 +0330

    fix withrawn abi and func calling

--- src/dapp/dapp.config.ts
+++ src/dapp/dapp.config.ts
@@ -116,11 +116,6 @@ export const dappConfig: DappConfig = {
   abis: {
     escrow: [
       {
-        inputs: [],
-        stateMutability: 'nonpayable',
-        type: 'constructor',
-      },
-      {
         anonymous: false,
         inputs: [
           {
@@ -241,6 +236,11 @@ export const dappConfig: DappConfig = {
             type: 'uint256',
           },
           {
+            internalType: 'bool',
+            name: '_verifiedOrg',
+            type: 'bool',
+          },
+          {
             internalType: 'enum Escrow.EscrowStatus',
             name: '_status',
             type: 'uint8',
@@ -284,6 +284,202 @@ export const dappConfig: DappConfig = {
       {
         inputs: [
           {
+            internalType: 'uint256',
+            name: '_escrowId',
+            type: 'uint256',
+          },
+          {
+            internalType: 'bool',
+            name: '_refund',
+            type: 'bool',
+          },
+        ],
+        name: 'escrowDecision',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function',
+      },
+      {
+        inputs: [
+          {
+            internalType: 'address',
+            name: '_contributor',
+            type: 'address',
+          },
+          {
+            internalType: 'string',
+            name: '_jobId',
+            type: 'string',
+          },
+          {
+            internalType: 'uint256',
+            name: '_amount',
+            type: 'uint256',
+          },
+          {
+            internalType: 'bool',
+            name: '_verifiedOrg',
+            type: 'bool',
+          },
+          {
+            internalType: 'contract IERC20',
+            name: '_token',
+            type: 'address',
+          },
+        ],
+        name: 'newEscrow',
+        outputs: [
+          {
+            internalType: 'uint256',
+            name: '',
+            type: 'uint256',
+          },
+        ],
+        stateMutability: 'nonpayable',
+        type: 'function',
+      },
+      {
+        inputs: [],
+        name: 'renounceOwnership',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function',
+      },
+      {
+        inputs: [
+          {
+            internalType: 'uint256',
+            name: '_escrowId',
+            type: 'uint256',
+          },
+          {
+            internalType: 'address',
+            name: '_contributor',
+            type: 'address',
+          },
+        ],
+        name: 'setContributor',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function',
+      },
+      {
+        inputs: [
+          {
+            internalType: 'uint256',
+            name: '_newFee',
+            type: 'uint256',
+          },
+        ],
+        name: 'setDecisionRetentionFee',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function',
+      },
+      {
+        inputs: [
+          {
+            internalType: 'uint256',
+            name: '_newFee',
+            type: 'uint256',
+          },
+        ],
+        name: 'setImpactContFee',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function',
+      },
+      {
+        inputs: [
+          {
+            internalType: 'uint256',
+            name: '_newFee',
+            type: 'uint256',
+          },
+        ],
+        name: 'setImpactOrgFee',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function',
+      },
+      {
+        inputs: [
+          {
+            internalType: 'uint256',
+            name: '_newFee',
+            type: 'uint256',
+          },
+        ],
+        name: 'setNoImpactContFee',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function',
+      },
+      {
+        inputs: [
+          {
+            internalType: 'uint256',
+            name: '_newFee',
+            type: 'uint256',
+          },
+        ],
+        name: 'setNoImpactOrgFee',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function',
+      },
+      {
+        inputs: [
+          {
+            internalType: 'address',
+            name: 'destination',
+            type: 'address',
+          },
+          {
+            internalType: 'contract IERC20',
+            name: '_token',
+            type: 'address',
+          },
+        ],
+        name: 'transferAssets',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function',
+      },
+      {
+        inputs: [
+          {
+            internalType: 'address',
+            name: 'newOwner',
+            type: 'address',
+          },
+        ],
+        name: 'transferOwnership',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function',
+      },
+      {
+        inputs: [],
+        stateMutability: 'nonpayable',
+        type: 'constructor',
+      },
+      {
+        inputs: [
+          {
+            internalType: 'uint256',
+            name: '_escrowId',
+            type: 'uint256',
+          },
+        ],
+        name: 'withdrawn',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function',
+      },
+      {
+        inputs: [
+          {
             internalType: 'contract IERC20',
             name: '_token',
             type: 'address',
@@ -298,29 +494,6 @@ export const dappConfig: DappConfig = {
           },
         ],
         stateMutability: 'view',
-        type: 'function',
-      },
-      {
-        inputs: [
-          {
-            internalType: 'uint256',
-            name: '_escrowId',
-            type: 'uint256',
-          },
-          {
-            internalType: 'bool',
-            name: '_refund',
-            type: 'bool',
-          },
-          {
-            internalType: 'bool',
-            name: '_verifiedContributer',
-            type: 'bool',
-          },
-        ],
-        name: 'escrowDecision',
-        outputs: [],
-        stateMutability: 'nonpayable',
         type: 'function',
       },
       {
@@ -357,6 +530,11 @@ export const dappConfig: DappConfig = {
             internalType: 'uint256',
             name: 'fee',
             type: 'uint256',
+          },
+          {
+            internalType: 'bool',
+            name: 'verifiedOrg',
+            type: 'bool',
           },
           {
             internalType: 'enum Escrow.EscrowStatus',
@@ -421,6 +599,11 @@ export const dappConfig: DappConfig = {
                 internalType: 'uint256',
                 name: 'fee',
                 type: 'uint256',
+              },
+              {
+                internalType: 'bool',
+                name: 'verifiedOrg',
+                type: 'bool',
               },
               {
                 internalType: 'enum Escrow.EscrowStatus',
@@ -541,45 +724,6 @@ export const dappConfig: DappConfig = {
         type: 'function',
       },
       {
-        inputs: [
-          {
-            internalType: 'address',
-            name: '_contributor',
-            type: 'address',
-          },
-          {
-            internalType: 'string',
-            name: '_jobId',
-            type: 'string',
-          },
-          {
-            internalType: 'uint256',
-            name: '_amount',
-            type: 'uint256',
-          },
-          {
-            internalType: 'bool',
-            name: '_verifiedOrg',
-            type: 'bool',
-          },
-          {
-            internalType: 'contract IERC20',
-            name: '_token',
-            type: 'address',
-          },
-        ],
-        name: 'newEscrow',
-        outputs: [
-          {
-            internalType: 'uint256',
-            name: '',
-            type: 'uint256',
-          },
-        ],
-        stateMutability: 'nonpayable',
-        type: 'function',
-      },
-      {
         inputs: [],
         name: 'owner',
         outputs: [
@@ -590,96 +734,6 @@ export const dappConfig: DappConfig = {
           },
         ],
         stateMutability: 'view',
-        type: 'function',
-      },
-      {
-        inputs: [],
-        name: 'renounceOwnership',
-        outputs: [],
-        stateMutability: 'nonpayable',
-        type: 'function',
-      },
-      {
-        inputs: [
-          {
-            internalType: 'uint256',
-            name: '_escrowId',
-            type: 'uint256',
-          },
-          {
-            internalType: 'address',
-            name: '_contributor',
-            type: 'address',
-          },
-        ],
-        name: 'setContributor',
-        outputs: [],
-        stateMutability: 'nonpayable',
-        type: 'function',
-      },
-      {
-        inputs: [
-          {
-            internalType: 'uint256',
-            name: '_newFee',
-            type: 'uint256',
-          },
-        ],
-        name: 'setDecisionRetentionFee',
-        outputs: [],
-        stateMutability: 'nonpayable',
-        type: 'function',
-      },
-      {
-        inputs: [
-          {
-            internalType: 'uint256',
-            name: '_newFee',
-            type: 'uint256',
-          },
-        ],
-        name: 'setImpactContFee',
-        outputs: [],
-        stateMutability: 'nonpayable',
-        type: 'function',
-      },
-      {
-        inputs: [
-          {
-            internalType: 'uint256',
-            name: '_newFee',
-            type: 'uint256',
-          },
-        ],
-        name: 'setImpactOrgFee',
-        outputs: [],
-        stateMutability: 'nonpayable',
-        type: 'function',
-      },
-      {
-        inputs: [
-          {
-            internalType: 'uint256',
-            name: '_newFee',
-            type: 'uint256',
-          },
-        ],
-        name: 'setNoImpactContFee',
-        outputs: [],
-        stateMutability: 'nonpayable',
-        type: 'function',
-      },
-      {
-        inputs: [
-          {
-            internalType: 'uint256',
-            name: '_newFee',
-            type: 'uint256',
-          },
-        ],
-        name: 'setNoImpactOrgFee',
-        outputs: [],
-        stateMutability: 'nonpayable',
         type: 'function',
       },
       {
@@ -719,37 +773,6 @@ export const dappConfig: DappConfig = {
       {
         inputs: [
           {
-            internalType: 'address',
-            name: 'destination',
-            type: 'address',
-          },
-          {
-            internalType: 'contract IERC20',
-            name: '_token',
-            type: 'address',
-          },
-        ],
-        name: 'transferAssets',
-        outputs: [],
-        stateMutability: 'nonpayable',
-        type: 'function',
-      },
-      {
-        inputs: [
-          {
-            internalType: 'address',
-            name: 'newOwner',
-            type: 'address',
-          },
-        ],
-        name: 'transferOwnership',
-        outputs: [],
-        stateMutability: 'nonpayable',
-        type: 'function',
-      },
-      {
-        inputs: [
-          {
             internalType: 'uint256',
             name: '',
             type: 'uint256',
@@ -777,24 +800,6 @@ export const dappConfig: DappConfig = {
           },
         ],
         stateMutability: 'view',
-        type: 'function',
-      },
-      {
-        inputs: [
-          {
-            internalType: 'uint256',
-            name: '_escrowId',
-            type: 'uint256',
-          },
-          {
-            internalType: 'bool',
-            name: '_verifiedContributer',
-            type: 'bool',
-          },
-        ],
-        name: 'withdrawn',
-        outputs: [],
-        stateMutability: 'nonpayable',
         type: 'function',
       },
     ],
