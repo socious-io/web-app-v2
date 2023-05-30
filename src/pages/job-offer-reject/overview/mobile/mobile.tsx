312acfe67485b3a4809b0ed9ab4f73362531e3c8
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2023-05-30 23:18:23 +0330

    fix(hired): after confirm prevnt to history back

--- src/pages/job-offer-reject/overview/mobile/mobile.tsx
+++ src/pages/job-offer-reject/overview/mobile/mobile.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useMatch, useNavigate } from '@tanstack/react-location';
 import { TopFixedMobile } from 'src/components/templates/top-fixed-mobile/top-fixed-mobile';
 import { Header } from 'src/components/atoms/header-v2/header';
@@ -7,24 +8,37 @@ import { Hired } from '../components/hired/hired';
 import { Offered } from '../components/offered/offered';
 import { Overview } from '../components/overview/overview';
 import { Loader } from '../../job-offer-reject.types';
+import { jobOfferRejectLoader } from '../../job-offer-reject.services';
 import css from './mobile.module.scss';
 
 export const Mobile = (): JSX.Element => {
   const resolver = useMatch().ownData as Loader;
+  const { id } = useMatch().params || {};
   const navigate = useNavigate();
+  const [updatedApplicantList, setUpdatedApplicantList] = useState<Loader>(resolver);
+
+  async function updateApplicantList() {
+    const result = await jobOfferRejectLoader({ params: { id } });
+    setUpdatedApplicantList(result);
+  }
 
   const tabs = [
     {
       name: 'Overview',
-      content: <Overview questions={resolver.screeningQuestions.questions} data={resolver.jobOverview} />,
+      content: (
+        <Overview
+          questions={updatedApplicantList.screeningQuestions.questions}
+          data={updatedApplicantList.jobOverview}
+        />
+      ),
       default: true,
     },
     {
       name: 'Applicants',
       content: (
         <Applicants
-          toReviewList={resolver.reviewList}
-          declinedList={resolver.declinedList}
+          toReviewList={updatedApplicantList.reviewList}
+          declinedList={updatedApplicantList.declinedList}
           onOfferClick={(id) => navigate({ to: `./${id}/offer` })}
         />
       ),
@@ -32,12 +46,23 @@ export const Mobile = (): JSX.Element => {
     {
       name: 'Offered',
       content: (
-        <Offered sent={resolver.sent} approved={resolver.approved} hired={resolver.hired} closed={resolver.closed} />
+        <Offered
+          sent={updatedApplicantList.sent}
+          approved={updatedApplicantList.approved}
+          hired={updatedApplicantList.hired}
+          closed={updatedApplicantList.closed}
+        />
       ),
     },
     {
       name: 'Hired',
-      content: <Hired hiredList={resolver.hiredList} endHiredList={resolver.endHiredList} />,
+      content: (
+        <Hired
+          hiredList={updatedApplicantList.hiredList}
+          endHiredList={updatedApplicantList.endHiredList}
+          onDone={updateApplicantList}
+        />
+      ),
     },
   ];
 
@@ -45,8 +70,8 @@ export const Mobile = (): JSX.Element => {
     <TopFixedMobile>
       <Header
         removeBorder
-        onBack={() => navigate({ to: `/jobs/created/${resolver.jobOverview.identity_id}` })}
-        title={resolver.jobOverview.title}
+        onBack={() => navigate({ to: `/jobs/created/${updatedApplicantList.jobOverview.identity_id}` })}
+        title={updatedApplicantList.jobOverview.title}
       />
       <div className={css.tabContainer}>
         <Tabs tabs={tabs} />
