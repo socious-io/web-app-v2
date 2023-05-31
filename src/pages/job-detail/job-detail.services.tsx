c306f45de35a2d6c32577d8dbf68491bf7675947
Sajad Abbasi <s.abbasi@me.com>
2023-05-31 09:06:44 +0330

    feat: add experience level to job detail

--- src/pages/job-detail/job-detail.services.tsx
+++ src/pages/job-detail/job-detail.services.tsx
@@ -4,21 +4,31 @@ import { translateProjectType } from 'src/constants/PROJECT_TYPES';
 import { translateProjectLength } from 'src/constants/PROJECT_LENGTH';
 import { when } from 'src/core/utils';
 import { translateRemotePreferences } from 'src/constants/PROJECT_REMOTE_PREFERENCE';
+import { getCountryByShortname } from 'src/constants/COUNTRIES';
+import { translateExperienceLevel } from 'src/constants/EXPERIENCE_LEVEL';
 
 export function getCategories(job: Job): Array<JSX.Element | string> {
+  console.log({ job });
+
+  const t = translateExperienceLevel(job.experience_level);
+  console.log({ t });
+
   const list: Array<JSX.Element | string> = [];
+
   const location = () => (
     <>
       <img style={{ marginRight: '2px' }} src="/icons/pin.svg" />
-      {job.city}, {job.country}
+      {job.city}, {getCountryByShortname(job.country)}
     </>
   );
+
   const projectType = () => (
     <>
       <img style={{ marginRight: '2px' }} src="/icons/part-time.svg" />
       {translateProjectType(job.project_type)}
     </>
   );
+
   const projectLength = () => (
     <>
       <img style={{ marginRight: '2px' }} src="/icons/time.svg" />
@@ -26,10 +36,13 @@ export function getCategories(job: Job): Array<JSX.Element | string> {
     </>
   );
 
+  const experienceLevel = () => <>{translateExperienceLevel(job.experience_level)}</>;
+
   when(job.country, () => list.push(location()));
   when(job.project_type, () => list.push(projectType()));
   when(job.project_length, () => list.push(projectLength()));
   when(job.remote_preference, () => list.push(translateRemotePreferences(job.remote_preference)));
+  when(job.experience_level !== undefined, () => list.push(experienceLevel()));
 
   return list;
 }
