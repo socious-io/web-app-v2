bd2781143a1415b355f6b123c03707192461cd64
Sajad Abbasi <s.abbasi@me.com>
2023-06-01 09:41:17 +0330

    fix: change password on desktop (#1037)

---
+++ change-log.ts
@@ -0,0 +1,12 @@
+import { defineConfig, loadEnv } from 'vite';
+console.log('evn: ',defineConfig );
+
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+  console.log('evn: ', env);
+  return {
+    // vite config
+  };
+});
