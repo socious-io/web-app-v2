72b463711e1273fe0e2c7a78847473b3e22fc92c
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2023-06-13 00:49:39 +0330

    refactor(general): add className props for fab component

--- src/components/atoms/fab/fab.tsx
+++ src/components/atoms/fab/fab.tsx
@@ -3,7 +3,7 @@ import { FabProps } from './fab.types';
 
 export const Fab = (props: FabProps): JSX.Element => {
   return (
-    <div onClick={props.onClick} className={css.container}>
+    <div onClick={props.onClick} className={`${css.container} ${props.className}`}>
       <img height={24} src="/icons/plus.svg" />
     </div>
   );
