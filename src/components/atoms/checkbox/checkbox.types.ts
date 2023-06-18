db6b96610cfe11d76b657c76597c95fc58489695
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2023-06-18 16:56:23 +0330

    fix(ENG-1072): agreement on create profile organization issue

--- src/components/atoms/checkbox/checkbox.types.ts
+++ src/components/atoms/checkbox/checkbox.types.ts
@@ -1,8 +1,12 @@
+import { FormGroup } from "src/core/form/useForm/useForm.types";
+
 export type CheckboxProps = {
   label: string;
   id: string;
+  name?: string;
   checked?: boolean;
   disabled?: boolean;
   defaultChecked?: boolean;
   onChange?: (value: boolean) => void;
+  register?: Required<FormGroup>;
 };
