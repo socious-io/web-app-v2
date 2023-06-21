818e2a15d5ec6e1e94ba69cff85b1e0277b3f42f
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2023-06-22 00:09:07 +0330

    refactor(general): optional label in radio group and className props

--- src/components/molecules/radio-group/radio-group.tsx
+++ src/components/molecules/radio-group/radio-group.tsx
@@ -1,14 +1,16 @@
 import css from './radio-group.module.scss';
 import { Radio } from '../../atoms/radio/radio';
 import { RadioGroupProps } from './radio-group.types';
+import { printWhen } from 'src/core/utils';
 
 export const RadioGroup = (props: RadioGroupProps): JSX.Element => {
   return (
-    <div className={css.container}>
-      <div className={css.label}>{props.label}</div>
+    <div className={`${css.container} ${props.containerClassName}`}>
+      {printWhen(<div className={css.label}>{props.label}</div>, !!props.label)}
       {props.list.map((radio) => {
         return (
-          <div key={radio.value} className={css.radioItem}>
+          <div key={radio.value} className={`${css.radioItem} ${props.className}`}>
+            {printWhen(radio.icon, !!radio.icon)}
             <Radio
               name={props.name}
               onChange={() => props.onChange(radio.value)}
