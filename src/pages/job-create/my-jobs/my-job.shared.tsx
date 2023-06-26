14ca9dcf5d76cda78b4d7f6aa9fa20a2f9bd4860
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2023-06-26 23:25:21 +0330

    feat(job-created): archived job list

--- src/pages/job-create/my-jobs/my-job.shared.tsx
+++ src/pages/job-create/my-jobs/my-job.shared.tsx
@@ -1,18 +1,21 @@
 import { useState } from 'react';
 import { useMatch, useNavigate } from '@tanstack/react-location';
-import { getActiveJobs, getDraftJobs, jobListToJobCardListAdaptor } from './my-jobs.services';
+import { getActiveJobs, getArchivedJobs, getDraftJobs, jobListToJobCardListAdaptor } from './my-jobs.services';
 import { MyJobsResolver } from './my-jobs.types';
 
 export const useMyJobShared = () => {
   const navigate = useNavigate();
   const resolver = useMatch();
-  const { activeJobs, draftJobs } = resolver.ownData as MyJobsResolver;
+  const { activeJobs, draftJobs, archivedJobs } = resolver.ownData as MyJobsResolver;
   const onGoingTitle = `On-Going (${activeJobs.total_count})`;
   const draftTitle = `Drafts (${draftJobs.total_count})`;
+  const archivedTitle = `Archived (${archivedJobs.total_count})`;
   const adoptedJobList = jobListToJobCardListAdaptor(activeJobs.items);
   const adoptedDraftList = jobListToJobCardListAdaptor(draftJobs.items);
+  const adoptedArchivedList = jobListToJobCardListAdaptor(archivedJobs.items);
   const [activeJobList, setActiveJobList] = useState({ ...activeJobs, items: adoptedJobList });
   const [draftJobList, setDraftJobList] = useState({ ...draftJobs, items: adoptedDraftList });
+  const [archivedJobList, setArchivedJobList] = useState({ ...archivedJobs, items: adoptedArchivedList });
 
   async function updateActiveJobList() {
     const identityId = resolver.params.id;
@@ -40,6 +43,19 @@ export const useMyJobShared = () => {
       .then(setDraftJobList);
   }
 
+  async function updateArchivedJobList() {
+    const identityId = resolver.params.id;
+    const payload = { identityId, page: archivedJobList.page + 1 };
+    getArchivedJobs(payload)
+      .then(({ items }) => ({
+        items: [...archivedJobList.items, ...jobListToJobCardListAdaptor(items)],
+        page: payload.page,
+        limit: archivedJobs.limit,
+        total_count: archivedJobs.total_count,
+      }))
+      .then(setArchivedJobList);
+  }
+
   function navigateToOverview(id?: string) {
     navigate({ to: `../${id}/overview` });
   }
@@ -55,5 +71,9 @@ export const useMyJobShared = () => {
     draftJobList,
     draftJobs,
     updateDraftJobList,
+    archivedTitle,
+    archivedJobs,
+    archivedJobList,
+    updateArchivedJobList,
   };
 };
