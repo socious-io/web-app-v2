a8cd8fcf12b1d9cee22095e1ca42f508f815137e
Sajad Abbasi <s.abbasi@me.com>
2023-07-04 14:05:35 +0330

    cicd: add branching to amplify.yml

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.scss';
 
+console.log('6.1.0')
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
