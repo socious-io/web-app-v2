2e35adf171162e33d528cd642fad979321ca1044
Sajad Abbasi <s.abbasi@me.com>
2023-07-10 11:22:45 +0330

    fix: org-create-culture links and navigations (#1136)

--- src/pages/organization-create/culture/desktop/desktop.tsx
+++ src/pages/organization-create/culture/desktop/desktop.tsx
@@ -1,31 +1,39 @@
-import { useNavigate } from '@tanstack/react-location';
 import { Button } from '../../../../components/atoms/button/button';
 import { Card } from '../../../../components/atoms/card/card';
 import { Steps } from '../../../../components/atoms/steps/steps';
 import { Textarea } from '../../../../components/atoms/textarea/textarea';
 import css from './desktop.module.scss';
+import { required, useForm } from '../../../../core/form';
+import { useOrganizationCreateShared } from '../../organization-create.shared';
 
 export const Desktop = (): JSX.Element => {
-  const navigate = useNavigate();
+  const { cultureValue, updateCulture, navigateToMission, onCultureSkip, submitForm } = useOrganizationCreateShared();
+
+  const form = useForm({ culture: { initialValue: cultureValue, validators: [required()] } });
+  form.controls.culture.subscribe(updateCulture);
 
   return (
     <div className={css.container}>
       <Card padding="0" className={css.card}>
         <div className={css.header}>
-          <div className={css.chevron} onClick={() => navigate({ to: '../mission' })}>
+          <div className={css.chevron} onClick={navigateToMission}>
             <img height={24} src="/icons/chevron-left.svg" />
           </div>
           <div className={css.stepsContainer}>
             <Steps clickable={false} length={6} current={5} />
           </div>
-          <div className={css.skip}>Skip</div>
+          <div onClick={onCultureSkip} className={css.skip}>
+            Skip
+          </div>
         </div>
         <div className={css.question}>Tell us about your organization's culture.</div>
         <div className={css.main}>
-          <Textarea variant="outline" placeholder="Your organization's culture" />
+          <Textarea placeholder="Your organization's culture" />
         </div>
         <div className={css.bottom}>
-          <Button onClick={() => navigate({ to: '../social-impact' })}>Continue</Button>
+          <Button disabled={!cultureValue} onClick={submitForm}>
+            Continue
+          </Button>
         </div>
       </Card>
     </div>
--- src/pages/organization-create/culture/mobile/mobile.tsx
+++ src/pages/organization-create/culture/mobile/mobile.tsx
@@ -1,61 +1,26 @@
-import { useNavigate } from '@tanstack/react-location';
-import { useDispatch, useSelector } from 'react-redux';
-import { CreateOrgWizard, setCulture } from '../../../../store/reducers/createOrgWizard.reducer';
-import { RootState } from '../../../../store/store';
 import { Button } from '../../../../components/atoms/button/button';
 import { Steps } from '../../../../components/atoms/steps/steps';
 import { Textarea } from '../../../../components/atoms/textarea/textarea';
-import { addOrganization, wizardFormToPayloadAdaptor } from '../../organization-create';
 import css from './mobile.module.scss';
 import { required, useForm } from '../../../../core/form';
-import { handleError } from '../../../../core/http';
-import { getIdentities } from '../../../../core/api';
-import { setIdentityList } from '../../../../store/reducers/identity.reducer';
+import { useOrganizationCreateShared } from '../../organization-create.shared';
 
 export const Mobile = (): JSX.Element => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  const cultureValue = useSelector<RootState, string>((state) => {
-    return state.createOrgWizard?.culture || '';
-  });
-
-  const formState = useSelector<RootState, CreateOrgWizard>((state) => {
-    return state.createOrgWizard;
-  });
+  const { cultureValue, updateCulture, navigateToMission, onCultureSkip, submitForm } = useOrganizationCreateShared();
 
   const form = useForm({ culture: { initialValue: cultureValue, validators: [required()] } });
-  form.controls.culture.subscribe((v) => dispatch(setCulture(v)));
-
-  function navigateToSuccess() {
-    navigate({ to: '../succeed' });
-  }
-
-  function onSkip() {
-    dispatch(setCulture(''));
-    submitOrganization(formState);
-  }
-
-  async function updateIdentityList() {
-    const identities = await getIdentities();
-    dispatch(setIdentityList(identities));
-  }
-
-  function submitOrganization(wizardForm: CreateOrgWizard) {
-    const payload = wizardFormToPayloadAdaptor(wizardForm);
-    addOrganization(payload).then(navigateToSuccess).then(updateIdentityList).catch(handleError());
-  }
+  form.controls.culture.subscribe(updateCulture);
 
   return (
     <div className={css.container}>
       <div className={css.header}>
-        <div className={css.chevron} onClick={() => navigate({ to: '../mission' })}>
+        <div className={css.chevron} onClick={navigateToMission}>
           <img height={24} src="/icons/chevron-left.svg" />
         </div>
         <div className={css.stepsContainer}>
           <Steps clickable={false} length={6} current={5} />
         </div>
-        <div onClick={onSkip} className={css.skip}>
+        <div onClick={onCultureSkip} className={css.skip}>
           Skip
         </div>
       </div>
@@ -64,7 +29,7 @@ export const Mobile = (): JSX.Element => {
         <Textarea name="culture" register={form} placeholder="Your organization's culture" />
       </div>
       <div className={css.bottom}>
-        <Button disabled={!cultureValue} onClick={() => submitOrganization(formState)}>
+        <Button disabled={!cultureValue} onClick={submitForm}>
           Continue
         </Button>
       </div>
