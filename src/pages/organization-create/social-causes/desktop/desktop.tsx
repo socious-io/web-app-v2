71dfdff452dd21581509d7d4bc360da7c178881a
Sajad Abbasi <s.abbasi@me.com>
2023-07-10 14:26:02 +0330

    fix: social causes search of create org (#1136)

--- src/pages/organization-create/social-causes/desktop/desktop.tsx
+++ src/pages/organization-create/social-causes/desktop/desktop.tsx
@@ -14,7 +14,7 @@ export const Desktop = (): JSX.Element => {
   const [list, setList] = useState(SOCIAL_CAUSES);
 
   function onSearch(value: string) {
-    const filtered = SOCIAL_CAUSES.filter((item) => item.label.toLowerCase().includes(value));
+    const filtered = SOCIAL_CAUSES.filter((item) => item.label.toLowerCase().includes(value.toLowerCase()));
     setList(filtered);
   }
 
