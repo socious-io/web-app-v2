cdbea3669f935d3fb12042e027b471ac49293bfc
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2023-07-11 16:21:44 +0330

    fix(my-jobs): add payment type and payment scheme in job card - item 1 (#1149)

--- src/components/molecules/job-card/job-card.tsx
+++ src/components/molecules/job-card/job-card.tsx
@@ -14,7 +14,10 @@ export const JobCard = (props: JobCardProps): JSX.Element => {
 
   return (
     <div className={css.container}>
-      <div className={css.title}>{props.title}</div>
+      <div className={css.title}>
+        {props.title}
+        {printWhen(<> ({props.type})</>, !!props.type)}
+      </div>
       <div className={css.body}>{mainContent()}</div>
       <div className={css.date}>{props.date}</div>
       {printWhen(<div className={css.bottomRight}>{props.bottomRight}</div>, !!props.bottomRight)}
