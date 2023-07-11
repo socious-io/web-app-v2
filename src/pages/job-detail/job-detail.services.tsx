bc29257c06846eb73da86ef9d24a65cf289c9954
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2023-07-11 16:31:39 +0330

    fix(job-detail): add payment type and range - item 3 (#1149)

--- src/pages/job-detail/job-detail.services.tsx
+++ src/pages/job-detail/job-detail.services.tsx
@@ -1,11 +1,12 @@
-import { get } from '../../core/http';
-import { Job } from '../../components/organisms/job-list/job-list.types';
+import { getCountryByShortname } from 'src/constants/COUNTRIES';
 import { translateProjectType } from 'src/constants/PROJECT_TYPES';
 import { translateProjectLength } from 'src/constants/PROJECT_LENGTH';
-import { when } from 'src/core/utils';
 import { translateRemotePreferences } from 'src/constants/PROJECT_REMOTE_PREFERENCE';
-import { getCountryByShortname } from 'src/constants/COUNTRIES';
 import { translateExperienceLevel } from 'src/constants/EXPERIENCE_LEVEL';
+import { translatePaymentType } from 'src/constants/PROJECT_PAYMENT_TYPE';
+import { translatePaymentRange } from 'src/constants/PAYMENT_RANGE';
+import { Job } from 'src/components/organisms/job-list/job-list.types';
+import { when } from 'src/core/utils';
 
 export function getCategories(job: Job): Array<JSX.Element | string> {
   const list: Array<JSX.Element | string> = [];
@@ -26,16 +27,40 @@ export function getCategories(job: Job): Array<JSX.Element | string> {
 
   const projectLength = () => (
     <>
-      <img style={{ marginRight: '2px' }} src="/icons/time.svg" />
+      <img style={{ marginRight: '2px' }} src="/icons/during.svg" />
       {translateProjectLength(job.project_length)}
     </>
   );
 
-  const experienceLevel = () => <>{translateExperienceLevel(job.experience_level)}</>;
+  const paymentType = () => (
+    <>
+      <img style={{ marginRight: '2px' }} src="/icons/heart-blue.svg" />
+      {translatePaymentType(job.payment_type)}
+    </>
+  );
+
+  const paymentRange = () => (
+    <>
+      <img style={{ marginRight: '2px' }} src={`/icons/${job.payment_type === 'PAID' ? 'paid' : 'time'}.svg`} />
+      {
+        translatePaymentRange(job.payment_range_lower, job.payment_range_higher, job.payment_type, job.payment_scheme)
+          .value
+      }
+    </>
+  );
+
+  const experienceLevel = () => (
+    <>
+      <img style={{ marginRight: '2px' }} src="/icons/level.svg" />
+      {translateExperienceLevel(job.experience_level)}
+    </>
+  );
 
   when(job.country, () => list.push(location()));
   when(job.project_type, () => list.push(projectType()));
   when(job.project_length, () => list.push(projectLength()));
+  when(job.payment_type, () => list.push(paymentType()));
+  when(job.payment_range_lower && job.payment_range_higher, () => list.push(paymentRange()));
   when(job.remote_preference, () => list.push(translateRemotePreferences(job.remote_preference)));
   when(job.experience_level !== undefined, () => list.push(experienceLevel()));
 
