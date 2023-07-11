7cdb6d88736752f383d5775a0e569625b641e3de
Sajad Abbasi <s.abbasi@me.com>
2023-07-11 10:47:32 +0330

    feat: add tier list (#956)

--- src/components/organisms/impact-category-list/impact-category-list.types.ts
+++ src/components/organisms/impact-category-list/impact-category-list.types.ts
@@ -2,6 +2,5 @@ import { ImpactBadgeProps } from '../../atoms/impact-badge/impact-badge.types';
 import { CSSProperties } from 'react';
 
 export interface ImpactCategoryListProps extends CSSProperties {
-//   activeList: ImpactBadgeProps[];
   activeList: string[];
 }
--- src/pages/achievements/achievements.shared.tsx
+++ src/pages/achievements/achievements.shared.tsx
@@ -4,12 +4,17 @@ import { useMatch } from '@tanstack/react-location';
 import { Loader } from './achievements.types';
 import { Header } from './components/header/header';
 import { ImpactCategoryList } from 'src/components/organisms/impact-category-list/impact-category-list';
+import { Tabs } from 'src/components/atoms/tabs/tabs';
+import { Tab } from 'src/components/atoms/tabs/tabs.types';
+import { JobHistoryList } from 'src/components/organisms/job-history-list/job-history-list';
+import { Tier } from './components/tier/tier';
 
 export const useAchievementsShared = () => {
   const { badges } = useMatch().ownData as Loader;
   const activeList = badges.badges.map((badge) => badge.social_cause_category);
   const points = badges.badges.reduce((prev, curr) => prev + curr.total_points, 0);
   const tier = evaluateTier(points);
+  console.log({ tier });
   const [showClaimPointsSlide, setShowClaimPointsSlide] = useState(false);
   const [showCheckRewardsSlide, setShowCheckRewardsSlide] = useState(false);
 
@@ -21,10 +26,29 @@ export const useAchievementsShared = () => {
       point={points}
     />
   );
+  const jobHistoryListJSX = <JobHistoryList data={[]} />;
+
+  const tabs: Tab[] = [
+    {
+      name: 'Achievements',
+      content: (
+        <div style={{ padding: '1rem 0', display: 'grid', gap: '1rem' }}>
+          <Tier currentTier={tier.current} tier={tier} />
+          <ImpactCategoryList activeList={activeList} />
+        </div>
+      ),
+      default: true,
+    },
+    {
+      name: 'History',
+      content: jobHistoryListJSX,
+      default: true,
+    },
+  ];
 
   const bottom = (
-    <div style={{ padding: '1rem 0' }}>
-      <ImpactCategoryList activeList={activeList} tier={tier} />
+    <div>
+      <Tabs tabs={tabs} />
     </div>
   );
 
