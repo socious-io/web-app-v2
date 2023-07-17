8cc397bd3a7b5a3a7938c716c808adad560a784d
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2023-07-17 13:24:46 +0330

    fix: reject clicked, route nowhere and be in current page (#1156)

---
+++ src/pages/job-offer-reject/applicant-detail/applicant-detail.shared.tsx
@@ -0,0 +1,18 @@
+import { useMatch, useNavigate } from '@tanstack/react-location';
+import { Resolver } from './applicant-detail.types';
+import { rejectApplicant } from '../job-offer-reject.services';
+
+export const useApplicantDetailShared = () => {
+  const navigate = useNavigate();
+  const { screeningQuestions, applicantDetail } = useMatch().ownData as Resolver;
+
+  function navigateToOverview() {
+    navigate({ to: '..' });
+  }
+
+  function onReject(id: string) {
+    return () => rejectApplicant(id).then(navigateToOverview);
+  }
+
+  return { navigate, screeningQuestions, applicantDetail, onReject };
+};
