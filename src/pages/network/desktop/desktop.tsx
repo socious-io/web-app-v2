61c99db913979f8747ee6e58b410339dd4dbba63
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2023-07-17 11:14:28 +0330

    fix: add network category for user and org and replace network icon with connection icon (#1155)

--- src/pages/network/desktop/desktop.tsx
+++ src/pages/network/desktop/desktop.tsx
@@ -12,7 +12,7 @@ export const Desktop: React.FC = () => {
   const { navigateNetwork, identity } = useNetworkShared();
 
   const NetworkMenuList = [
-    { label: 'Connections', icon: '/icons/network.svg', link: () => navigate({ to: '/network/connections' }) },
+    { label: 'Connections', icon: '/icons/connection.svg', link: () => navigate({ to: '/network/connections' }) },
     { label: 'Followings', icon: '/icons/followers.svg', link: () => navigate({ to: '/network/followings' }) },
   ];
 
--- src/pages/team/desktop/desktop.tsx
+++ src/pages/team/desktop/desktop.tsx
@@ -26,7 +26,7 @@ export const Desktop: React.FC = () => {
   } = useTeamShared();
 
   const NetworkMenuList = [
-    { label: 'Connections', icon: '/icons/network.svg', link: () => navigate({ to: '/network/connections' }) },
+    { label: 'Connections', icon: '/icons/connection.svg', link: () => navigate({ to: '/network/connections' }) },
     { label: 'Followings', icon: '/icons/followers.svg', link: () => navigate({ to: '/network/followings' }) },
   ];
 
