167e9b5fd95b64a535c3a29093ab45f4061e3125
Sajad Abbasi <s.abbasi@me.com>
2023-07-27 11:08:09 +0330

    feat: add alert to confirm buttons (#1178)

--- src/components/molecules/alert-confirm/alert-confirm.tsx
+++ src/components/molecules/alert-confirm/alert-confirm.tsx
@@ -9,11 +9,16 @@ export type AlertConfirmProps = {
 };
 
 export function AlertConfirm(props: AlertConfirmProps) {
+  function confirm() {
+    props.onConfirm();
+    props.onClose();
+  }
+
   return (
     <div className={css.container}>
       <div className={css.title}>{props.options.title}</div>
       <div className={css.message}>{props.options.message}</div>
-      <Button onClick={props.onConfirm}>Confirm</Button>
+      <Button onClick={confirm}>Confirm</Button>
       <Button onClick={props.onClose} color="white">
         Cancel
       </Button>
