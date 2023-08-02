4c74183cb008af40eaed9388a8f7d2b5547215b3
jeyem <me@e-mahmoudi.me>
2023-08-02 17:54:15 +0330

    flint wallet

---
+++ src/dapp/wallets/flint.ts
@@ -0,0 +1,66 @@
+import { Chain } from 'wagmi/chains';
+import { Connector, Address, ConnectorData } from 'wagmi';
+
+export default class extends Connector {
+  id = 'flint';
+  name = 'Flint Wallet';
+  options = {};
+  ready = false;
+
+  // Event emitters
+  onAccountsChanged = (_: string[]): void => {}; // empty function
+  onChainChanged = (_: number): void => {}; // empty function
+  onDisconnect = (): void => {}; // empty function
+  getBlockExplorerUrls = (_: Chain): string[] | undefined => undefined;
+  isChainUnsupported = (_: number): boolean => true;
+
+  async connect(config?: { chainId?: number }): Promise<Required<ConnectorData<any>>> {
+    const provider = await this.getProvider();
+
+    const accounts = await provider.enable();
+    const chainId = config?.chainId || this.chains[0].id;
+    if (chainId) await this.switchChain(chainId);
+
+    return {
+      provider,
+      chain: {
+        id: chainId,
+        unsupported: false,
+      },
+      account: accounts[0],
+    };
+  }
+
+  async switchChain(chainId: number): Promise<Chain> {
+    /* const provider = await this.getProvider()
+      await provider.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: chainId }],
+      }); */
+    return this.chains.filter((c) => c.id == chainId)[0];
+  }
+
+  async getAccount(): Promise<Address> {
+    const provider = await this.getProvider();
+    const accounts = provider.enable();
+    return accounts[0];
+  }
+  async getChainId(): Promise<number> {
+    const { chain } = await this.connect();
+    return chain.id;
+  }
+
+  async getProvider() {
+    return window?.evmproviders?.flint;
+  }
+
+  async disconnect() {}
+
+  async getSigner(config?: { chainId?: number }): Promise<any> {
+    return '';
+  }
+
+  async isAuthorized(): Promise<boolean> {
+    return true;
+  }
+}
