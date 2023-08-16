de26a290bbfe42de772c111448e138cd024319ef
Marjan <marjan.arami@arades.de>
2023-08-16 03:55:19 +0330

    Modify title and meta tag for job details page

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,24 @@
-import { defineCustomElements } from '@ionic/pwa-elements/loader';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.scss';
+import { defineCustomElements } from '@ionic/pwa-elements/loader'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Helmet } from 'react-helmet'
+import App from './App'
+import './index.scss'
 
-console.log('version: 6.1.30');
+console.log('version: 6.1.30')
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   //   <React.StrictMode>
-  <App />
+  <>
+    <Helmet>
+      <meta charSet="UTF-8" />
+      <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Socious</title>
+    </Helmet>
+    <App />
+  </>
   //   </React.StrictMode>
-);
+)
 
-defineCustomElements(window);
+defineCustomElements(window)
