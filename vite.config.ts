162de0a2339e2fe32176ae6534bb4bb149202d4c
Arun-chaitanya <arunchaitanya.iitkgp@gmail.com>
2023-08-24 15:14:10 +0530

    vite config and tsconfig changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,35 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vitejs.dev/config/
+const aliases = {
+  '@assets': './src/assets/*',
+  '@atoms': './src/components/atoms',
+  '@molecules': './src/components/molecules',
+  '@organisms': './src/components/organisms',
+  '@templates': './src/components/templates',
+  '@constants': './src/constants',
+  '@core': './src/core',
+  '@dapp': './src/dapp',
+  '@hooks': './src/hooks',
+  '@pages': './src/pages',
+  '@store': './src/store',
+  '@styles': './src/styles',
+  '@translations': './src/translations',
+};
+
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   optimizeDeps: {
     exclude: [`@ionic/pwa-elements/loader`],
+  },
+  resolve: {
+    alias: Object.entries(aliases).map(([key, value]) => ({
+      find: key,
+      replacement: path.resolve(__dirname, value),
+    })),
   },
   envDir: 'environments',
 });
