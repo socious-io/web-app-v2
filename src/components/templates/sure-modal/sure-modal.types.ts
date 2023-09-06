5f6965e8b222e306a2e25e7de55208f876f0e592
Iman rb <iman.rezasefat@gmail.com>
2023-09-06 17:52:15 +0330

    feat: modal added for external sites

--- src/components/templates/sure-modal/sure-modal.types.ts
+++ src/components/templates/sure-modal/sure-modal.types.ts
@@ -2,5 +2,5 @@ import { ModalProps } from '../modal/modal.types';
 
 export interface SureModalProps extends Omit<ModalProps, 'children'> {
   onDone: () => void;
-  modalTexts: { title: string; confirmButton: string; cancleButton: string; body: string };
+  modalTexts: { title: string; confirmButton: string; cancleButton: string; body: string; titleColor?: string };
 }
