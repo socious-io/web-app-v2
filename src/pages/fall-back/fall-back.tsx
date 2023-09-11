ef10d4a0cffd9b916b1c8a40baf9189696a513f8
Marjan <marjan.arami@arades.de>
2023-09-11 15:47:42 +0330

    Add error boundary and fallback component

---
+++ src/pages/fall-back/fall-back.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button } from 'src/components/atoms/button/button';
+import { Typography } from 'src/components/atoms/typography/typography';
+import css from './fall-back.module.scss';
+
+const FallBack = (): JSX.Element => {
+  console.log('fallback page');
+  return (
+    <div className={css.container}>
+      <div className={css.errorCode}>500</div>
+      <div className={css.errorMsg}>Internal Server Error</div>
+      <div className={css.errorDetails}>We apologize for the inconvenience. Please try again later</div>
+      <div className={css.buttonContainer}>
+        <Button className={css.button}>
+          <a href="/" className={css.link}>
+            Home Page
+          </a>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default FallBack;
