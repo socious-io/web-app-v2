f374a42986d528d939777b5eac7f919914429597
Marjan <marjan.arami@arades.de>
2023-09-13 15:34:15 +0330

    calling discord logger in case of fallback

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { ErrorBoundary } from 'react-error-boundary';
 import App from './App';
 import './index.scss';
 import FallBack from './pages/fall-back/fall-back';
+import { logError } from './pages/fall-back/fall-back.services';
 
 console.log('version: 6.1.30');
 
@@ -18,7 +19,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Socious</title>
     </Helmet>
-    <ErrorBoundary fallback={<FallBack />}>
+    <ErrorBoundary fallback={<FallBack />} onError={logError}>
       <App />
     </ErrorBoundary>
   </>
