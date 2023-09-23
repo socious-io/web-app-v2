6c2e477c408c006a57413507fc3af549c13809a0
Nabil TAZI <arobasenabil@gmail.com>
2023-09-23 14:04:34 +0900

    removed comments

--- src/components/templates/top-up-summary-card/top-up-summary-card.types.ts
+++ src/components/templates/top-up-summary-card/top-up-summary-card.types.ts
@@ -1,12 +1,10 @@
 import { PaymentDetailProps } from 'src/components/molecules/payment-detail/payment-detail.types';
 
 export interface TopUpSummaryCardProps {
-  //extends PaymentDetailProps {
   title: string;
   unit?: string;
   offer_rate: number;
   weekly_limit: number;
   isPaidCrypto: boolean;
   containerClassName?: string;
-  //   total_price: number;
 }
