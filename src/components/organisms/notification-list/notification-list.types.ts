d3c9efb10712955ed3eceb3b8d73db5ce75fc7ef
Marjan <marjan.arami@arades.de>
2023-10-03 01:51:48 +0330

    router/services/types refactoring of NOTIFICATIONS

--- src/components/organisms/notification-list/notification-list.types.ts
+++ src/components/organisms/notification-list/notification-list.types.ts
@@ -1,38 +1,40 @@
+import { Notification } from 'src/core/api';
+
 export interface NotificationListProps {
-  list: Notifications[];
+  list: Notification[];
   onMorePageClick: () => void;
   showSeeMore: boolean;
   route: 'd' | 'm';
 }
 
-export type Notifications = {
-  created_at: string;
-  data: DataObj;
-  id: string;
-};
+// export type Notifications = {
+//   created_at: string;
+//   data: DataObj;
+//   id: string;
+// };
 
-export type DataObj = {
-  body: BodyObj;
-  identity: IdentityObj;
-  type: string;
-  parentId: string;
-  refId: string;
-};
+// export type DataObj = {
+//   body: BodyObj;
+//   identity: IdentityObj;
+//   type: string;
+//   parentId: string;
+//   refId: string;
+// };
 
-export type BodyObj = {
-  body: string;
-  title: string;
-};
+// export type BodyObj = {
+//   body: string;
+//   title: string;
+// };
 
-export type IdentityObj = {
-  meta: MetaObj;
-  type: string;
-};
+// export type IdentityObj = {
+//   meta: MetaObj;
+//   type: string;
+// };
 
-export type MetaObj = {
-  avatar?: string;
-  image?: string;
-  name: string;
-  shortname: string;
-  username: string;
-};
+// export type MetaObj = {
+//   avatar?: string;
+//   image?: string;
+//   name: string;
+//   shortname: string;
+//   username: string;
+// };
