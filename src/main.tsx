6c23afe697f6443b4e71e39d7901a5202c07d588
jeyem <me@e-mahmoudi.me>
2023-10-09 19:54:01 +0330

    init datadog

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,36 @@
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ErrorBoundary } from 'react-error-boundary';
 import { Helmet } from 'react-helmet';
-
+import { config } from './config';
 import App from './App';
 import './index.scss';
 import FallBack from './pages/fall-back/fall-back';
 import { logError } from './pages/fall-back/fall-back.services';
+import { datadogRum } from '@datadog/browser-rum';
 
-console.log('version: 6.1.30');
+const VERSION = '7.0.0';
+
+if (config.datadogClientToken && config.datadogAppId) {
+  datadogRum.init({
+    applicationId: config.datadogAppId,
+    clientToken: config.datadogClientToken,
+    site: 'datadoghq.com',
+    service: 'Socious-Webapp',
+    sampleRate: 100,
+    trackInteractions: true,
+  });
+
+  datadogRum.addRumGlobalContext('env', config.env);
+  datadogRum.addRumGlobalContext('version', VERSION);
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   //   <React.StrictMode>
   <>
     <Helmet>
       <meta charSet="UTF-8" />
+      <meta name="version" content={VERSION} />
       <link rel="icon" type="image/png" href="/favicon.png" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Socious</title>
