7ea448c0c4c640f30c98691cb2d62f6bfcbe81f8
jeyem <me@e-mahmoudi.me>
2023-10-10 18:01:19 +0330

    datadog RUM

--- src/store/thunks/identity.thunks.ts
+++ src/store/thunks/identity.thunks.ts
@@ -1,4 +1,17 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { identities } from 'src/core/api';
+import { setUser } from 'src/core/datadog';
 
-export const currentIdentities = createAsyncThunk('identity/currentIdentities', identities);
+export const currentIdentities = createAsyncThunk('identity/currentIdentities', async () => {
+  const currentIdentities = await identities();
+  const primary = currentIdentities.find((i) => i.primary);
+  // Condition to just make typescript happy
+  if (primary) {
+    setUser({
+      id: primary.id,
+      email: primary.meta.email,
+      name: primary.meta.name,
+    });
+  }
+  return currentIdentities;
+});
