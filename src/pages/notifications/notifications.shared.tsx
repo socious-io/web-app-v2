9b1ce27696b7eaf43f78d31993dac0fe645f5ee8
Marjan <marjan.arami@arades.de>
2023-10-16 12:06:53 +0330

    added swith account on notification click

--- src/pages/notifications/notifications.shared.tsx
+++ src/pages/notifications/notifications.shared.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useLoaderData } from 'react-router-dom';
-import { CurrentIdentity, notifications, NotificationsRes, OrgMeta, UserMeta } from 'src/core/api';
+import { CurrentIdentity, identities, notifications, NotificationsRes, OrgMeta, UserMeta } from 'src/core/api';
+import { nonPermanentStorage } from 'src/core/storage/non-permanent';
 import { RootState } from 'src/store';
+import { setIdentityList } from 'src/store/reducers/identity.reducer';
 
 export const useNotificationsShared = () => {
   const list = useLoaderData() as NotificationsRes;
+  const dispatch = useDispatch();
   const [notificationList, setNotificationList] = useState(list.items);
   const [page, setPage] = useState(1);
   const totalCount = list.total_count;
@@ -28,5 +31,10 @@ export const useNotificationsShared = () => {
     });
   };
 
-  return { notificationList, identity, avatarImg, onMorePageClick, onShowSeeMore };
+  const switchAccount = async (id: string) => {
+    await nonPermanentStorage.set({ key: 'identity', value: id });
+    identities().then((resp) => dispatch(setIdentityList(resp)));
+  };
+
+  return { notificationList, identity, avatarImg, onMorePageClick, onShowSeeMore, switchAccount };
 };
