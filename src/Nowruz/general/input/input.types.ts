d66ff107eeb549444fc8427b7d082b384f07d799
Marjan <marjan.arami@arades.de>
2023-10-25 16:15:52 +0330

    add default prop

--- src/Nowruz/general/input/input.types.ts
+++ src/Nowruz/general/input/input.types.ts
@@ -1,13 +1,10 @@
-export type InputProps = {
-  id?: string;
-  name?: string;
+import { TextFieldProps as DefaultProps } from '@mui/material';
+export interface InputProps extends DefaultProps {
   label?: string;
   required?: boolean;
   errors?: string[];
   isValid?: boolean;
-  type?: 'text' | 'password';
   validMessage?: string;
   prefix?: string;
   color: 'primary' | 'secondary' | 'error' | 'info' | 'success' | 'warning';
-  placeholder?: string;
-};
+}
