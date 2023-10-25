100c5d4f428c34f43e9ad4f1e35c7f8f6cb38534
Iman rb <iman.rezasefat@gmail.com>
2023-10-25 10:10:35 +0330

    feat: new button and backlink & storybook config

---
+++ src/stories/back-link.stories.tsx
@@ -0,0 +1,23 @@
+import { Meta, Story } from '@storybook/react';
+import React from 'react';
+import { BackLink } from 'src/Nowruz/general/BackLink';
+import { BackLinkProps } from 'src/Nowruz/general/BackLink/back-link.types';
+
+import { withRouter } from '.storybook/withRouter';
+export default {
+  title: 'Components/BackLink',
+  component: BackLink,
+  decorators: [withRouter],
+} as Meta;
+
+const Template: Story<BackLinkProps> = (args: BackLinkProps) => <BackLink {...args} />;
+
+export const Preview = Template.bind({});
+Preview.args = {
+  title: 'Jobs',
+};
+
+
+
+
+
