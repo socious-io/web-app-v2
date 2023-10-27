13ba1a268dadde9012d7be330a4bcf395fd2674e
Marjan <marjan.arami@arades.de>
2023-10-27 22:25:12 +0330

    vertical stepper

---
+++ src/Nowruz/general/stepper/stepperIcon.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import css from './stepper.module.scss';
+
+interface StepperIconProps {
+  step: number;
+  activeStep: number;
+  activeColor: string;
+  disabledColor: string;
+}
+
+const StepperIconWrapper: React.FC<StepperIconProps & { Component: React.ComponentType }> = ({
+  step,
+  activeStep,
+  activeColor,
+  disabledColor,
+  Component,
+}) => {
+  return (
+    <div className={css.iconDiv}>
+      <Component stroke={step <= activeStep ? activeColor : disabledColor} />
+    </div>
+  );
+};
+
+export default StepperIconWrapper;
