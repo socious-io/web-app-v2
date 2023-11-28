803e1ea59fbbcbee62a7754a5307cb1dfa2d4750
Marjan <marjan.arami@arades.de>
2023-11-28 12:29:05 +0330

    modified link and icon-button focus state

--- src/Nowruz/modules/general/components/link/index.tsx
+++ src/Nowruz/modules/general/components/link/index.tsx
@@ -7,7 +7,7 @@ import { LinkProps } from './link.types';
 export const Link: React.FC<LinkProps> = (props) => {
   const { label, href, customStyle } = props;
   return (
-    <MuiLink underline="hover" className={`${css.link} ${customStyle}`} href={href}>
+    <MuiLink className={`${css.link} ${customStyle}`} href={href} {...props}>
       {label}
     </MuiLink>
   );
