f923b2218ab3e0abfaf899abcb641f9b34c64b03
Marjan <marjan.arami@arades.de>
2023-11-28 22:00:31 +0330

    add circle checkbox

--- src/Nowruz/modules/general/components/checkbox/checkbox.tsx
+++ src/Nowruz/modules/general/components/checkbox/checkbox.tsx
@@ -1,21 +1,50 @@
 import { FormControlLabel, Checkbox as MUICheckbox } from '@mui/material';
 import React from 'react';
+import variables from 'src/components/_exports.module.scss';
+import { Icon } from 'src/Nowruz/general/Icon';
 
 import css from './checkbox.module.scss';
 import { CheckboxProps } from './checkbox.types';
 
 interface IconProps {
   checked: boolean;
+  size: 'small' | 'medium';
 }
-const BpCheckedIcon: React.FC<IconProps> = ({ checked }) => {
+const SquareIcon: React.FC<IconProps> = ({ checked, size }) => {
   return (
-    <div className={`${css.icon} ${!checked && css.iconNotChecked} ${checked && css.iconChecked}`}>
-      {checked && <img src="/icons/nowruz/checkGreen.svg" alt="" />}
+    <div
+      className={`${size === 'small' ? 'w-4 h-4 ' : 'w-5 h-5 '} ${css.icon} ${!checked && css.iconNotChecked} ${
+        checked && css.iconChecked
+      }`}
+    >
+      {checked && <Icon name="check" color={variables.color_primary_600} fontSize={size === 'small' ? 12 : 14} />}
     </div>
   );
 };
 
-export const Checkbox: React.FC<CheckboxProps> = ({ id, label, required, errors, onChange, isValid, ...props }) => {
+const CircleIcon: React.FC<IconProps> = ({ checked, size }) => {
+  return (
+    <div
+      className={` rounded-lg flex items-center justify-center ${size === 'small' ? 'w-4 h-4 ' : 'w-5 h-5 '} ${
+        !checked && css.circleIconNotChecked
+      } ${checked && css.circleIconChecked}`}
+    >
+      {checked && <Icon name="check" color="white" fontSize={size === 'small' ? 10 : 14} />}
+    </div>
+  );
+};
+
+export const Checkbox: React.FC<CheckboxProps> = ({
+  id,
+  label,
+  required,
+  errors,
+  onChange,
+  isValid,
+  type = 'checkBox',
+  size = 'small',
+  ...props
+}) => {
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === ' ') {
       // Simulate a click event when Enter is pressed
@@ -41,8 +70,16 @@ export const Checkbox: React.FC<CheckboxProps> = ({ id, label, required, errors,
               onKeyDown={handleKeyDown}
               className={css.default}
               color="default"
-              checkedIcon={<BpCheckedIcon checked />}
-              icon={<BpCheckedIcon checked={false} />}
+              checkedIcon={
+                type === 'checkBox' ? <SquareIcon checked size={size} /> : <CircleIcon checked size={size} />
+              }
+              icon={
+                type === 'checkBox' ? (
+                  <SquareIcon checked={false} size={size} />
+                ) : (
+                  <CircleIcon checked={false} size={size} />
+                )
+              }
               {...props}
             />
           }
