de19df2ddc8dc2e4dd0bb1be94ef270383011b89
Iman rb <iman.rezasefat@gmail.com>
2023-11-28 11:38:31 +0330

    add google login to signup page

--- src/Nowruz/modules/Auth/containers/signup/EmailForm/index.tsx
+++ src/Nowruz/modules/Auth/containers/signup/EmailForm/index.tsx
@@ -1,3 +1,4 @@
+import { Google } from 'public/icons/nowruz/google';
 import React from 'react';
 import { Button } from 'src/Nowruz/modules/general/components/Button';
 import { Input } from 'src/Nowruz/modules/general/components/input/input';
@@ -5,7 +6,7 @@ import { Input } from 'src/Nowruz/modules/general/components/input/input';
 import { useEmailForm } from './useEmailForm';
 
 export const EmailForm = () => {
-  const { register, errors, onSubmit, handleSubmit } = useEmailForm();
+  const { register, errors, onSubmit, handleSubmit, navigate } = useEmailForm();
   const type = localStorage.getItem('registerFor');
   return (
     <>
@@ -19,9 +20,13 @@ export const EmailForm = () => {
           placeholder="Enter your email"
           errors={errors['email']?.message ? [errors['email']?.message.toString()] : undefined}
         />
-        <div className="mt-8">
+        <div className=" flex flex-col gap-4 mt-8">
           <Button color="primary" block onClick={handleSubmit(onSubmit)}>
             Continue
+          </Button>
+          <Button color="primary" variant="outlined" onClick={() => navigate('/oauth/google')}>
+            <Google />
+            <div>Continue with Google</div>
           </Button>
         </div>
       </form>
