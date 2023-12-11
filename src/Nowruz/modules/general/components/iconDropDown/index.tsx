768754c29f0486b3f89adf9ae47a6893c2aee0d0
Marjan <marjan.arami@arades.de>
2023-12-11 17:07:37 +0330

    fix some issues of sign-up

--- src/Nowruz/modules/general/components/iconDropDown/index.tsx
+++ src/Nowruz/modules/general/components/iconDropDown/index.tsx
@@ -1,34 +1,53 @@
-import { Divider } from '@mui/material';
-import React from 'react';
+import { Divider, IconButton, MenuItem, MenuList } from '@mui/material';
+import React, { createRef, useRef } from 'react';
 import { Avatar } from 'src/Nowruz/modules/general/components/avatar/avatar';
 import { IconListItem } from 'src/Nowruz/modules/general/components/avatarDropDown/iconListItem';
 import { AvatarLabelGroup } from 'src/Nowruz/modules/general/components/avatarLabelGroup';
 
+import css from './iconDropDown.module.scss';
 import { IconDropDownProps } from './iconDropDown.types';
 import { useIconDropDown } from './useIconDropDown';
 
 export const IconDropDown: React.FC<IconDropDownProps> = (props) => {
   const { size = '40px', type, img, iconName, customStyle, accounts = [], iconItems = [], customItems = [] } = props;
   const { open, setOpen, switchAccount } = useIconDropDown();
+
   return (
-    <div className="flex flex-col items-end relative ">
-      <div className="flex items-center justify-end">
-        <Avatar size={size} type={type} img={img} iconName={iconName} onClick={() => setOpen(!open)} />
-      </div>
+    <div className="flex flex-col items-end relative">
+      <IconButton
+        className={`${css.avatarBtn} ${open && `${css.avatarBtnOpen}`}`}
+        disableRipple
+        onClick={() => setOpen(!open)}
+      >
+        <Avatar
+          size={size}
+          type={type}
+          img={img}
+          iconName={iconName}
+          onClick={() => setOpen(!open)}
+          iconCustomStyle="!cursor-pointer"
+        />
+      </IconButton>
       {open && (
-        <div
-          className={`w-[280px] max-h-96 overflow-y-auto overflow-x-hidden bg-Base-White border border-solid border-Gray-light-mode-200 rounded-default shadow-Shadows/shadow-lg flex flex-col absolute top-[48px] ${customStyle}`}
-        >
+        <MenuList autoFocusItem className={`${css.menuList} ${customStyle}`}>
           {accounts.map((a) => (
-            <AvatarLabelGroup key={a.id} account={a} handleClick={() => switchAccount(a.id)} />
+            <MenuItem key={a.id} className={css.menuItem}>
+              <AvatarLabelGroup account={a} handleClick={() => switchAccount(a.id)} />
+            </MenuItem>
           ))}
-          {iconItems.length ? <Divider /> : ''}
+          {iconItems.length ? <Divider className="!m-0" /> : ''}
           {iconItems.map((i) => (
-            <IconListItem key={i.label} iconName={i.iconName} label={i.label} onClick={i.onClick} />
+            <MenuItem key={i.label} className={css.menuItem}>
+              <IconListItem iconName={i.iconName} label={i.label} onClick={i.onClick} />
+            </MenuItem>
           ))}
-          {customItems.length ? <Divider /> : ''}
-          {customItems.map((i) => i)}
-        </div>
+          {customItems.length ? <Divider className="!m-0" /> : ''}
+          {customItems.map((i) => (
+            <MenuItem sx={{ padding: '0' }} className={css.menuItem}>
+              {i}
+            </MenuItem>
+          ))}
+        </MenuList>
       )}
     </div>
   );
