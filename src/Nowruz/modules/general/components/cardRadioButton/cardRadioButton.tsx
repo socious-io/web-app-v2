b85183b0e5c7c190ad762e5a8d2ce9e346e21080
Marjan <marjan.arami@arades.de>
2023-12-16 07:50:17 +0330

    add update education

--- src/Nowruz/modules/general/components/cardRadioButton/cardRadioButton.tsx
+++ src/Nowruz/modules/general/components/cardRadioButton/cardRadioButton.tsx
@@ -1,6 +1,6 @@
-import { Radio, RadioGroup } from '@mui/material';
-import { Check } from 'public/icons/nowruz/check';
-import variables from 'src/components/_exports.module.scss';
+import { RadioGroup } from '@mui/material';
+// import { Check } from 'public/icons/nowruz/check';
+// import variables from 'src/components/_exports.module.scss';
 
 import css from './cardRadioButton.module.scss';
 import { CardRadioButtonProps } from './cardRadioButton.types';
--- src/core/api/additionals/additionals.types.ts
+++ src/core/api/additionals/additionals.types.ts
@@ -1,20 +1,32 @@
 import { Media } from '../media/media.types';
 export type AdditionalTypes = 'PORTFOLIO' | 'CERTIFICATE' | 'EDUCATION' | 'BENEFIT' | 'RECOMMENDATIONS';
 
+export interface EducationMeta {
+  field?: string;
+  grade?: string;
+  degree?: string;
+  end_month?: string;
+  end_year?: string;
+  start_month?: string;
+  start_year?: string;
+  school_id?: string;
+  school_name?: string;
+}
+
 export interface AdditionalReq {
   type: AdditionalTypes;
   title: string;
-  description: string;
-  url: string | null;
-  image: string | any;
-  sub_image: string | any;
-  identity_id: string;
-  ref_identity_id: string | null;
-  meta: any | null;
+  description?: string;
+  url?: string | null;
+  image?: string | any;
+  sub_image?: string | any;
+  ref_identity_id?: string | null;
+  meta?: EducationMeta | any | null;
   enabled: boolean | null;
 }
 
 export interface AdditionalRes extends AdditionalReq {
+  identity_id: string;
   id: string;
   image: Media;
   sub_image: Media;
