dfd622007155179e5a61abd602ac3b6bdcae3946
Marjan <marjan.arami@arades.de>
2024-01-03 14:22:44 +0330

    add timezone to city (onboarding, profile, edit info)

--- src/Nowruz/modules/userProfile/containers/editInfo/useEditInfo.tsx
+++ src/Nowruz/modules/userProfile/containers/editInfo/useEditInfo.tsx
@@ -49,8 +49,9 @@ export const useEditInfo = (handleClose: () => void) => {
     });
     return mappedObj;
   };
-  const [cityVal, setCityVal] = useState(!user || !user.city ? null : { label: user.city });
+  const [cityVal, setCityVal] = useState(!user || !user.city ? null : { label: `${user.city}, ${user.country}` });
   const [selectedCity, setSelectedCity] = useState(user?.city);
+  const [selectedCountry, setSelectedCountry] = useState(user?.country);
   const [languages, setLanguages] = useState<LanguageProps[]>(mapLanguageToItems(user?.languages || []));
   const [isUsernameValid, setIsusernameValid] = useState(false);
   const [isUsernameAvailable, setIsusernameAvailable] = useState(false);
@@ -90,8 +91,9 @@ export const useEditInfo = (handleClose: () => void) => {
 
   const resetState = () => {
     setSocialCauses(socialCausesToCategory(user?.social_causes));
-    setCityVal(!user || !user.city ? null : { label: user.city });
+    setCityVal(!user || !user.city ? null : { label: `${user.city}, ${user.country}` });
     setSelectedCity(user?.city);
+    setSelectedCountry(user?.country);
     setLanguages(mapLanguageToItems(user?.languages || []));
     setIsusernameValid(false);
     setIsusernameAvailable(false);
@@ -135,8 +137,9 @@ export const useEditInfo = (handleClose: () => void) => {
 
   const cityToOption = (cities: Location[]) => {
     return cities.map((city) => ({
-      label: `${city.name}, ${city.region_name}`,
+      label: JSON.stringify({ label: `${city.name}, ${city.country_name}`, description: city.timezone_utc }),
       countryCode: city.country_code,
+      city: city.name,
     }));
   };
 
@@ -151,7 +154,8 @@ export const useEditInfo = (handleClose: () => void) => {
     }
   };
   const onSelectCity = (location) => {
-    setSelectedCity(location.label);
+    setSelectedCity(location.city);
+    setSelectedCountry(location.countryCode);
     setCityVal({ label: location.label });
   };
   async function updateIdentityList() {
@@ -172,6 +176,7 @@ export const useEditInfo = (handleClose: () => void) => {
       username: getValues().username,
       mission: getValues().summary,
       city: selectedCity,
+      country: selectedCountry,
       social_causes: SocialCauses.map((item) => item.value),
     };
 
