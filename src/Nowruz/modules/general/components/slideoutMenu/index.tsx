c38db49a05b07c8d7270b45e6c017284dd21d16f
Marjan <marjan.arami@arades.de>
2024-02-02 10:42:18 +0330

    changed backgorund color of backdrop to transparent and added transition direction

--- src/Nowruz/modules/general/components/slideoutMenu/index.tsx
+++ src/Nowruz/modules/general/components/slideoutMenu/index.tsx
@@ -1,4 +1,4 @@
-import { Backdrop } from '@mui/material';
+import { Backdrop, Slide } from '@mui/material';
 import React from 'react';
 import { BackLink } from 'src/Nowruz/modules/general/components/BackLink';
 import { CloseButton } from 'src/Nowruz/modules/general/components/closeButton';
@@ -10,18 +10,20 @@ export const Overlay: React.FC<OverlayProps> = ({ open, onClose, children }) => 
   return (
     <>
       {open && (
-        <Backdrop sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }} open={open}>
-          <div className={css.container}>
-            <div className={css.header}>
-              <div className="hidden md:flex">
-                <CloseButton handleClose={onClose} />
+        <Backdrop sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, backgroundColor: 'transparent' }} open={open}>
+          <Slide direction="left" in={open} mountOnEnter unmountOnExit>
+            <div className={css.container}>
+              <div className={css.header}>
+                <div className="hidden md:flex">
+                  <CloseButton handleClose={onClose} />
+                </div>
+                <div className="flex md:hidden  mt-6 ml-4 mr-auto mb-auto">
+                  <BackLink title="Back" onBack={onClose} customStyle="!w-fit" />
+                </div>
               </div>
-              <div className="flex md:hidden  mt-6 ml-4 mr-auto mb-auto">
-                <BackLink title="Back" onBack={onClose} customStyle="!w-fit" />
-              </div>
+              <div className={css.content}>{children}</div>
             </div>
-            <div className={css.content}>{children}</div>
-          </div>
+          </Slide>
         </Backdrop>
       )}
     </>
