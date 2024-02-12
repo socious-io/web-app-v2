388c82a92b7f92d0f403ded690ac7874c04fa8f9
mojgan-a <mojgan.aloodari@gmail.com>
2024-02-12 21:22:33 +0330

    The menu should be closed if the mouse is out of the Status block (anywhere on thfilteredMenue page) /TASS-985

--- src/Nowruz/modules/layout/components/statusDropDown/index.tsx
+++ src/Nowruz/modules/layout/components/statusDropDown/index.tsx
@@ -6,6 +6,7 @@ import { Dot } from 'src/Nowruz/modules/general/components/dot';
 import { ToggleButton } from 'src/Nowruz/modules/general/components/toggleButton';
 
 import css from './statusDropDown.module.scss';
+import { event } from 'cypress/types/jquery';
 
 interface StatusDropDownProps {
   type: 'users' | 'organizations';
@@ -29,44 +30,54 @@ export const StatusDropDown: React.FC<StatusDropDownProps> = (props) => {
   const handleOutsideClick = (e) => {
     if (newRef.current && !newRef.current.contains(e.target)) { if (open) { setOpen(!open) } }
   }
+  // const handleOutsideClick = (e) => {
+  //   if (newRef.current && !newRef.current.contains(e.target)) { if (open) { setOpen(!open) } }
+  // }
+  // onMouseOut={()=> setOpen(!open)}
 
   return (
-    <div className="w-full h-full flex flex-col items-end relative" ref={newRef}>
-      <Button variant="outlined" className={css.statusButton} color="primary" onClick={() => setOpen(!open)}>
+    <div className="w-full h-full flex flex-col items-end relative group" ref={newRef}>
+      <Button variant="outlined" className={css.statusButton} color="primary" 
+        onMouseLeave={()=> setOpen(false)} onMouseOver={()=> setOpen(true)}
+        onClick={() => setOpen(!open)}>
         <Dot size="small" color={variables.color_success_500} shadow shadowColor={variables.color_success_100} />
         Status
         <Icon name="chevron-down" fontSize={20} color={variables.color_grey_700} className="!cursor-pointer" />
       </Button>
 
       {open && (
-        <div className={css.menu}>
-          {type === 'users' && (
-            <>
+        <div  className={css.parentMenu} onMouseLeave={()=> setOpen(false)} onMouseOver={()=> setOpen(true)}>
+          <div className={css.menu}
+          >
+            {type === 'users' && (
+              <>
+                <div className={css.item}>
+                  <ToggleButton checked={openToWork} onChange={handleOpenToWork} size="small" />
+                  <div className="flex flex-col">
+                    <p className={css.title}>Open to work</p>
+                    <p className={css.subtitle}>You are available for paid jobs. You will be able to receive offers.</p>
+                  </div>
+                </div>
+                <div className={css.item}>
+                  <ToggleButton checked={openToVolunteer} onChange={handleOpenToVolunteer} size="small" />
+                  <div className="flex flex-col">
+                    <p className={css.title}>Open to volunteer</p>
+                    <p className={css.subtitle}>You are available for volunteer jobs.</p>
+                  </div>
+                </div>
+              </>
+            )}
+            {type === 'organizations' && (
               <div className={css.item}>
-                <ToggleButton checked={openToWork} onChange={handleOpenToWork} size="small" />
+                <ToggleButton checked={hiring} onChange={handleHiring} size="small" />
                 <div className="flex flex-col">
-                  <p className={css.title}>Open to work</p>
-                  <p className={css.subtitle}>You are available for paid jobs. You will be able to receive offers.</p>
+                  <p className={css.title}>Hiring</p>
+                  <p className={css.subtitle}>You are currently actively hiring professionals.</p>
                 </div>
               </div>
-              <div className={css.item}>
-                <ToggleButton checked={openToVolunteer} onChange={handleOpenToVolunteer} size="small" />
-                <div className="flex flex-col">
-                  <p className={css.title}>Open to volunteer</p>
-                  <p className={css.subtitle}>You are available for volunteer jobs.</p>
-                </div>
-              </div>
-            </>
-          )}
-          {type === 'organizations' && (
-            <div className={css.item}>
-              <ToggleButton checked={hiring} onChange={handleHiring} size="small" />
-              <div className="flex flex-col">
-                <p className={css.title}>Hiring</p>
-                <p className={css.subtitle}>You are currently actively hiring professionals.</p>
-              </div>
-            </div>
-          )}
+            )}
+          </div>
+
         </div>
       )}
     </div>
