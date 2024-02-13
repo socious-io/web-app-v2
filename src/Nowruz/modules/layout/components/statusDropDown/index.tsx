affb0d19c2af93ac0c2246fddb1aec152f301c0d
mojgan-a <mojgan.aloodari@gmail.com>
2024-02-13 12:01:39 +0330

    TAS-988/The menu should be closed if the mouse is out of the Status block

--- src/Nowruz/modules/layout/components/statusDropDown/index.tsx
+++ src/Nowruz/modules/layout/components/statusDropDown/index.tsx
@@ -21,22 +21,8 @@ export const StatusDropDown: React.FC<StatusDropDownProps> = (props) => {
   const [open, setOpen] = useState(false);
   const { type, openToWork, openToVolunteer, hiring, handleHiring, handleOpenToVolunteer, handleOpenToWork } = props;
 
-  // handle click outside to close menu
-  const newRef = useRef(null)
-  useEffect(() => {
-    document.addEventListener("mousedown", handleOutsideClick)
-    return () => { document.removeEventListener("mousedown", handleOutsideClick) }
-  })
-  const handleOutsideClick = (e) => {
-    if (newRef.current && !newRef.current.contains(e.target)) { if (open) { setOpen(!open) } }
-  }
-  // const handleOutsideClick = (e) => {
-  //   if (newRef.current && !newRef.current.contains(e.target)) { if (open) { setOpen(!open) } }
-  // }
-  // onMouseOut={()=> setOpen(!open)}
-
   return (
-    <div className="w-full h-full flex flex-col items-end relative group" ref={newRef}>
+    <div className="w-full h-full flex flex-col items-end relative group">
       <Button variant="outlined" className={css.statusButton} color="primary" 
         onMouseLeave={()=> setOpen(false)} onMouseOver={()=> setOpen(true)}
         onClick={() => setOpen(!open)}>
