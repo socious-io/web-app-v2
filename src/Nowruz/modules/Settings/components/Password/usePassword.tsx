38bbcdf3a4a750df919026b5e5bfade3196461da
mojgan-a <mojgan.aloodari@gmail.com>
2024-02-17 19:50:56 +0330

    remove issue

--- src/Nowruz/modules/Settings/components/Password/usePassword.tsx
+++ src/Nowruz/modules/Settings/components/Password/usePassword.tsx
@@ -1,11 +1,12 @@
-import { passwordPattern } from 'src/core/regexs';
-import { changePassword } from 'src/core/api';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { useEffect, useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { Inputs } from "./password.type"
-
+import { changePassword } from 'src/core/api';
+import { passwordPattern } from 'src/core/regexs';
 import * as yup from 'yup';
+
+import { Inputs } from "./password.type";
+
 
 
 const schema = yup.object().shape({
@@ -18,7 +19,7 @@ export const usePassword = () => {
     const [isPasswordLengthValid, setIsPasswordLengthValid] = useState(false);
     const [isPasswordPatternValid, setIsPasswordPatternValid] = useState(false);
     const [isFormValid, setIsFormValid] = useState(false);
-    const [isPasswordMatch, setPasswordMatch] = useState(true)
+    const [isPasswordMatch, setPasswordMatch] = useState(true);
     const {
         watch,
         register,
@@ -28,7 +29,7 @@ export const usePassword = () => {
     } = useForm<Inputs>({
         resolver: yupResolver(schema),
     });
-    const current_password = watch('current_password')
+    const current_password = watch('current_password');
     const password = watch('password');
     const confirmPassword = watch('confirm');
     useEffect(() => {
@@ -42,36 +43,14 @@ export const usePassword = () => {
 
     const onSubmit: SubmitHandler<Inputs> = async (data) => {        
         try {
-          await changePassword({ current_password, password })
+          await changePassword({ current_password, password });
           reset();
           
-        } catch (error) { console.log(error) }
+        } catch (error) { console.log(error); }
       };
 
-    // const onSubmit: SubmitHandler<Inputs> = async ({ current_password, password }) => {
-    //     console.log({ current_password, password });
-        
-    //     try {
-    //         changePassword({ current_password, password });
-    //     } catch (e) {
-    //         console.log(e);
-    //     }
-    // };
-
-    // function onSubmit() {
-    //     const payload = {
-    //       "current_password": current_password,
-    //       "password": password,
-    //     };
-    //     changePassword(payload)
-    //     .then(() => {debugger})
-    //     .catch((err) => {
-    //         console.log(err.response.data);
-    //     });
-    //   }
-
     return { register, handleSubmit, errors, onSubmit,reset,isPasswordMatch ,isFormValid ,isPasswordLengthValid,isPasswordPatternValid };
-}
+};
 
 
 
