6c6484a0b77a06b18c032d2058a781fff0a6e9bf
Jayson Ilagan <jayson.vergara.ilagan@gmail.com>
2024-02-19 02:28:55 +1300

    chore: filtering of search

--- src/Nowruz/pages/search/index.tsx
+++ src/Nowruz/pages/search/index.tsx
@@ -1,67 +1,57 @@
-import { ReactNode, useCallback, useMemo } from 'react';
-import { Job, Organization, User } from 'src/core/api';
+import variables from 'src/components/_exports.module.scss';
+import { Icon } from 'src/Nowruz/general/Icon';
 import { Button } from 'src/Nowruz/modules/general/components/Button';
 import { Pagination } from 'src/Nowruz/modules/general/components/Pagination';
-import ProfileCard from 'src/Nowruz/modules/general/components/profileCard';
-import { JobListingCard } from 'src/Nowruz/modules/Jobs/components/JobListingCard';
+import { Overlay } from 'src/Nowruz/modules/general/components/slideoutMenu';
+import { FilterSlider } from 'src/Nowruz/modules/Search/components/FilterSlider';
 import { useSearch } from 'src/Nowruz/pages/search/useSearch';
 
 import css from './list.module.scss';
 
 export const Search = () => {
-  const { page, setPage, total, PER_PAGE, searchResult, isMobile, type, q } = useSearch();
-
-  const card = useCallback(
-    (item: Job | Organization | User) => {
-      let card: ReactNode = <JobListingCard job={item as Job} />;
-      switch (type) {
-        case 'users':
-        case 'organizations':
-          card = <ProfileCard identity={item as User | Organization} labelShown={false} />;
-          break;
-
-        case 'projects':
-          card = <JobListingCard job={item as Job} />;
-          break;
-      }
-      return card;
-    },
-    [type],
-  );
-
-  const readableType = useMemo(() => {
-    if (type === 'projects') return 'jobs';
-    if (type === 'users') return 'people';
-    return 'organization';
-  }, [type]);
+  const {
+    data: { page, searchResult, total, PER_PAGE, readableType, q, sliderFilterOpen, filter },
+    operations: { setPage, card, handleCloseOrApplyFilter, onApply, onClose },
+  } = useSearch();
 
   const headerClass = `${css.header}`;
   return (
     <div className={css.container}>
-      <div>
+      <div className={css.headerContainer}>
         <div className={headerClass}>
           <h1 className={css.title}>{`Search for ${q}`}</h1>
           <h2 className={css.subtitle}>{`${total} ${readableType} found`}</h2>
         </div>
+        <div>
+          <Button color="secondary" variant="outlined" className={css.filterButton} onClick={handleCloseOrApplyFilter}>
+            <Icon fontSize={20} name="filter-lines" color={variables.color_grey_700} />
+            Filters
+          </Button>
+        </div>
       </div>
       <div className={css.list}>
         {searchResult.items?.map((item) => <div className="mt-6">{card(item)}</div>)}
-        {!isMobile && (
-          <div className="mt-11">
-            <Pagination
-              count={Math.floor(total / PER_PAGE) + (total % PER_PAGE && 1)}
-              onChange={(e, p) => setPage(p)}
-            />
-          </div>
-        )}
-        {isMobile && (
-          <div className="mt-5 flex items-center justify-center">
-            <Button color="primary" variant="text" onClick={() => setPage(page + 1)}>
-              See more
-            </Button>
-          </div>
-        )}
+
+        <div className="mt-11 hidden md:block">
+          {total >= PER_PAGE && (
+            <Pagination count={Math.floor(total / PER_PAGE + (total % PER_PAGE))} onChange={(e, p) => setPage(p)} />
+          )}
+        </div>
+
+        <div className="mt-5 flex items-center justify-center block md:hidden">
+          <Button color="primary" variant="text" onClick={() => setPage(page + 1)}>
+            See more
+          </Button>
+        </div>
       </div>
+      <Overlay
+        open={sliderFilterOpen}
+        onClose={handleCloseOrApplyFilter}
+        title="Filter by"
+        subtitle={`Filter ${readableType} by social causes, skills and more`}
+      >
+        <FilterSlider type={readableType} onApply={onApply} onClose={onClose} filter={filter} />
+      </Overlay>
     </div>
   );
 };
--- src/Nowruz/pages/search/useSearch.tsx
+++ src/Nowruz/pages/search/useSearch.tsx
@@ -1,8 +1,18 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useLoaderData, useSearchParams } from 'react-router-dom';
-import { JobsRes, OrganizationsRes, UsersRes } from 'src/core/api';
+import { Job, JobsRes, Organization, OrganizationsRes, User, UsersRes } from 'src/core/api';
 import { search as searchReq } from 'src/core/api/site/site.api';
 import { isTouchDevice } from 'src/core/device-type-detector';
+import ProfileCard from 'src/Nowruz/modules/general/components/profileCard';
+import { JobListingCard } from 'src/Nowruz/modules/Jobs/components/JobListingCard';
+
+type FilterReq = {
+  causes_tags?: Array<string>;
+  skills?: Array<string>;
+  country?: Array<string>;
+  city?: Array<string>;
+  label?: { value: string; label: string; countryCode: string };
+};
 
 export const useSearch = () => {
   const data = useLoaderData() as JobsRes | UsersRes | OrganizationsRes;
@@ -10,17 +20,28 @@ export const useSearch = () => {
   const type = searchParams.get('type');
   const q = searchParams.get('q');
 
-  const PER_PAGE = 10;
+  const PER_PAGE = 20;
   const isMobile = isTouchDevice();
-  const [searchResult, setSearchResult] = useState<JobsRes | UsersRes | OrganizationsRes>({});
+  const [searchResult, setSearchResult] = useState({} as JobsRes | UsersRes | OrganizationsRes);
   const [page, setPage] = useState(1);
+  const [sliderFilterOpen, setSliderFilterOpen] = useState(false);
+  const [filter, setFilter] = useState<FilterReq>({} as FilterReq);
+
+  const filterNeeded = (filter: FilterReq) => {
+    const propertyName = type === 'projects' ? 'causes_tags' : 'social_causes';
+    return {
+      [propertyName]: filter.causes_tags,
+      skills: filter.skills,
+      country: filter.country,
+      city: filter.city,
+    };
+  };
 
   const fetchMore = async (page: number) => {
     const body = {
-      //filter: { skills: ['BLOCKCHAIN_DEVELOPMENT', 'PLUTUS'] },
-      filter: {},
+      filter: filter ? filterNeeded(filter) : {},
       type,
-    };
+    } as any;
     if (q?.trim()) {
       Object.assign(body, { q });
     }
@@ -29,6 +50,45 @@ export const useSearch = () => {
     if (isMobile && page > 1) setSearchResult({ ...searchResult, ...data });
     else setSearchResult(data);
   };
+
+  const handleCloseOrApplyFilter = () => {
+    setSliderFilterOpen(!sliderFilterOpen);
+  };
+
+  const onClose = () => {
+    handleCloseOrApplyFilter();
+  };
+
+  const onApply = async (filterRaw: FilterReq) => {
+    setFilter(filterRaw);
+    const body = {
+      filter: filterRaw ? filterNeeded(filterRaw) : {},
+      type,
+    } as any;
+    if (q?.trim()) {
+      Object.assign(body, { q });
+    }
+    console.log(JSON.stringify(body));
+    const data = await searchReq(body, { limit: 20, page: 1 });
+    setSearchResult(data);
+    handleCloseOrApplyFilter();
+  };
+
+  const readableType = useMemo(() => {
+    if (type === 'projects') return 'jobs';
+    if (type === 'users') return 'people';
+    return 'organization';
+  }, [type]);
+
+  const card = useCallback(
+    (item: Job | Organization | User) => {
+      if (type && ['users', 'organizations'].includes(type)) {
+        return <ProfileCard identity={item as User | Organization} labelShown={false} />;
+      }
+      return <JobListingCard job={item as Job} />;
+    },
+    [type],
+  );
 
   useEffect(() => {
     fetchMore(page);
@@ -41,13 +101,22 @@ export const useSearch = () => {
   }, [data]);
 
   return {
-    page,
-    setPage,
-    searchResult,
-    total: searchResult.total_count ?? data.total_count,
-    PER_PAGE,
-    isMobile,
-    type,
-    q,
+    data: {
+      page,
+      searchResult,
+      total: searchResult.total_count ?? data.total_count,
+      PER_PAGE,
+      readableType,
+      q,
+      sliderFilterOpen,
+      filter,
+    },
+    operations: {
+      setPage,
+      card,
+      handleCloseOrApplyFilter,
+      onApply,
+      onClose,
+    },
   };
 };
