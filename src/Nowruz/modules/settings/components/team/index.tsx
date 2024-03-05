dc187c114a3c67b524ec721f290d13540784b0ad
mojgan-a <mojgan.aloodari@gmail.com>
2024-03-05 12:19:28 +0330

    add infinit scroll to user team management

--- src/Nowruz/modules/settings/components/team/index.tsx
+++ src/Nowruz/modules/settings/components/team/index.tsx
@@ -2,49 +2,58 @@ import { Avatar } from "src/Nowruz/modules/general/components/avatar/avatar";
 
 import css from "./team.module.scss";
 import { useTeam } from "./useTeam";
+import InfiniteScroll from "react-infinite-scroller";
 
 const Team = () => {
-    const {followings,remove,loadMore} = useTeam();
+    const {followings,remove,loadMore,hasMore} = useTeam();
     return(
         <>
             <div className={css.borderSection}>
                 <div className="w-full py-8 items-center">
                     <h2 className="grow css.title text-lg font-semibold">Team management</h2>
-                    <p className='text-sm font-normal text-Gray-light-mode-600 pt-1'>
-                        Manage your teams
+                    <p className='text-sm font-normal text-Gray-light-mode-600 pt-1'> 
+                        Manage your teams 
                     </p>
                 </div>
             </div>
             <div className="grid grid-cols-4 gap-4">
-                <div>
-                    <h6>On teams</h6>
-                    <p> You’re currently on these teams. </p>
+                <div className="py-5">
+                    <h6 className="text-sm font-semibold text-Gray-light-mode-700 leading-5">On teams</h6>
+                    <p className="text-sm font-normal text-Gray-light-mode-600"> You’re currently on these teams. </p>
                 </div>
                 <div className="col-span-3">
-                        {
-                            followings?.items.map((i)=>(
-                                <div className={css.uList}>
-                                    <div className="flex justify-start items-center">
-                                        <Avatar size="40px" type={'users'} img={i.identity_meta.img} />
-                                        <div className="flex flex-col ml-3">
-                                            <span className="leading-7 text-Gray-light-mode-900">
-                                            {i.identity_meta.name}
-                                            </span>
-                                            <span className="text-sm font-medium leading-5 text-Gray-light-mode-600">
-                                            @{i.identity_meta.shortname}
-                                            </span>
-                                        </div>
-                                    </div>
-                                    <div onClick={()=> {remove(i.identity_meta.id)}} className="cursor-pointer text-Gray-light-mode-600 text-sm">
-                                        Leave
+                    <InfiniteScroll
+                        initialLoad={false}
+                        threshold={150}
+                        useWindow={false}
+                        pageStart={1}
+                        loadMore={loadMore}
+                        hasMore={hasMore}
+                        className="flex-1"
+                    >
+                    {
+                        followings?.items.map((i)=>(
+                            <div className={css.uList}>
+                                <div className="flex justify-start items-center">
+                                    <Avatar size="40px" type={'users'} img={i.identity_meta.img} />
+                                    <div className="flex flex-col ml-3">
+                                        <span className="leading-7 text-Gray-light-mode-900">
+                                        {i.identity_meta.name}
+                                        </span>
+                                        <span className="text-sm font-medium leading-5 text-Gray-light-mode-600">
+                                        @{i.identity_meta.shortname}
+                                        </span>
                                     </div>
                                 </div>
+                                <div onClick={()=> {remove(i.identity_meta.id)}} className="cursor-pointer text-Gray-light-mode-600 text-sm">
+                                    Leave
+                                </div>
+                            </div>
 
-                            ))
-                        }
+                        ))
+                    }
+                    </InfiniteScroll>
                 </div>
-                <div onClick={()=> loadMore()}>Load More</div>
-
             </div>
         </>
     );
--- src/Nowruz/modules/settings/components/team/useTeam.tsx
+++ src/Nowruz/modules/settings/components/team/useTeam.tsx
@@ -5,6 +5,8 @@ import { follow, FollowingRes, getFollowings, unfollow } from 'src/core/api';
 export const useTeam = () => {
     const [followings, setFollowings] = useState();
     const [currentPage, setCurrectPage] = useState(1);
+    const [hasMore, sethasMore] = useState(true);
+
     let result:FollowingRes;
     const getFollowing = async () => {
         result = await getFollowings({page:1});
@@ -17,21 +19,29 @@ export const useTeam = () => {
     }
     
     async function loadMore() {
-        const Req = await getFollowings({ page: currentPage + 1 });
-        setCurrectPage((prev) => prev + 1);
-        setFollowings({
-            ...followings,
-            ...Req,
-            items: [...followings.items, ...Req.items],
-        });
+        if(hasMore) {
+
+            const Req = await getFollowings({ page: currentPage + 1 });
+            if(Req.items.length) {
+                setCurrectPage((prev) => prev + 1);
+                setFollowings({
+                    ...followings,
+                    ...Req,
+                    items: [...followings.items, ...Req.items],
+                });
+
+            } else {
+                sethasMore(false)
+            }
+        }
     }
-    
+   
+
     useEffect(()=>{
         getFollowing();
     },[]);
 
-    console.log('followings',followings);
 
 
-    return {followings,remove,loadMore};
+    return {followings,remove,loadMore,hasMore};
 };
