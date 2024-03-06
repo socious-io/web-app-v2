56cb1bcc8a4f18d4eeae115826767dc6d2204668
mojgan-a <mojgan.aloodari@gmail.com>
2024-03-06 12:34:04 +0330

    add ui

--- src/Nowruz/modules/orgSettings/components/team/index.tsx
+++ src/Nowruz/modules/orgSettings/components/team/index.tsx
@@ -1,47 +1,102 @@
 import { SearchDropdown } from "src/Nowruz/modules/general/components/SearchDropdown";
 import css from "./team.module.scss";
+import { Button } from "src/Nowruz/modules/general/components/Button";
 import { useTeam } from "./useTeam";
+import { Avatar } from "src/Nowruz/modules/general/components/avatar/avatar";
 
 const Team = () => {
-    const {pipi} = useTeam();
+    const {members,onRemove} = useTeam();
     return(
         <>
-        <div className={css.borderSection}>
-            <div className="w-full py-8 items-center">
-                <h2 className="grow css.title text-lg font-semibold">Team management</h2>
-                <p className='text-sm font-normal text-Gray-light-mode-600 pt-1'>
-                Manage your team members and their account permissions here.  
-                </p>          
+            <div className={css.borderSection}>
+                <div className="w-full py-8 items-center">
+                    <h2 className="grow css.title text-lg font-semibold">Team management</h2>
+                    <p className='text-sm font-normal text-Gray-light-mode-600 pt-1'>
+                    Manage your team members and their account permissions here.  
+                    </p>     
+                </div>
             </div>
-        </div>
-        <div className="grid grid-cols-5 gap-4">
-            <div>
-                <h6>Invite team members</h6>
-                <p>
-                Invite member to post or manage your organization
-                </p>
-            </div>
-            <div className="col-span-3">
-                <SearchDropdown
-                placeholder="Please select"
-                icon="image-user"
-                // options={memberList}
-                isSearchable
-                // onChange={(option) => onSelectExperienceLevel(option.value)}
-                />
-            </div>
-            <div>
-                <SearchDropdown
+            <div className="grid grid-cols-5 gap-4 pt-6">
+                <div>
+                    <h6 className="text-sm font-semibold text-Gray-light-mode-700">Invite team members</h6>
+                    <p className="text-sm font-normal text-Gray-light-mode-600">
+                    Invite member to post or manage your organization
+                    </p>
+                </div>
+                <div className="col-span-3">
+                    <SearchDropdown
                     placeholder="Please select"
-                    // options={EXPERIENCE_LEVEL_V2}
+                    icon="image-user"
+                    options={members?.items}
                     isSearchable
                     // onChange={(option) => onSelectExperienceLevel(option.value)}
-                />
+                    />
+                </div>
+                <div>
+                    <SearchDropdown
+                        placeholder="Please select"
+                        // options={EXPERIENCE_LEVEL_V2}
+                        isSearchable
+                        // onChange={(option) => onSelectExperienceLevel(option.value)}
+                    />
+                </div>
             </div>
-        </div>
-        <div>
-            
-        </div>
+            <div className="grid grid-cols-5 gap-4 align-bottom mt-4">
+                <div>&nbsp;</div>
+                <div className="col-span-3">
+                    + Add another 
+                </div>
+                <div>
+                    <Button color="primary" size="medium" startIcon={<img src="/icons/team.svg"/>} className="w-full">Send invites</Button>
+                </div>
+            </div>
+
+            <div className={css.borderSection}></div>
+            <div className="grid grid-cols-5 gap-4 pt-6">
+                <div className="pt-6">
+                    <h6 className="text-sm font-semibold text-Gray-light-mode-700">Invite team members</h6>
+                    <p  className="text-sm font-normal text-Gray-light-mode-600">
+                    Invite member to post or manage your organization
+                    </p>
+                </div>
+                <div className="col-span-4">
+                    <div className={css.tableCereditList}>
+                        <table>
+                            <thead className="border border-Gray-light-mode-200">
+                                <tr className="text-xs font-medium text-Gray-light-mode-600">
+                                    <th>Name</th>
+                                    <th>Role</th>
+                                    <th>&nbsp;</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {members?.items.map((i)=> (
+                                    <tr className="text-sm font-normal text-left">
+                                        <td className="w-3/5 flex justify-start items-center">
+                                        <Avatar size="40px" type={'users'} img={i.avatar} />
+                                            <div className="flex flex-col ml-3">
+                                                <span className="leading-7 text-Gray-light-mode-900">
+                                                {i.first_name} {i.last_name}
+                                                </span>
+                                                <span className="text-sm font-medium leading-5 text-Gray-light-mode-600">
+                                                @{i.username}
+                                                </span>
+                                            </div>
+                                        </td>
+                                        <td className="w-1/5">{i.id}</td>
+                                        <td  className="w-1/5 flex justify-start items-center gap-2">
+                                            <div className="text-sm font-semibold text-Gray-light-mode-600" onClick={()=> onRemove(i.id) }>Delete</div>
+                                            <div className="text-sm font-semibold text-Brand-700">Edit</div>
+                                        </td>
+                                    </tr>
+
+                                ))}
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
--- src/Nowruz/modules/orgSettings/components/team/useTeam.tsx
+++ src/Nowruz/modules/orgSettings/components/team/useTeam.tsx
@@ -1,27 +1,59 @@
 import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import { useSelector } from 'react-redux';
+
 import {
     addOrganizationMember,
     CurrentIdentity,
     filterFollowings,
     getOrganizationMembers,
     MembersRes,
+    FollowingRes,
+    getFollowings,
     removeOrganizationMember,
+    organizations,
 } from 'src/core/api';
-
+import { convertFollowingsToContactList } from './memberList';
+import { Resolver } from './team.type';
+import { RootState } from 'src/store';
 
 export const useTeam = () => {
-    
 
-    const [memberList, setMemberList] = useState();
+    const currentIdentity = useSelector<RootState, CurrentIdentity | undefined>((state) => {
+        return state.identity.entities.find((identity) => identity.current);
+    });
+    const [members, setMembers] = useState<MembersRes>();
+    const [followings, setFollowings] = useState<FollowingRes>();
 
-    getOrganizationMembers
 
-   
-    function onSearchMember(value: string) {
-        getNewFollowings(value);
+    const orgId = currentIdentity?.id;
+
+    const getMembers = async() => {
+        console.log('orgId',orgId);
+        const memberRes = await getOrganizationMembers( ''+orgId +'', { page: 1 });
+        console.log(memberRes.items);
+        setMembers(memberRes);
+    };
+
+    const getFollowing = async() => {
+        const FollowingRes = await filterFollowings({ page: 1, name: '', type: 'users' });
+        console.log('FollowingRes',FollowingRes);
+        setFollowings(FollowingRes);
+    };
+    useEffect(()=>{
+        getMembers();  
+        getFollowing();      
+    },[]);
+
+    function onRemove(user_id: string) {
+        removeOrganizationMember(''+orgId +'', user_id).then(() => {
+            getMembers();
+        });
     }
-
-    return{ memberList};
+    
+//     const initialMemberList = convertFollowingsToContactList(followings?.items);
+// console.log('initialMemberList',initialMemberList);
+    
+    return{ orgId,members,followings,onRemove};
 
 };
