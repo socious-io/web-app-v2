f41da6dd4feef81995fadf5f2e71dcb7b3e10f09
mojgan-a <mojgan.aloodari@gmail.com>
2024-03-06 19:24:32 +0330

    .

--- src/Nowruz/modules/orgSettings/components/team/useTeam.tsx
+++ src/Nowruz/modules/orgSettings/components/team/useTeam.tsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from "react";
-import { useLoaderData } from "react-router-dom";
 import { useSelector } from 'react-redux';
-
 import {
     addOrganizationMember,
     CurrentIdentity,
@@ -9,13 +7,11 @@ import {
     getOrganizationMembers,
     MembersRes,
     FollowingRes,
-    getFollowings,
     removeOrganizationMember,
-    organizations,
 } from 'src/core/api';
-import { convertFollowingsToContactList } from './memberList';
-import { Resolver } from './team.type';
 import { RootState } from 'src/store';
+
+import { convertFollowingsToContactList } from './memberList';
 
 export const useTeam = () => {
 
@@ -25,59 +21,54 @@ export const useTeam = () => {
     const [members, setMembers] = useState<MembersRes>();
     const [followings, setFollowings] = useState<FollowingRes>();
     const [memberList, setMemberList] = useState();
-    const [role, setRole] = useState([{value: 'Admin'}]);
+    const [role, setRole] = useState([{ value: 'Admin' }]);
     const [userId, setUserId] = useState('');
 
     const orgId = currentIdentity?.id;
 
-    const getMembers = async() => {
-        console.log('orgId',orgId);
-        const memberRes = await getOrganizationMembers( ''+orgId +'', { page: 1 });
+    const getMembers = async () => {
+        console.log('orgId', orgId);
+        const memberRes = await getOrganizationMembers('' + orgId + '', { page: 1 });
         console.log(memberRes);
         setMembers(memberRes);
     };
-    
 
-    const getFollowing = async(name:string) => {
-        await filterFollowings({ page: 1, name: name , type: 'users' }).then((res)=>{
+    const getFollowing = async (name: string) => {
+        await filterFollowings({ page: 1, name: name, type: 'users' }).then((res) => {
             setFollowings(res);
-            console.log('followings',followings);
+            console.log('followings', followings);
             const initialMemberList = convertFollowingsToContactList(res?.items);
             setMemberList(initialMemberList);
         });
-        
+
     };
 
-    
-  function onSearchMember(value: string) {
-    getFollowing(value);
-    setUserId(value);
-  }
+    function onSearchMember(value: string) {
+        console.log('value', value);
+
+        getFollowing(value);
+        setUserId(value);
+    }
 
 
-    useEffect(()=>{
-        getMembers();  
-        getFollowing(''); 
-    },[]);
-
-    
-    
-
+    useEffect(() => {
+        getMembers();
+        getFollowing('');
+    }, []);
 
     function onRemove(user_id: string) {
-        removeOrganizationMember(''+orgId +'', user_id).then(() => {
+        removeOrganizationMember('' + orgId + '', user_id).then(() => {
             getMembers();
         });
         getFollowing('');
     }
-    function onAddMember(user_id: string) {
-        addOrganizationMember( ''+orgId +'', user_id).then(() => {
-          getOrganizationMembers( ''+orgId +'', { page: members.page }).then(setMembers);
-          getFollowing('');
-        });
-      }
-    
-    
-    return{ members,followings,onRemove,memberList,onSearchMember,onAddMember,role,userId};
 
+    function onAddMember(user_id: string) {
+        addOrganizationMember('' + orgId + '', user_id).then(() => {
+            getOrganizationMembers('' + orgId + '', { page: members.page }).then(setMembers);
+            getFollowing('');
+        });
+    }
+
+    return { members, followings, onRemove, memberList, onSearchMember, onAddMember, role, userId };
 };
