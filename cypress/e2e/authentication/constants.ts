3f42e6d597f212e1449d1272d069a30107374ba4
satanmourner <mahmoudisanaz59@gmail.com>
2024-03-23 17:54:14 +0330

    test(TAS-1283): login e2e test

---
+++ cypress/e2e/authentication/constants.ts
@@ -0,0 +1,15 @@
+//Server
+export const API_SERVER = Cypress.env('api_server');
+export const APP_URL = Cypress.env('app_url');
+
+//Credentials
+export const VALID_EMAIL = 'valid_email@gmail.com';
+export const VALID_PASSWORD = 'v@lidP@$$wd';
+
+export const INVALID_EMAIL = 'wrong_email@gmail.com';
+export const INVALID_PASSWORD = 'wrong_password';
+
+//Authentication
+export const ACCESS_TOKEN = 'VALID_ACCESS_TOKEN';
+export const REFRESH_TOKEN = 'VALID_REFRESH_TOKEN';
+export const TOKEN_TYPE = 'VALID_TOKEN_TYPE';
