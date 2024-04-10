844cf677c66fe64ac1d3d7fd6b1415aad8a77ee3
satanmourner <mahmoudisanaz59@gmail.com>
2024-04-10 23:45:04 +0330

    feat: add issued tab for user and org

--- src/Nowruz/pages/Credentials/useCredentials.tsx
+++ src/Nowruz/pages/Credentials/useCredentials.tsx
@@ -1,23 +1,35 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 import { CurrentIdentity, OrgMeta } from 'src/core/api';
 import { CredentialList } from 'src/Nowruz/modules/credentials/credentialsList';
+import { IssuedList } from 'src/Nowruz/modules/credentials/issuedList';
 import { RootState } from 'src/store';
 
 export const useCredentials = () => {
+  const { hash } = useLocation();
+  const [hideVerifyBanner, setHideVerifyBanner] = useState(localStorage.getItem('hideVerifiedBanner') === 'true');
+  const [openVerifiyAlert, setOpenVerifiyAlert] = useState(false);
   const currentIdentity = useSelector<RootState, CurrentIdentity | undefined>(state =>
     state.identity.entities.find(item => item.current),
   );
-  const [hideVerifyBanner, setHideVerifyBanner] = useState(localStorage.getItem('hideVerifiedBanner') === 'true');
+
+  const verified = (currentIdentity?.meta as OrgMeta)?.verified;
+  const tabs = [
+    { label: 'Issued', content: <IssuedList /> },
+    { label: 'Requested', content: <CredentialList /> },
+    { label: 'Archived', content: <></> },
+  ];
+  const activeTabIndex = {
+    '#issued': 0,
+    '#requested': 1,
+  };
 
   const handleDismissVerified = () => {
     localStorage.setItem('hideVerifiedBanner', 'true');
     setHideVerifyBanner(true);
   };
 
-  const [openVerifiyAlert, setOpenVerifiyAlert] = useState(false);
-  const verified = (currentIdentity?.meta as OrgMeta)?.verified;
-  const tabs = [{ label: 'Requested', content: <CredentialList /> }];
   return {
     tabs,
     verified,
@@ -26,5 +38,6 @@ export const useCredentials = () => {
     hideVerifyBanner,
     handleDismissVerified,
     type: currentIdentity?.type,
+    activeTabIndex: activeTabIndex[hash] || 0,
   };
 };
