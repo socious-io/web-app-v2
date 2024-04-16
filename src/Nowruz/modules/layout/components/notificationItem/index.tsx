2a3fc74f0a8292feb7ea4c256ea6fb68c70aecba
Marjan <marjan.arami@arades.de>
2024-04-16 15:30:03 +0330

    changed notificationItem for referral-notifications

--- src/Nowruz/modules/layout/components/notificationItem/index.tsx
+++ src/Nowruz/modules/layout/components/notificationItem/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import variables from 'src/components/_exports.module.scss';
 import { Notification } from 'src/core/api';
 import { toRelativeTime } from 'src/core/relative-time';
-import { UserType } from 'src/core/types';
 import { Avatar } from 'src/Nowruz/modules/general/components/avatar/avatar';
 import { Dot } from 'src/Nowruz/modules/general/components/dot';
 
@@ -15,22 +14,12 @@ interface NotificationItemProps {
 
 export const NotificationItem: React.FC<NotificationItemProps> = ({ item, onClick }) => {
   const avatarImg = item.data.identity.meta.avatar || item.data.identity.meta.image || '';
-  const notifType = item.type;
-  const referralNotifs = [
-    'REFERRAL_JOINED',
-    'REFERRAL_VERIFIED',
-    'REFERRAL_HIRED',
-    'REFERRAL_COMPLETED_JOB',
-    'REFERRAL_CONFIRMED_JOB',
-  ];
   return (
     <div className="flex gap-3 w-full h-fit items-start cursor-pointer" onClick={onClick}>
-      <Avatar size="48px" img={avatarImg} type={(item.data.type as UserType) || 'users'} />
+      <Avatar size="48px" img={avatarImg} type={item.data.type || 'users'} />
       <div className="w-full h-fit flex flex-col">
         <div className="flex gap-2">
-          <p className={css.name}>
-            {notifType in referralNotifs ? item.data.identity.meta.email : item.data.identity.meta.name}
-          </p>
+          <p className={css.name}>{item.data.identity.meta.name}</p>
           <p className={css.time}>{toRelativeTime(item.created_at.toString())}</p>
         </div>
         <p className={css.message}>{item.data.body.body?.toString() || ''}</p>
