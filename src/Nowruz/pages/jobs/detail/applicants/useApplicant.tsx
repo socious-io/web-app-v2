fa0a320edd640e78f21134782c74112e46fef7af
marjan-ara <marjan.arami@gmail.com>
2024-05-11 17:30:48 +0330

    fixed applicant tab

--- src/Nowruz/pages/jobs/detail/applicants/useApplicant.tsx
+++ src/Nowruz/pages/jobs/detail/applicants/useApplicant.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import { Applicant } from 'src/core/api';
 
-export const useApplicant = (applicant: Applicant) => {
+export const useApplicant = (applicant: Applicant, openReject: () => void, closeDetails: () => void) => {
   const navigate = useNavigate();
   const handleViewProfile = () => {
     navigate(`/profile/users/${applicant.user.username}/view`);
@@ -11,10 +11,18 @@ export const useApplicant = (applicant: Applicant) => {
     window.open(applicant.attachment?.url, '_blank', 'rel=noopener noreferrer');
   };
 
-  const questionList = applicant?.answers?.map((a) => {
-    const q = applicant.questions?.find((item) => item.id === a.question_id);
+  const questionList = applicant?.answers?.map(a => {
+    const q = applicant.questions?.find(item => item.id === a.question_id);
     return { id: q?.id, question: q?.question, answer: a?.answer };
   });
 
-  return { handleViewProfile, handleClickResume, questionList };
+  const handleReject = () => {
+    openReject();
+    closeDetails();
+  };
+
+  const handleMessage = () => {
+    navigate(`/chats?participantId=${applicant.user.id}`);
+  };
+  return { handleViewProfile, handleClickResume, questionList, handleReject, handleMessage };
 };
