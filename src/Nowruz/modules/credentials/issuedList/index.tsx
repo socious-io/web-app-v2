fa0685ce2481cd3399fb47a121b419df8a29546b
marjan-ara <marjan.arami@gmail.com>
2024-05-25 16:23:06 +0330

    asearch for

--- src/Nowruz/modules/credentials/issuedList/index.tsx
+++ src/Nowruz/modules/credentials/issuedList/index.tsx
@@ -75,16 +75,12 @@ export const IssuedList = () => {
               <div className={css.col}>
                 <div className="flex">
                   {item.status === 'PENDING' && <CreditStatus icon="clock" label="Pending" theme="secondary" />}
-                  {item.status === 'APPROVED' && <CreditStatus icon="check" label="Approved" theme="primary" />}
+                  {item.status === 'APPROVED' && <CreditStatus icon="check-circle" label="Accepted" theme="success" />}
                   {item.status === 'SENT' && (
-                    <CreditStatus
-                      icon="check"
-                      label={userProfile ? 'Received' : 'Sent'}
-                      theme={userProfile ? 'secondary' : 'success'}
-                    />
+                    <CreditStatus icon={userProfile ? 'arrow-down' : 'arrow-up'} label="Issued" theme="secondary" />
                   )}
-                  {item.status === 'REJECTED' && <CreditStatus icon="x-close" label="Rejected" theme="error" />}
-                  {item.status === 'CLAIMED' && <CreditStatus icon="check" label="Claimed" theme="success" />}
+                  {item.status === 'REJECTED' && <CreditStatus icon="alert-circle" label="Declined" theme="error" />}
+                  {item.status === 'CLAIMED' && <CreditStatus icon="check-circle" label="Claimed" theme="success" />}
                 </div>
               </div>
               <div className={css.col}>{formatDate(item.created_at)}</div>
