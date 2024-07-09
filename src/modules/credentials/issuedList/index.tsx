013258ca4db49432657a653b8273a2a8231aa902
satanmourner <mahmoudisanaz59@gmail.com>
2024-07-09 23:34:16 +0330

    refactor: credential list issued tab table

--- src/modules/credentials/issuedList/index.tsx
+++ src/modules/credentials/issuedList/index.tsx
@@ -1,8 +1,13 @@
+import { ColumnDef, flexRender, getCoreRowModel, Getter, useReactTable } from '@tanstack/react-table';
+import { useMemo } from 'react';
+import { CredentialEducationRes, CredentialExperienceRes } from 'src/core/api';
 import { formatDate } from 'src/core/time';
+import { getIdentityMeta } from 'src/core/utils';
 import { Avatar } from 'src/modules/general/components/avatar/avatar';
 import { Button } from 'src/modules/general/components/Button';
 import { Checkbox } from 'src/modules/general/components/checkbox/checkbox';
 import { Pagination } from 'src/modules/general/components/Pagination';
+import { PaginationMobile } from 'src/modules/general/components/paginationMobile';
 import Status from 'src/modules/general/components/Status';
 
 import css from './issuedList.module.scss';
@@ -12,6 +17,8 @@ export const IssuedList = () => {
   const {
     issuedList,
     totalPage,
+    generateStatus,
+    page,
     setPage,
     selectedCredential,
     onSelectCredential,
@@ -20,6 +27,107 @@ export const IssuedList = () => {
     onClaim,
     onArchive,
   } = useIssuedList();
+
+  const columns = useMemo<ColumnDef<CredentialExperienceRes | CredentialEducationRes>[]>(
+    () => [
+      {
+        id: 'name',
+        header: 'Name',
+        accessorKey: 'id',
+        cell: ({ getValue }: { getValue: Getter<string> }) => {
+          const id = getValue();
+          const item = issuedList.find(list => list.id === id) || null;
+          if (item) {
+            const { name, username } = getIdentityMeta(userProfile ? item.org : item.user);
+            return (
+              <div className="flex justify-start items-center gap-3">
+                <Avatar
+                  size="40px"
+                  type={userProfile ? 'organizations' : 'users'}
+                  img={userProfile ? item.org_image?.url : item.avatar?.url}
+                />
+                <div className="flex flex-col text-sm font-normal text-Gray-light-mode-600 leading-5">
+                  <span className="font-medium text-Gray-light-mode-900">{name}</span>
+                  {username}
+                </div>
+              </div>
+            );
+          } else {
+            return null;
+          }
+        },
+      },
+      {
+        id: 'type',
+        header: 'Credential Type',
+        accessorKey: 'id',
+        cell: ({ getValue }: { getValue: Getter<string> }) => {
+          const item = issuedList.find(list => list.id === getValue()) || {};
+          return 'experience' in item ? 'Work Certificate' : 'Educational Certificate';
+        },
+      },
+      {
+        id: 'status',
+        header: 'Status',
+        accessorKey: 'status',
+        cell: ({ getValue }: { getValue: Getter<string> }) => (
+          <div className="flex items-center">
+            <Status {...generateStatus[getValue()]} />
+          </div>
+        ),
+      },
+      {
+        id: 'date',
+        header: 'Issued Date',
+        accessorKey: 'created_at',
+        cell: ({ getValue }: { getValue: Getter<string> }) => formatDate(getValue()),
+      },
+    ],
+    [issuedList, selectedCredential],
+  );
+
+  const actionsCell = {
+    id: 'actions',
+    header: '',
+    accessorKey: 'id',
+    cell: ({ getValue }: { getValue: Getter<string> }) => {
+      const item = issuedList.find(list => list.id === getValue()) || null;
+      if (item) {
+        return (
+          <div className="flex flex-1 items-center">
+            {['APPROVED', 'SENT'].includes(item.status) && (
+              <Button
+                color="primary"
+                variant="text"
+                onClick={() => onClaim(item.id, 'experience' in item)}
+                customStyle="!text-sm !font-semibold"
+              >
+                Claim
+              </Button>
+            )}
+
+            <Button
+              color="secondary"
+              variant="text"
+              onClick={() => onArchive(item.id, 'experience' in item)}
+              customStyle="!text-sm !font-semibold"
+            >
+              Archive
+            </Button>
+          </div>
+        );
+      } else {
+        return null;
+      }
+    },
+  };
+
+  const table = useReactTable({
+    data: issuedList,
+    columns: verified && userProfile ? [...columns, actionsCell] : columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
   return (
     <div className="flex flex-col">
       {userProfile && (
@@ -34,105 +142,60 @@ export const IssuedList = () => {
           </Button>
         </div>
       )}
-      <div className={css.tableIssuedList}>
-        <div className={css.header}>
-          <div className="flex flex-[2_2_0%] gap-3">
-            {/* <Checkbox id="select-all" /> */}
-            Name
-          </div>
-          <div className="flex-1">Credential Type</div>
-          <div className={css.col}>Status</div>
-          <div className={css.col}>Issued Date</div>
-          {verified && <div className={css.col} />}
+      <div className={css['table']}>
+        <table className="w-full rounded-lg">
+          <thead className={css['header']}>
+            {table.getHeaderGroups().map(headerGroup => {
+              return (
+                <tr key={headerGroup.id}>
+                  {headerGroup.headers.map(header => {
+                    return (
+                      <th id={header.id} key={header.id} className={css['header__item']}>
+                        {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                      </th>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map(row => {
+              return (
+                <tr key={row.id} className="cursor-pointer">
+                  {row.getVisibleCells().map(cell => {
+                    const item = cell.column.id === 'name' ? cell.row.original : null;
+                    return (
+                      <td className={css['col']} key={cell.id}>
+                        {cell.column.id === 'name' ? (
+                          <div className="flex justify-start items-center gap-3">
+                            {userProfile && item && (
+                              <Checkbox
+                                id={item.id}
+                                checked={selectedCredential.id === item.id}
+                                onChange={() => onSelectCredential(item.id, 'experience' in item)}
+                                disabled={!verified}
+                              />
+                            )}
+                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                          </div>
+                        ) : (
+                          flexRender(cell.column.columnDef.cell, cell.getContext())
+                        )}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <div className={`${css.paginationBox} hidden md:block`}>
+          <Pagination page={page} count={totalPage} onChange={(e, p) => setPage(p)} />
         </div>
-        <div className="flex flex-col">
-          {issuedList.map(item => (
-            <div
-              key={item.id}
-              className="flex items-center text-sm font-normal text-left px-6 py-4 border border-solid border-x-0 border-b-0 border-Gray-light-mode-200"
-            >
-              <div className="flex flex-[2_2_0%] justify-start items-center gap-3">
-                <Checkbox
-                  id={item.id}
-                  checked={selectedCredential.id === item.id}
-                  onChange={() => onSelectCredential(item.id, 'experience' in item)}
-                  disabled={!verified}
-                />
-                {userProfile ? (
-                  <Avatar size="40px" type={'organizations'} img={item.org_image?.url} />
-                ) : (
-                  <Avatar size="40px" type={'users'} img={item.avatar?.url} />
-                )}
-
-                <div className="flex flex-col">
-                  <span className="leading-5 text-sm font-normal text-Gray-light-mode-900">
-                    {userProfile ? item.org.name : `${item.user.first_name} ${item.user.last_name}`}
-                  </span>
-                  <span className="leading-5 text-sm font-normal text-Gray-light-mode-600">
-                    {userProfile ? `@${item.org.shortname}` : `@${item.user.username}`}
-                  </span>
-                </div>
-              </div>
-              <div className="flex-1">{'experience' in item ? 'Work Certificate' : 'Educational Certificate'}</div>
-              <div className={css.col}>
-                <div className="flex">
-                  {item.status === 'PENDING' && (
-                    <Status icon="clock" label="Pending" theme="secondary" transparent={true} />
-                  )}
-                  {item.status === 'APPROVED' && (
-                    <Status
-                      icon={userProfile ? 'arrow-down' : 'arrow-up'}
-                      label="Issued"
-                      theme="secondary"
-                      transparent={true}
-                    />
-                  )}
-                  {item.status === 'SENT' && (
-                    <Status
-                      icon={userProfile ? 'arrow-down' : 'arrow-up'}
-                      label="Issued"
-                      theme="secondary"
-                      transparent={true}
-                    />
-                  )}
-                  {item.status === 'REJECTED' && <Status icon="alert-circle" label="Declined" theme="error" />}
-                  {item.status === 'CLAIMED' && <Status icon="check-circle" label="Claimed" theme="success" />}
-                </div>
-              </div>
-              <div className={css.col}>{formatDate(item.created_at)}</div>
-              {verified &&
-                (userProfile ? (
-                  <div className={css.col}>
-                    {['APPROVED', 'SENT'].includes(item.status) && (
-                      <Button
-                        color="primary"
-                        variant="text"
-                        onClick={() => onClaim(item.id, 'experience' in item)}
-                        customStyle="!text-sm !font-semibold"
-                      >
-                        Claim
-                      </Button>
-                    )}
-
-                    <Button
-                      color="secondary"
-                      variant="text"
-                      onClick={() => onArchive(item.id, 'experience' in item)}
-                      customStyle="!text-sm !font-semibold"
-                    >
-                      Archive
-                    </Button>
-                  </div>
-                ) : (
-                  //FIXME: org create issue later
-                  <div className={css.col} />
-                ))}
-            </div>
-          ))}
+        <div className={`${css.paginationBox} block md:hidden`}>
+          <PaginationMobile page={page} count={totalPage} handleChange={setPage} />
         </div>
-      </div>
-      <div className={css.paginationBox}>
-        <Pagination count={totalPage} onChange={(e, p) => setPage(p)} />
       </div>
     </div>
   );
