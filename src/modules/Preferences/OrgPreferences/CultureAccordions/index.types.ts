c5e917af88098fc4bbcebcb49c59255aef94ddd8
satanmourner <mahmoudisanaz59@gmail.com>
2024-07-21 20:20:49 +0330

    feat: add cultures in org preferences

---
+++ src/modules/Preferences/OrgPreferences/CultureAccordions/index.types.ts
@@ -0,0 +1,14 @@
+import { Preference } from 'src/core/api';
+
+export interface CultureAccordionsProps {
+  preferences: Preference[];
+}
+
+export type CultureType = {
+  key: string;
+  title: string;
+  subtitle: string;
+  value: string;
+  answers: { label: string; value: string }[];
+  description?: string;
+};
