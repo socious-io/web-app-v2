b59088fd13c0713f398a45605d9717265553c4cd
Socious Contributor <172609423+socious-contributor@users.noreply.github.com>
2024-11-05 23:50:28 +0900

    WIP: very WIP, not dangerous but probably won't work w/o addtl dev: construct transaction in UI

--- src/modules/contract/components/PaymentCrypto/usePaymentCrypto.tsx
+++ src/modules/contract/components/PaymentCrypto/usePaymentCrypto.tsx
@@ -1,11 +1,10 @@
+import { Asset, BlockfrostProvider, BrowserWallet, MeshTxBuilder } from '@meshsdk/core';
 import { useEffect, useState } from 'react';
 import { Offer, OrgMeta, hireOffer, payByOffer } from 'src/core/api';
 import dapp from 'src/dapp';
 
-// export const
-
 export const usePaymentCrypto = (handleCloseModal: (paymentSuccess: boolean) => void, offer?: Offer) => {
-  const { chainId, signer, account, isConnected, open } = dapp.useWeb3();
+  const { chainId, signer, account, isConnected, isLaceConnected, open } = dapp.useWeb3();
   const [disabledPayment, setDisabledPayment] = useState(!isConnected);
   const [openErrorModal, setOpenErrorModal] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
@@ -44,36 +43,78 @@ export const usePaymentCrypto = (handleCloseModal: (paymentSuccess: boolean) => 
 
     //FIXME(Elaine): here
     setDisabledPayment(true);
-    try {
-      // put escrow on smart contract
-      const result = await dapp.escrow({
-        signer,
-        chainId,
-        totalAmount: offer.total || 0,
-        escrowAmount: offer.assignment_total || 0,
-        contributor,
-        token: offer.crypto_currency_address,
-        projectId: offer.project_id,
-        verifiedOrg: (offer.offerer.meta as OrgMeta).verified_impact || false,
-        addressReferringOrg: offer.org_referrer_wallet,
-        addressReferringCont: offer.contributor_referrer_wallet,
-        applyOrgFeeDiscount,
-        applyContFeeDiscount,
-      });
 
-      // this is paramater need to sync with backend to make Hire available
-      await payByOffer(offer.id, {
-        service: 'CRYPTO',
-        source: account?.toString() || '',
-        txHash: result.txHash,
-        meta: result,
-      });
-      await hireOffer(offer.id);
-      handleCloseModal(true);
-    } catch (err: any) {
-      handleCloseModal(false);
-      setErrorMessage(err?.response?.data.error || err?.message);
-      setOpenErrorModal(true);
+    if (isLaceConnected) {
+      try {
+        const blockfrostKey = import.meta.env.VITE_BLOCKFROST_KEY;
+        const blockchainProvider = new BlockfrostProvider('<Your-API-Key>');
+        const meshTxBuilder = new MeshTxBuilder({ fetcher: blockchainProvider, submitter: blockchainProvider });
+        const wallet = await BrowserWallet.enable('lace'); // FIXME(Elaine): do we rewrite everything with Mesh's own wallet provider or can we just use the stuff we've already written for lace
+        const contract = new dapp.MeshEscrowContract({
+          mesh: meshTxBuilder,
+          fetcher: blockchainProvider,
+          wallet: wallet,
+          networkId: 2, //FIXME(Elaine): change so that this can be non preview
+        });
+
+        const escrowAmount: Asset[] = [
+          {
+            unit: 'lovelace',
+            quantity: String((offer.amount || 0) * 1_000_000),
+            //FIXME(Elaine): Don't hardcode
+          },
+        ];
+        const feeAmount: Asset[] = [
+          {
+            unit: 'lovelace',
+            quantity: String((offer.fee || 0) * 1_000_000),
+            //FIXME(Elaine): Check if fee is percentage or fixed. We need *absolute* fee here, not percent
+          },
+        ];
+        const feeAddress =
+          'addr_test1qq76z8p3seaq052z9tjlnkxma0d4c7tw5jd4ru8x5jctjkchnel735ck8xakezvp596mdsy9vfu9khvsm0ylpmmprpssafrtcx';
+        //FIXME(Elaine): change this, don't hardcode, add staking for SOCIO, etc
+
+        const tx = await contract.initiateEscrow(escrowAmount, feeAddress, feeAmount);
+        const signedTx = await wallet.signTx(tx);
+        const txHash = await wallet.submitTx(signedTx);
+      } catch (err: any) {
+        console.log('Error with lace payment: ', err);
+      }
+    } else {
+      //FIXME(Elaine): double check the control flow on this
+
+      try {
+        // put escrow on smart contract
+        const result = await dapp.escrow({
+          signer,
+          chainId,
+          totalAmount: offer.total || 0,
+          escrowAmount: offer.assignment_total || 0,
+          contributor,
+          token: offer.crypto_currency_address,
+          projectId: offer.project_id,
+          verifiedOrg: (offer.offerer.meta as OrgMeta).verified_impact || false,
+          addressReferringOrg: offer.org_referrer_wallet,
+          addressReferringCont: offer.contributor_referrer_wallet,
+          applyOrgFeeDiscount,
+          applyContFeeDiscount,
+        });
+
+        // this is paramater need to sync with backend to make Hire available
+        await payByOffer(offer.id, {
+          service: 'CRYPTO',
+          source: account?.toString() || '',
+          txHash: result.txHash,
+          meta: result,
+        });
+        await hireOffer(offer.id);
+        handleCloseModal(true);
+      } catch (err: any) {
+        handleCloseModal(false);
+        setErrorMessage(err?.response?.data.error || err?.message);
+        setOpenErrorModal(true);
+      }
     }
 
     setDisabledPayment(false);
