caa2821d9f6c722f4148fdf7abc9c023afaf9963
Socious Contributor <172609423+socious-contributor@users.noreply.github.com>
2024-11-06 16:44:28 +0900

    not working: try to disable vite optimization

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,9 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), wasm(), topLevelAwait()],
+
   optimizeDeps: {
-    exclude: [`@ionic/pwa-elements/loader`],
+    noDiscovery: true,
+    exclude: [`@ionic/pwa-elements/loader`, `@syntect/wasm`],
   },
 });
