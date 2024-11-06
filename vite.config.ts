98eb8520fbbbd7994fd1b4667f05e05c58dfdf05
Socious Contributor <172609423+socious-contributor@users.noreply.github.com>
2024-11-06 12:31:49 +0900

    fix webassembly dep introduced by mesh, somehow new error with redux

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import topLevelAwait from 'vite-plugin-top-level-await';
+import wasm from 'vite-plugin-wasm';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), wasm(), topLevelAwait()],
   optimizeDeps: {
     exclude: [`@ionic/pwa-elements/loader`],
   },
