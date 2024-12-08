b39356557633e82b78d33b4a32bba3b1a934e869
satanmourner <mahmoudisanaz59@gmail.com>
2024-12-08 23:47:15 +0330

    feat: adaptors/apis/types for service crud

--- src/core/api/services/services.types.ts
+++ src/core/api/services/services.types.ts
@@ -53,19 +53,3 @@ export interface ServiceReq {
   work_samples: string[];
   kind: 'SERVICE';
 }
-
-export interface ServiceReq {
-  title: string;
-  description: string;
-  payment_currency: string;
-  skills: string[];
-  job_category_id: string;
-  project_length: string;
-  commitment_hours_lower: string;
-  commitment_hours_higher: string;
-  payment_mode: PaymentMode;
-  payment_range_lower: string;
-  payment_range_higher: string;
-  work_samples: string[];
-  kind: 'SERVICE';
-}
