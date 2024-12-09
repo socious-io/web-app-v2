5d7b37ea57860f553f6f01b835f59bd1137ced00
Iman rb <iman.rezasefat@gmail.com>
2024-12-09 17:01:26 +0330

    fix : android multi querystring bug

--- src/core/deepLinks.tsx
+++ src/core/deepLinks.tsx
@@ -2,11 +2,15 @@ import { App, URLOpenListenerEvent } from '@capacitor/app';
 import React, { useEffect } from 'react';
 
 export const DeepLinks: React.FC = () => {
+  function escapeAmpersands(url: string): string {
+    // Replace all '&' characters with '\&'
+    return url.replace(/&/g, '\\&');
+  }
   useEffect(() => {
     const handleAppUrlOpen = (event: URLOpenListenerEvent) => {
       const slug = event.url.split('.app').pop();
       if (slug) {
-        window.location.href = slug;
+        window.location.href = escapeAmpersands(slug);
       }
     };
     App.addListener('appUrlOpen', handleAppUrlOpen);
