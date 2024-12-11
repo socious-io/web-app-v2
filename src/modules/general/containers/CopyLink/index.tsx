b70dc314532a880fe68422f4df3e1a2afff9a1ad
satanmourner <mahmoudisanaz59@gmail.com>
2024-12-11 18:32:31 +0330

    feat: copy link as a general component

---
+++ src/modules/general/containers/CopyLink/index.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { translate } from 'src/core/utils';
+import { Icon } from 'src/modules/general/components/Icon';
+import { Input } from 'src/modules/general/components/input/input';
+import variables from 'src/styles/constants/_exports.module.scss';
+
+import styles from './index.module.scss';
+import { CopyLinkProps } from './index.types';
+
+const CopyLink: React.FC<CopyLinkProps> = ({ link, copyText = '', onCopy }) => {
+  const [copied, setCopied] = useState(false);
+
+  const onCopyClick = () => {
+    onCopy();
+    setCopied(true);
+  };
+
+  return (
+    <Input
+      id="copy-url"
+      value={link}
+      postfix={
+        <div className={copied ? styles['copy--success'] : styles['copy']} onClick={onCopyClick}>
+          <Icon
+            name={copied ? 'tick' : 'copy-01'}
+            fontSize={20}
+            color={copied ? variables.color_success_700 : variables.color_grey_700}
+          />
+          {copyText || translate('general-copy')}
+        </div>
+      }
+    />
+  );
+};
+
+export default CopyLink;
