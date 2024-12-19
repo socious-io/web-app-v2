1f0b393bb80d8fdd0c77c74158d62a5027e29bc4
satanmourner <mahmoudisanaz59@gmail.com>
2024-12-19 19:55:51 +0330

    feat: service payment flow on pay page

---
+++ src/modules/Services/containers/ServicePaymentFlow/useServicePaymentFlow.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useLoaderData } from 'react-router-dom';
+import { Service } from 'src/core/adaptors';
+import { Card, CurrentIdentity, updateWallet } from 'src/core/api';
+import { formatDate } from 'src/core/time';
+import Dapp from 'src/dapp';
+import { OrderStatus } from 'src/modules/Services/components/ServiceOrderDetail/index.types';
+import { RootState } from 'src/store';
+
+export const useServicePaymentFlow = () => {
+  const { isConnected, open: openConnect, account } = Dapp.useWeb3();
+  const { serviceDetail: service, cards } = useLoaderData() as { serviceDetail: Service; cards: Card[] };
+  const currentIdentity = useSelector<RootState, CurrentIdentity | undefined>(state => {
+    return state.identity.entities.find(identity => identity.current);
+  });
+  const [step, setStep] = useState(1);
+  const [cardsList, setCardsList] = useState(cards);
+  const [selectedCardId, setSelectedCardId] = useState('');
+  const [openAddCardModal, setOpenAddCardModal] = useState(false);
+  const [orderStatus, setOrderStatus] = useState<OrderStatus | null>(null);
+  const cardsOptionList = cardsList.map(card => {
+    const iconPath = `/icons/pay-icons/${card.meta.brand.toLowerCase().replaceAll(' ', '')}.svg`;
+    return {
+      value: card.id,
+      title: `${card.meta.brand} ending in ${card.meta.last4}`,
+      description: `Expiry ${card.meta.exp_month}/${card.meta.exp_year}`,
+      img: <img src={iconPath} alt="" />,
+    };
+  });
+  const walletAddress = currentIdentity?.meta.wallet_address;
+  const feePercentage = 2;
+  const feeCalculation = parseFloat(service.price) * (feePercentage / 100);
+  const orderPayment = { feePercentage, fee: feeCalculation, total: feeCalculation + parseFloat(service.price) };
+  const paymentIsFiat = service.payment === 'FIAT';
+
+  useEffect(() => {
+    if (isConnected && account && (!walletAddress || String(walletAddress) !== account)) {
+      updateWallet({ wallet_address: account });
+    }
+  }, [isConnected, account]);
+
+  const onPay = async () => {
+    //TODO: payment API call and result for order status
+    setOrderStatus({ status: 'PENDING', orderId: 'SO2023124434353', date: formatDate(new Date()) });
+    setStep(prev => prev + 1);
+  };
+
+  return {
+    data: {
+      service,
+      orderPayment,
+      step,
+      paymentIsFiat,
+      cardsOptionList,
+      selectedCardId,
+      openAddCardModal,
+      isConnected,
+      orderStatus,
+    },
+    operations: { onPay, setCardsList, setSelectedCardId, setOpenAddCardModal, openConnect },
+  };
+};
