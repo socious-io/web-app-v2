45573593699440f7b2eeaf21059eac19129b2dbe
satanmourner <mahmoudisanaz59@gmail.com>
2024-12-20 10:28:24 +0330

    feat: some props on service order detail component for step-2/step-3 of payment flow

--- src/modules/Services/components/ServiceOrderDetail/index.types.ts
+++ src/modules/Services/components/ServiceOrderDetail/index.types.ts
@@ -1,8 +1,18 @@
 import { Service } from 'src/core/adaptors';
+import { NewContractStatus } from 'src/core/api';
+
+export type OrderStatus = {
+  status: NewContractStatus;
+  orderId: string;
+  date: string;
+};
 
 export interface ServiceOrderDetailProps {
   service: Service;
   orderPayment: { total: number; fee: number; feePercentage: number };
+  orderStatus?: OrderStatus | null;
   buttonName?: string;
+  disabledButton?: boolean;
+  loadingButton?: boolean;
   onButtonClick?: () => void;
 }
