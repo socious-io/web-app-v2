2887d7f89bc206f7fa7fe8d2351b89ea1269a963
satanmourner <mahmoudisanaz59@gmail.com>
2024-12-20 10:28:24 +0330

    feat: some props on service order detail component for step-2/step-3 of payment flow

--- src/modules/Services/components/ServiceOrderDetail/index.tsx
+++ src/modules/Services/components/ServiceOrderDetail/index.tsx
@@ -1,4 +1,5 @@
-import { Divider } from '@mui/material';
+import { CircularProgress, Divider } from '@mui/material';
+import { contractStatus } from 'src/constants/CONTRACTS_STATUS';
 import { CURRENCY_SIGNS } from 'src/constants/PAYMENT_CURRENCY';
 import { translate } from 'src/core/utils';
 import { AccountItem } from 'src/modules/general/components/avatarDropDown/avatarDropDown.types';
@@ -7,6 +8,7 @@ import { Button } from 'src/modules/general/components/Button';
 import { Chip } from 'src/modules/general/components/Chip';
 import { ExpandableText } from 'src/modules/general/components/expandableText';
 import { Icon } from 'src/modules/general/components/Icon';
+import Status from 'src/modules/general/components/Status';
 import variables from 'src/styles/constants/_exports.module.scss';
 
 import styles from './index.module.scss';
@@ -15,11 +17,15 @@ import { ServiceOrderDetailProps } from './index.types';
 const ServiceOrderDetail: React.FC<ServiceOrderDetailProps> = ({
   service,
   orderPayment,
+  orderStatus = null,
   buttonName = '',
+  disabledButton = false,
+  loadingButton = false,
   onButtonClick,
 }) => {
   const { name, price, currency, identity, description, skills, delivery, payment } = service || {};
   const { feePercentage, fee, total } = orderPayment;
+  const { status, orderId, date } = orderStatus || {};
 
   const generatePriceFormat = (price: string | number, currency: string) =>
     `${CURRENCY_SIGNS[currency] || ''}${price} ${currency}`;
@@ -28,7 +34,7 @@ const ServiceOrderDetail: React.FC<ServiceOrderDetailProps> = ({
     <div className={styles['container']}>
       <div className={styles['card']}>
         <span className={styles['card__header']}>{name}</span>
-        <AvatarLabelGroup account={identity as AccountItem} customStyle="!p-0" />
+        <AvatarLabelGroup account={identity as AccountItem} customStyle="!p-0" avatarSize="48px" />
         <ExpandableText seeMoreButton={false} text={description} isMarkdown customStyle={styles['card__description']} />
         <div className={styles['card__skills']}>
           {skills.map(item => (
@@ -53,23 +59,55 @@ const ServiceOrderDetail: React.FC<ServiceOrderDetailProps> = ({
             {price} {currency.name}
           </div>
         </div>
+        {orderStatus && (
+          <>
+            <Divider />
+            <div className={styles['order']}>
+              {status && (
+                <div className={styles['order__label']}>
+                  {translate('service-detail.order.status')}
+                  <Status {...contractStatus[status]} />
+                </div>
+              )}
+              <div className={styles['order__label']}>
+                {translate('service-detail.order.number')}
+                <span className={styles['order__value']}>{orderId}</span>
+              </div>
+              <div className={styles['order__label']}>
+                {translate('service-detail.order.date')}
+                <span className={styles['order__value']}>{date}</span>
+              </div>
+            </div>
+          </>
+        )}
       </div>
-      <Divider />
-      <div className={styles['payment']}>
-        <div className={styles['payment__fee']}>
-          {translate('service-detail.order.fee')} ({feePercentage}%)
-          <span>{generatePriceFormat(fee, currency.name)}</span>
-        </div>
-        <div className={styles['payment__total']}>
-          {translate('service-detail.order.total')}
-          <span>{generatePriceFormat(total, currency.name)}</span>
-        </div>
-      </div>
-      <Divider />
-      {buttonName && (
-        <Button color="primary" variant="contained" customStyle="w-full" onClick={onButtonClick}>
-          {buttonName}
-        </Button>
+      {!orderStatus && (
+        <>
+          <Divider />
+          <div className={styles['payment']}>
+            <div className={styles['payment__fee']}>
+              {translate('service-detail.order.fee')} ({feePercentage}%)
+              <span>{generatePriceFormat(fee, currency.name)}</span>
+            </div>
+            <div className={styles['payment__total']}>
+              {translate('service-detail.order.total')}
+              <span>{generatePriceFormat(total, currency.name)}</span>
+            </div>
+          </div>
+          <Divider />
+          {buttonName && (
+            <Button
+              color="primary"
+              variant="contained"
+              customStyle="w-full"
+              disabled={disabledButton}
+              onClick={onButtonClick}
+            >
+              {loadingButton && <CircularProgress size="16px" sx={{ color: variables.color_white }} />}
+              {buttonName}
+            </Button>
+          )}
+        </>
       )}
     </div>
   );
