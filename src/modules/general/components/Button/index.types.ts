48d106b89ef79a6ff32336df823bb253fb86afad
satanmourner <mahmoudisanaz59@gmail.com>
2025-01-13 23:48:02 +0330

    refactor: button warning theme

--- src/modules/general/components/Button/index.types.ts
+++ src/modules/general/components/Button/index.types.ts
@@ -1,9 +1,9 @@
-import { ButtonProps as DefaultProps, LinkProps } from '@mui/material';
+import { ButtonProps as DefaultProps } from '@mui/material';
 
 export interface ButtonProps extends DefaultProps {
   customStyle?: string;
   block?: boolean;
-  color: 'primary' | 'secondary' | 'error' | 'info' | 'inherit';
+  color: 'primary' | 'secondary' | 'error' | 'warning' | 'info' | 'inherit';
   to?: string;
   target?: '_blank' | '_top' | '_self' | '_parent';
 }
