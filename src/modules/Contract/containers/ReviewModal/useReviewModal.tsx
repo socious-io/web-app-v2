7ee7ad87d88a5e72cff63088d8ef11931c80d185
satanmourner <mahmoudisanaz59@gmail.com>
2025-01-23 00:24:07 +0330

    refactor: review modal after confirming a contract

---
+++ src/modules/Contract/containers/ReviewModal/useReviewModal.tsx
@@ -0,0 +1,56 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { Contract, feedbackContractAdaptor } from 'src/core/adaptors';
+import { updateFeedback } from 'src/store/reducers/contracts.reducer';
+import * as yup from 'yup';
+
+import { Form } from './index.types';
+
+const schema = yup.object().shape({
+  content: yup.string().required('Content is required'),
+});
+
+export const useReviewModal = (closeReviewModal: () => void, contract: Contract) => {
+  const dispatch = useDispatch();
+  const partnerName = contract.partner?.meta.name || '';
+  const [selectedReviewValue, setSelectedReviewValue] = useState('satisfactory');
+  const [openSuccessModal, setOpenSuccessModal] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Form>({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit: SubmitHandler<Form> = async (formData: Form) => {
+    const { error, data } =
+      selectedReviewValue === 'satisfactory'
+        ? await feedbackContractAdaptor(contract.id, formData.content)
+        : await feedbackContractAdaptor(contract.id, formData.content);
+    if (error) return;
+    else if (data) {
+      dispatch(updateFeedback({ id: contract.id, feedback: true }));
+      closeReviewModal();
+      setOpenSuccessModal(true);
+    }
+  };
+
+  return {
+    data: {
+      selectedReviewValue,
+      register,
+      errors,
+      partnerName,
+      openSuccessModal,
+    },
+    operations: {
+      setSelectedReviewValue,
+      handleSubmit,
+      onSubmit,
+      setOpenSuccessModal,
+    },
+  };
+};
