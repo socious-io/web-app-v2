74b5eac33c2b33144c9602d8f6e37bc986dfbdee
Iman rb <iman.rezasefat@gmail.com>
2025-02-11 17:37:20 +0330

    changed google login implementation

--- src/pages/oauth/google.tsx
+++ src/pages/oauth/google.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { Capacitor } from '@capacitor/core';
-import { GoogleAuth } from '@codetrix-studio/capacitor-google-auth';
+// import { GoogleAuth } from '@codetrix-studio/capacitor-google-auth';
+import { SocialLogin, GoogleLoginResponseOnline } from '@capgo/capacitor-social-login';
 import { useEffect } from 'react';
 import { useLoaderData, useNavigate, useSearchParams } from 'react-router-dom';
 import { config } from 'src/config';
@@ -12,10 +13,10 @@ import { nonPermanentStorage } from 'src/core/storage/non-permanent';
 import store from 'src/store';
 import { setIdentityList } from 'src/store/reducers/identity.reducer';
 
-GoogleAuth.initialize({
-  clientId: config.googleOauthClientIdIos,
-  scopes: ['profile', 'email'],
-  grantOfflineAccess: true,
+SocialLogin.initialize({
+  google: {
+    iOSClientId: config.googleOauthClientIdIos,
+  },
 });
 export const GoogleOauth2 = () => {
   //in ios instead of https there is capacitor in the link !!
@@ -95,8 +96,14 @@ export const GoogleOauth2 = () => {
     };
 
     if (Capacitor.getPlatform() === 'ios') {
-      GoogleAuth.signIn().then(googleUser => {
-        handleGoogleOauth(googleUser.authentication.idToken, 'ios');
+      SocialLogin.login({
+        provider: 'google',
+        options: {
+          scopes: ['email', 'profile'],
+        },
+      }).then(googleUser => {
+        const googleRes = googleUser.result as GoogleLoginResponseOnline;
+        handleGoogleOauth(googleRes.idToken, 'ios');
       });
     } else {
       if (!code) {
