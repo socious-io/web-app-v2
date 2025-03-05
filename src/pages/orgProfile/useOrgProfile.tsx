9f37476835f50b43436c5c2e6c12992d0bd180c5
Sanaz Mahmoudi <mahmoudisanaz59@gmail.com>
2025-03-05 14:14:20 +0330

    fix: preferences tab for own profile

--- src/pages/orgProfile/useOrgProfile.tsx
+++ src/pages/orgProfile/useOrgProfile.tsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useLoaderData, useLocation } from 'react-router-dom';
 import { ReviewsRes } from 'src/core/adaptors';
-import { JobsRes, OrganizationProfile } from 'src/core/api';
+import { CurrentIdentity, JobsRes, OrganizationProfile, UserProfile } from 'src/core/api';
 import { translate } from 'src/core/utils';
 import Badge from 'src/modules/general/components/Badge';
 import OrgPreferences from 'src/modules/Preferences/OrgPreferences';
 import ReviewsList from 'src/modules/Reviews/containers/ReviewsList';
 import { About } from 'src/modules/userProfile/components/about';
 import { OrganizationJobs } from 'src/modules/userProfile/components/jobs';
+import { RootState } from 'src/store';
 import { setIdentity, setIdentityType } from 'src/store/reducers/profile.reducer';
 
 export const useOrgProfile = () => {
@@ -19,9 +20,13 @@ export const useOrgProfile = () => {
     orgJobs: JobsRes;
     reviews: ReviewsRes;
   };
+  const currentIdentity = useSelector<RootState, CurrentIdentity | undefined>(state => {
+    return state.identity.entities.find(identity => identity.current);
+  });
   const [active, setActive] = useState(0);
   const totalJobs = orgJobs?.total_count || 0;
   const totalReviews = reviews?.total || 0;
+  const myProfile = currentIdentity?.id === organization?.id;
 
   dispatch(setIdentity(organization));
   dispatch(setIdentityType('organizations'));
@@ -43,7 +48,7 @@ export const useOrgProfile = () => {
       ),
       content: <OrganizationJobs />,
     },
-    { label: translate('org-profile.preferences'), content: <OrgPreferences /> },
+    ...(myProfile ? [{ label: translate('org-profile.preferences'), content: <OrgPreferences /> }] : []),
     ...(totalReviews ? [{ label: translate('org-profile.reviews'), content: <ReviewsList /> }] : []),
   ];
 
