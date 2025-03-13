8dceb6746d475e0d98a1a5987147436eb382913f
Shant <Shant>
2025-03-13 18:30:56 +0330

    Like, Comment, repost the Feeds test created

--- cypress/e2e/communities/Like-response.ts
+++ cypress/e2e/communities/Like-response.ts
@@ -1,8 +1,7 @@
-
 export const LIKE_RESPONSE = {
-    "id": "ff1b081f-7a04-4412-8e3c-d6f788d40e6f",
-    "identity_id": "035f724e-5824-4129-8ed6-96c6dda81a3a",
-    "post_id": "bf59fb3f-c07c-465d-9303-33a15ab507ae",
-    "created_at": "2025-03-11T12:30:43.974Z",
-    "comment_id": null
+  id: 'bf59fb3f-c07c-465d-9303-33a15ab507ae',
+  identity_id: '20b07c1c-b142-4ee3-a87f-122aa99a7659',
+  post_id: 'bf59fb3f-c07c-465d-9303-33a15ab507ae',
+  created_at: '2025-03-11T12:30:43.974Z',
+  comment_id: null,
 };
--- cypress/e2e/communities/like-feed.cy.ts
+++ cypress/e2e/communities/like-feed.cy.ts
@@ -1,6 +1,6 @@
 import { LIKE_RESPONSE } from './Like-response';
-import { FEEDS } from './mocks';
-import { Notification } from './notificationMock';
+// import { POST } from './postMock';
+import { COMMENT, FEEDS, REPOST_FEED } from './mocks';
 import {
   API_SERVER,
   APP_URL,
@@ -12,18 +12,15 @@ import {
   USERNAME,
 } from '../authentication/constants';
 import { User, generateRandomEmail } from '../authentication/utilities';
+import { NOTIFICATIONS } from '../userSettings/mocks';
 
 const SIGNINGUP_EMAIL = generateRandomEmail();
 const user = new User(FIRSTNAME, LASTNAME, SIGNINGUP_EMAIL, USERNAME);
 
 describe('Like Feed items', () => {
   beforeEach(() => {
-    cy.setCookie('access_token', ACCESS_TOKEN);
-    cy.setCookie('refresh_token', REFRESH_TOKEN);
-    cy.setCookie('token_type', TOKEN_TYPE);
-
-    cy.intercept('GET', 'https://dev.socious.io/api/v2/posts*', req => {
-      req.reply(200, { FEEDS });
+    cy.intercept('GET', `http://localhost:3000/posts*t=*&page=*&limit=*`, req => {
+      req.reply(200, FEEDS);
     }).as('getFeeds');
 
     cy.intercept('GET', `**/identities*`, req => {
@@ -41,28 +38,61 @@ describe('Like Feed items', () => {
     cy.intercept('POST', '**/cdn-cgi/rum*', req => req.reply(200, { message: 'success' })).as('getrum');
     cy.intercept('GET', '**/chats/unreads/counts*', req => req.reply(200, { count: 0 })).as('getUnreadCounts');
     cy.intercept('POST', '**/cdn-cgi/zaraz/t*', req => req.reply(200, { message: 'success' })).as('postT');
-    cy.intercept('GET', 'https://dev.socious.io/api/v2/notifications*', req => req.reply(200, Notification)).as(
-      'getNotifications',
-    );
+    cy.intercept('GET', `${APP_URL}/notifications*t=*`, req => {
+      req.reply(200, NOTIFICATIONS);
+    }).as('getNotifications');
 
     cy.intercept('POST', '**/posts/*/like', req => {
       req.reply(200, LIKE_RESPONSE);
     }).as('likePost');
+
+    cy.intercept('POST', `${APP_URL}/posts/*/comments`, req => {
+      req.reply(200, COMMENT);
+    }).as('postComment');
+
+    cy.intercept('POST', `${APP_URL}/posts/*/share`, req => {
+      req.reply(200, REPOST_FEED);
+    }).as('postRepost');
   });
   Cypress.on('uncaught:exception', (err, runnable) => {
     return false;
   });
   it('should open the Feeds and like the first post', () => {
-    cy.getCookie('access_token').should('exist');
-    cy.getCookie('refresh_token').should('exist');
-    cy.getCookie('token_type').should('exist');
+    cy.visit(APP_URL + '/feeds');
+    cy.wait('@getFeeds');
+    cy.wait('@getIdentities');
 
+    cy.get('#Feeds-Title').should('be.visible');
+
+    cy.get('[data-testid="like-button"]').first().click();
+
+    cy.wait('@likePost', { timeout: 5000 }) // 5s timeout to give the app time
+      .its('response.statusCode')
+      .should('eq', 200);
+  });
+
+  it('should open the comment box from the feed and type in it', () => {
     cy.visit(APP_URL + '/feeds');
     cy.wait('@getFeeds');
 
     cy.get('#Feeds-Title').should('be.visible');
 
-    cy.get('[data-testid="like-button"]').first().click();
-    cy.wait('@likePost').its('response.statusCode').should('eq', 200);
+    cy.get('[data-testid="comment-button"]').click();
+    cy.get('input[name="comment"]').type('hello this is the comment that it is being tested by');
+
+    cy.contains('button', 'Send').should('be.visible').click();
+    cy.wait('@postComment').its('response.statusCode').should('eq', 200);
+  });
+
+  it('should repost the feed', () => {
+    cy.visit(APP_URL + '/feeds');
+    cy.wait('@getFeeds');
+
+    cy.get('#Feeds-Title').should('be.visible');
+    cy.contains('div', 'Repost').should('exist').click();
+
+    cy.get('textarea[name="repost"]').type('this is reposted by cypress');
+    cy.contains('button', 'Post').click();
+    cy.wait('@postRepost').its('response.statusCode').should('eq', 200);
   });
 });
