c6f6fc5fbde3fb1291edebcae2c142837b4260f5
Shant Isaghoolian <shant@Shants-MacBook-Air.local>
2025-06-10 18:03:14 -0700

    changed back the cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,11 +6,10 @@ dotenv.config();
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      config.env.VITE_ENV = process.env.VITE_ENV || 'development';
+      config.env.api_server = process.env.VITE_BASE_URL;
+      config.env.app_url = process.env.VITE_APP_URL;
+
       return config;
-    },
-    env: {
-      VITE_ENV: 'test',
     },
   },
 });
