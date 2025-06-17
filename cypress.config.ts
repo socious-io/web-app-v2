7d54f365b3efc3fda5d72b9dd88080a34574c324
Shant Isaghoolian <shant@Shants-MacBook-Air.local>
2025-06-16 20:50:43 -0700

    default config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'cypress';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      config.env.VITE_ENV = process.env.VITE_ENV || 'development';
+      config.env.api_server = process.env.VITE_BASE_URL;
+      config.env.app_url = process.env.VITE_APP_URL;
+
       return config;
     },
-    env: {
-      VITE_ENV: 'test',
-    },
-    supportFile: 'cypress/support/e2e.ts',
   },
 });
