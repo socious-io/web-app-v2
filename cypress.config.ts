cb0f4da64076b11b06703273661a646743bb1724
Shant Isaghoolian <shant@Shants-MacBook-Air.local>
2025-06-16 20:50:43 -0700

    default config

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,9 +5,5 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       return config;
     },
-    env: {
-      VITE_ENV: 'test',
-    },
-    supportFile: 'cypress/support/e2e.ts',
   },
 });
