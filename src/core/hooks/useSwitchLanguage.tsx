8274ec1fd3bc24f4daf57cf90a8de7039ae204e4
Sanaz Mahmoudi <mahmoudisanaz59@gmail.com>
2025-07-24 19:05:42 +0330

    fix: everytime no query param or selected language by user, set browser language

--- src/core/hooks/useSwitchLanguage.tsx
+++ src/core/hooks/useSwitchLanguage.tsx
@@ -1,16 +1,23 @@
-import i18next from 'i18next';
 import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useSearchParams } from 'react-router-dom';
 import { supportedLanguages } from 'src/constants/constants';
 
 const useSwitchLanguage = (defaultLanguage = 'en') => {
-  const [selectedLanguage, setSelectedLanguage] = useState(() => {
-    return localStorage.getItem('i18nextLng') || defaultLanguage;
-  });
+  const { i18n } = useTranslation();
+  const [selectedLanguage, setSelectedLanguage] = useState(i18n.language || defaultLanguage);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const queryLang = searchParams.get('lang');
 
   const switchLanguage = language => {
     if (supportedLanguages.map(lang => lang.value).includes(language)) {
       setSelectedLanguage(language);
-      i18next.changeLanguage(language);
+      i18n.changeLanguage(language);
+      localStorage.setItem('lang', language);
+      if (queryLang) {
+        searchParams.set('lang', language);
+        setSearchParams(searchParams);
+      }
       window.location.reload();
     } else {
       console.warn(`Language '${language}' is not supported.`);
